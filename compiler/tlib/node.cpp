012beb7a7e3ade5174f94350015d95594cfdb0cf
Yann Orlarey <orlarey@grame.fr>
2005-08-04 17:40:22 +0000

    *** empty log message ***

---
+++ compiler/tlib/node.cpp
@@ -0,0 +1,14 @@
+#include "node.hh"
+
+ostream& Node::print (ostream& fout) const 					///< print a node on a stream
+{
+	switch (fType) {
+		case kIntNode : 	return fout << fData.i;
+		case kFloatNode : 	return fout << fData.f;
+		case kSymNode : 	return fout << *(fData.s);
+		case kPointerNode : return fout << "ptr:" << fData.p;
+	}
+			
+	return fout;
+}
+
