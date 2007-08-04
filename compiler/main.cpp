820a5c5cccdf323f8313bb9702d10461b280a615
Yann Orlarey <orlarey@grame.fr>
2007-08-04 06:41:42 +0000

    version 0.9.9.2a2 (bug correction)

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -214,7 +214,7 @@ bool process_cmdline(int argc, char* argv[])
 
 void printversion()
 {
-	cout << "FAUST, DSP to C++ compiler, Version 0.9.9.2a1\n";
+	cout << "FAUST, DSP to C++ compiler, Version 0.9.9.2a2\n";
 	cout << "Copyright (C) 2002-2007, GRAME - Centre National de Creation Musicale. All rights reserved. \n\n";
 }
 
