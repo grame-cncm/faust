4b2726b18f08d5cbff30f8d772895e073d057dd1
Yann Orlarey <orlarey@grame.fr>
2008-03-22 10:02:48 +0000

    *** empty log message ***

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -18,7 +18,7 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************
  ************************************************************************/
-#define FAUSTVERSION "0.9.9.3b"
+#define FAUSTVERSION "0.9.9.4"
 
 #include <stdio.h>
 #include <string.h>
