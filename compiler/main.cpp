0947a376d90a811024b50509cb18d3a7aad6b510
Yann Orlarey <orlarey@grame.fr>
2008-06-21 15:31:59 +0000

    version 0.9.9.4e

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -18,7 +18,7 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************
  ************************************************************************/
-#define FAUSTVERSION "0.9.9.4d"
+#define FAUSTVERSION "0.9.9.4e"
 
 #include <stdio.h>
 #include <string.h>
