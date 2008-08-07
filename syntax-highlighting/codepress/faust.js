483f862c6792d8c413f081f5a78088e2b9a8d7ad
Yann Orlarey <orlarey@grame.fr>
2008-08-07 17:22:45 +0000

    code snippets added

--- syntax-highlighting/codepress/faust.js
+++ syntax-highlighting/codepress/faust.js
@@ -15,7 +15,46 @@ Language.syntax = [
 
 
 
-Language.snippets = []
+Language.snippets = [
+    // program and definitions
+    { input : 'pr', output : 'process = $0;' },
+    { input : 'im', output : 'import("$0.lib");' },
+    { input : 'co', output : 'component("$0.dsp")' },
+    { input : 'wi', output : 'with {$0}' },
+
+    // pattern matching
+    { input : 'ca', output : 'case {$0}' },
+    { input : 'ru', output : '($0) => ...;' },
+    
+    // declaration
+    { input : 'de', output : 'declare $0 \t"";' },
+    { input : 'dn', output : 'declare name "$0";' },
+    { input : 'da', output : 'declare author "$0";' },
+    { input : 'dl', output : 'declare license "$0";' },
+    { input : 'dv', output : 'declare version "$0";' },
+
+    // user interface
+    { input : 'bu', output : 'button("$0")' },
+    { input : 'ch', output : 'checkbox("$0")' },
+
+    { input : 'hs', output : 'hslider("$0", 50, 0, 100, 0.1)' },
+    { input : 'vs', output : 'vslider("$0", 50, 0, 100, 0.1)' },
+    { input : 'ne', output : 'nentry ("$0", 50, 0, 100, 0.1)' },
+
+    { input : 'hb', output : 'hbargraph("$0", 0, 100)' },
+    { input : 'vb', output : 'vbargraph("$0", 0, 100)' },
+
+    { input : 'vg', output : 'vgroup("$0", ...)' },
+    { input : 'hg', output : 'hgroup("$0", ...)' },
+    { input : 'tg', output : 'tgroup("$0", ...)' },
+
+    // iterations
+    { input : 'pa', output : 'par(i, $0, ...)' },
+    { input : 'su', output : 'sum(i, $0, ...)' },
+    { input : 'se', output : 'seq(i, $0, ...)' },
+    { input : 'mu', output : 'prod(i, $0, ...)' },
+
+]
 
 Language.complete = [
 	{ input : '\'',output : '\'$0\'' },
