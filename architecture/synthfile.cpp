2359bbeba5a63e9b03b6a54c6f431ddc367f0a19
Yann Orlarey <orlarey@grame.fr>
2008-10-31 08:51:30 +0000

    alloc replaced by calloc

--- architecture/synthfile.cpp
+++ architecture/synthfile.cpp
@@ -42,8 +42,6 @@ inline int		lsr (int x, int n)			{ return int(((unsigned int)x) >> n); }
 *******************************************************************************
 *******************************************************************************/
 
-//inline void *aligned_calloc(size_t nmemb, size_t size) { return (void*)((unsigned)(calloc((nmemb*size)+15,sizeof(char)))+15 & 0xfffffff0); }
-//inline void *aligned_calloc(size_t nmemb, size_t size) { return (void*)((size_t)(calloc((nmemb*size)+15,sizeof(char)))+15 & ~15); }
 
 <<includeIntrinsic>>
 
@@ -368,11 +366,11 @@ class Interleaver
 		
 		// allocate separate input channels
 		for (int i = 0; i < fNumInputs; i++) {
-			fInputs[i] = (float*) alloc (fNumFrames, sizeof(float));
+			fInputs[i] = (float*) calloc (fNumFrames, sizeof(float));
 		}
 		
 		// allocate interleaved output channel
-		fOutput = (float*) alloc(fNumFrames*fNumOutputs, sizeof(float));
+		fOutput = (float*) calloc(fNumFrames*fNumOutputs, sizeof(float));
 		
 	}
 	
