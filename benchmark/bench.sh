1a524b4f254fede9b2d6affab1b9de31a022672c
Yann Orlarey <orlarey@grame.fr>
2009-02-02 09:49:25 +0000

    more informations

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+AOPT="-b 2048 -p 2 -d hw:1"
+DST=results-$(date +%y%m%d.%H%M%S)
 
-echo "benchmark" > results.txt
+echo "Faust ALSA Benchmark : " $AOPT > $DST
+uname -a >> $DST
 for d in *dir; do
 	for f in $d/*; do
 		if [ -x $f ]; then
-			$f -b 2048 -p 3 -d hw:0 >> results.txt
+			$f $AOPT >> $DST
 		fi
 	done
 done
