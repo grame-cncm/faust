7d7d959166b1e24f5fc321693ca3e25550801363
Yann Orlarey <orlarey@grame.fr>
2009-02-02 08:30:36 +0000

    *** empty log message ***

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cat benchmark > results.txt
+echo "benchmark" > results.txt
 for d in *dir; do
 	for f in $d/*; do
 		if [ -x $f ]; then
-			$f -b 2048 -p 2 -d hw:1 >> results.txt
+			$f -b 2048 -p 3 -d hw:0 >> results.txt
 		fi
 	done
 done
