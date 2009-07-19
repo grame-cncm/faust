4e8409922c9fe6d881985787f5bb6c948b842860
Yann Orlarey <orlarey@grame.fr>
2009-07-19 12:28:23 +0000

    startTiming and endTiming functions added

---
+++ compiler/errors/timing.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cassert>
+#include <sys/time.h>
+#include "timing.hh"
+
+using namespace std;
+
+
+#if 0
+double mysecond()
+{
+        struct timeval tp;
+        struct timezone tzp;
+        int i;
+
+        i = gettimeofday(&tp,&tzp);
+        return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
+}
+
+int		lIndex=0;
+double 	lStartTime[1024];
+double 	lEndTime[1024];
+
+static void tab (int n, ostream& fout)
+{
+        fout << '\n';
+        while (n--)     fout << '\t'; 
+}
+
+void startTiming (const char* msg)
+{
+	assert(lIndex < 1023);
+	tab(lIndex, cerr); cerr << "start " << msg << endl;
+	lStartTime[lIndex++] = mysecond();
+}
+
+void endTiming (const char* msg)
+{
+	assert(lIndex>0);
+	lEndTime[--lIndex] = mysecond();
+	tab(lIndex, cerr); cerr << "end " << msg << " (duration : " << lEndTime[lIndex] - lStartTime[lIndex] << ")" << endl;
+}
+
+#else
+
+void startTiming (const char* msg)
+{}
+
+void endTiming (const char* msg)
+{}
+
+#endif
+
---
+++ compiler/errors/timing.hh
@@ -0,0 +1,14 @@
+#ifndef __TIMING__
+#define __TIMING__
+
+
+// use startTiming("foo") and endTiming("foo") to measure the execution time of a portion of code
+// edit timing.cpp de unactivate the code
+
+void startTiming (const char* msg);
+
+void endTiming (const char* msg);
+
+#endif
+
+
