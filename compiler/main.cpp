e310ce489ebc892a85fec6e23dfe79e959e1061e
Yann Orlarey <orlarey@grame.fr>
2010-03-02 13:24:10 +0000

    merge of doc3 branch, version number is now 0.9.13

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -18,7 +18,7 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************
  ************************************************************************/
-#define FAUSTVERSION "0.9.12"
+#define FAUSTVERSION "0.9.13"
 
 #include <stdio.h>
 #include <string.h>
