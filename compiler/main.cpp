92f8399f330c0617f58e9f641678673a23d4a922
Yann Orlarey <orlarey@grame.fr>
2010-05-10 12:11:29 +0000

    Version 0.9.24

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -18,7 +18,7 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************
  ************************************************************************/
-#define FAUSTVERSION "0.9.23"
+#define FAUSTVERSION "0.9.24"
 
 #include <stdio.h>
 #include <string.h>
