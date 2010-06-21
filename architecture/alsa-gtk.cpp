be260263f054d55b97647afaf62f1cead4713692
Yann Orlarey <orlarey@grame.fr>
2010-06-21 19:03:41 +0000

    Tooltip faormat added

--- architecture/alsa-gtk.cpp
+++ architecture/alsa-gtk.cpp
@@ -707,6 +707,28 @@ struct uiItem;
 typedef void (*uiCallback)(float val, void* data);
 
 /**
+* Format tooltip string by replacing some white spaces by 
+* return characters so that line width doesn't exceed n.
+* Limitation : long words exceeding n are not cut 
+*/
+static string formatTooltip(int n, const string& tt)
+{
+	string  ss = tt;	// ss string we are going to format
+	int		lws = 0;	// last white space encountered
+	int 	lri = 0;	// last return inserted
+	for (int i=0; i<tt.size(); i++) {
+		if (tt[i] == ' ') lws = i;
+		if (((i-lri) >= n) && (lws > lri)) {
+			// insert return here
+			ss[lws] = '\n';
+			lri = lws;
+		}
+	}
+	cout << ss;
+	return ss;
+}
+
+/**
  * Graphic User Interface : abstract definition
  */
 
@@ -1205,7 +1227,7 @@ void GTKUI::declare(float* zone, const char* key, const char* value)
         fGuiSize[zone]=atof(value);
     }
     else if (strcmp(key,"tooltip")==0) {
-        fTooltip[zone] = value ;
+        fTooltip[zone] = formatTooltip(30,value) ;
     }
 }
         
