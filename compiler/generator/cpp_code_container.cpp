8f6e5d4fa745e9310a93a1f74f7db1002340c29c
Tim Blechmann <tim@klingt.org>
2010-10-04 11:54:49 +0200

    remove unused openmp header
    clang doesn't provide openmp, but the header is not required
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- compiler/generator/cpp_code_container.cpp
+++ compiler/generator/cpp_code_container.cpp
@@ -33,68 +33,66 @@ using namespace std;
 #include "Text.hh"
 #include "floats.hh"
 
-#include <omp.h>
-
 extern bool gUIMacroSwitch;
 extern int gVectorLoopVariant;
 
 extern map<Tree, set<Tree> > gMetaDataSet;
 
-CodeContainer* CPPCodeContainer::createScalarContainer(const string& name) 
-{ 
+CodeContainer* CPPCodeContainer::createScalarContainer(const string& name)
+{
     return new CPPScalarCodeContainer(name, "", 0, 1, fOut);
 }
 
 void CPPCodeContainer::produceInternal()
 {
     int n = 0;
-    
+
     // Global declarations
     /*
-    tab(n, *fOut); 
+    tab(n, *fOut);
     if (fGlobalDeclarationInstructions->fCode.size() > 0) {
         fCodeProducer.Tab(n);
         fGlobalDeclarationInstructions->accept(&fCodeProducer);
     }
     */
-    
+
     tab(n, *fOut); *fOut << "class " << fKlassName << " {";
-    
-        tab(n+1, *fOut); 
-        
+
+        tab(n+1, *fOut);
+
         if (gUIMacroSwitch) {
             tab(n, *fOut); *fOut << "  public:";
         } else {
             tab(n, *fOut); *fOut << "  private:";
         }
-        tab(n+1, *fOut); 
-        tab(n+1, *fOut); 
-        
+        tab(n+1, *fOut);
+        tab(n+1, *fOut);
+
         // Fields
         if (fDeclarationInstructions->fCode.size() > 0) {
             fCodeProducer.Tab(n+1);
-            
+
             // Sort arrays to be at the begining
             fDeclarationInstructions->fCode.sort(sortFunction1);
-            
+
             fDeclarationInstructions->accept(&fCodeProducer);
         }
-            
+
     tab(n, *fOut); *fOut << "  public:";
-     
+
         // Input method
-        tab(n+1, *fOut); 
+        tab(n+1, *fOut);
         tab(n+1, *fOut); *fOut << "int getNumInputs" << fKlassName << "() { "
                             << "return " << fNumInputs
                             << "; }";
-        
+
         // Output method
         tab(n+1, *fOut); *fOut << "int getNumOutputs" << fKlassName << "() { "
                             << "return " << fNumOutputs
                             << "; }";
-       
+
         // Inits
-        tab(n+1, *fOut); 
+        tab(n+1, *fOut);
         tab(n+1, *fOut); *fOut << "void instanceInit" << fKlassName << "(int samplingFreq) {";
             if (fInitInstructions->fCode.size() > 0) {
                 tab(n+2, *fOut);
@@ -102,7 +100,7 @@ void CPPCodeContainer::produceInternal()
                 fInitInstructions->accept(&fCodeProducer);
             }
         tab(n+1, *fOut); *fOut << "}";
-     
+
         // Fill
         tab(n+1, *fOut);
         tab(n+1, *fOut); *fOut << "void fill" << fKlassName << subst("(int count, $0* output) {", ifloat());
@@ -114,71 +112,71 @@ void CPPCodeContainer::produceInternal()
         ForLoopInst* loop = fCurLoop->getScalarLoop();
         loop->accept(&fCodeProducer);
         tab(n+1, *fOut); *fOut << "}";
-    
+
     tab(n, *fOut); *fOut << "};" << endl;
-    
+
     // Memory methods (as globals)
     tab(n, *fOut); *fOut << fKlassName << "* " << "new" <<  fKlassName << "() { "
                         << "return (" << fKlassName << "*) new "<< fKlassName << "()"
                         << "; }";
-                        
+
     tab(n, *fOut); *fOut << "void " << "delete" << fKlassName << "(" << fKlassName << "* dsp) { "
                         << "delete dsp"
                         << "; }";
     tab(n, *fOut);
 }
 
-void CPPCodeContainer::produceClass() 
+void CPPCodeContainer::produceClass()
 {
     // Initialize "fSamplingFreq" with the "samplingFreq" parameter of the init function
     pushFrontInitMethod(InstBuilder::genStoreVarInst(
-                            InstBuilder::genNamedAddress("fSamplingFreq", Address::kGlobal), 
+                            InstBuilder::genNamedAddress("fSamplingFreq", Address::kGlobal),
                                 InstBuilder::genLoadVarInst(InstBuilder::genNamedAddress("samplingFreq", Address::kFunArgs))));
-    
+
     int n = 0;
-     
+
     // Libraries
     printLibrary(*fOut);
     printIncludeFile(*fOut);
-   
+
     // Sub containers
     generateSubContainers();
-        
+
     // Functions
-    tab(n, *fOut); 
+    tab(n, *fOut);
     if (fGlobalDeclarationInstructions->fCode.size() > 0) {
         fCodeProducer.Tab(n);
         fGlobalDeclarationInstructions->accept(&fCodeProducer);
     }
-    
+
     tab(n, *fOut); *fOut << "class " << fKlassName << " : public " << fSuperKlassName << " {";
-    
-        tab(n+1, *fOut); 
-        
+
+        tab(n+1, *fOut);
+
         if (gUIMacroSwitch) {
             tab(n, *fOut); *fOut << "  public:";
         } else {
             tab(n, *fOut); *fOut << "  private:";
         }
-        tab(n+1, *fOut); 
-       
+        tab(n+1, *fOut);
+
         // Fields
         if (fDeclarationInstructions->fCode.size() > 0) {
             fCodeProducer.Tab(n+1);
             tab(n+1, *fOut);
-            
+
             // Sort arrays to be at the begining
             fDeclarationInstructions->fCode.sort(sortFunction1);
-            
+
             fDeclarationInstructions->accept(&fCodeProducer);
         }
-            
+
     tab(n, *fOut); *fOut << "  public:";
-    
+
         // Print metadata declaration
         tab(n+1, *fOut);
         tab(n+1, *fOut); *fOut   << "void static metadata(Meta* m) { ";
-        
+
         for (map<Tree, set<Tree> >::iterator i = gMetaDataSet.begin(); i != gMetaDataSet.end(); i++) {
             if (i->first != tree("author")) {
                 tab(n+2, *fOut); *fOut << "m->declare(\"" << *(i->first) << "\", " << **(i->second.begin()) << ");";
@@ -192,34 +190,34 @@ void CPPCodeContainer::produceClass()
                 }
             }
         }
-    
+
         tab(n+1, *fOut); *fOut << "}" << endl;
-        
+
         tab(n+1, *fOut); *fOut << "virtual ~" << fKlassName << "() {";
             tab(n+2, *fOut); *fOut << "destroy();";
         tab(n+1, *fOut); *fOut << "}" << endl;
-        
+
         tab(n+1, *fOut); *fOut << "void destroy() {";
             if (fDestroyInstructions->fCode.size() > 0) {
-                tab(n+2, *fOut); 
+                tab(n+2, *fOut);
                 fCodeProducer.Tab(n+2);
                 fDestroyInstructions->accept(&fCodeProducer);
             }
         tab(n+1, *fOut);  *fOut << "}";
-        tab(n+1, *fOut); 
-            
+        tab(n+1, *fOut);
+
         // Input method
         tab(n+1, *fOut); *fOut << "virtual int getNumInputs() { "
                             << "return " << fNumInputs
                             << "; }";
-        
+
         // Output method
         tab(n+1, *fOut); *fOut << "virtual int getNumOutputs() { "
                             << "return " << fNumOutputs
                             << "; }";
-       
+
         // Inits
-        tab(n+1, *fOut); 
+        tab(n+1, *fOut);
         tab(n+1, *fOut); *fOut << "static void classInit(int samplingFreq) {";
             if (fStaticInitInstructions->fCode.size() > 0) {
                 tab(n+2, *fOut);
@@ -228,7 +226,7 @@ void CPPCodeContainer::produceClass()
             }
         tab(n+1, *fOut); *fOut << "}";
 
-        tab(n+1, *fOut); 
+        tab(n+1, *fOut);
         tab(n+1, *fOut); *fOut << "virtual void instanceInit(int samplingFreq) {";
             if (fInitInstructions->fCode.size() > 0) {
                 tab(n+2, *fOut);
@@ -237,14 +235,14 @@ void CPPCodeContainer::produceClass()
             }
         tab(n+1, *fOut); *fOut << "}";
 
-        tab(n+1, *fOut); 
+        tab(n+1, *fOut);
         tab(n+1, *fOut); *fOut << "virtual void init(int samplingFreq) {";
             tab(n+2, *fOut); *fOut << "classInit(samplingFreq);";
             tab(n+2, *fOut); *fOut << "instanceInit(samplingFreq);";
         tab(n+1, *fOut); *fOut << "}";
-        
+
         // User interface
-        tab(n+1, *fOut); 
+        tab(n+1, *fOut);
         tab(n+1, *fOut); *fOut << "virtual void buildUserInterface(UI* interface) {";
             if (fUserInterfaceInstructions->fCode.size() > 0) {
                 tab(n+2, *fOut);
@@ -252,20 +250,20 @@ void CPPCodeContainer::produceClass()
                 fUserInterfaceInstructions->accept(&fCodeProducer);
             }
         tab(n+1, *fOut); *fOut << "}";
-        
+
         // Compute
         generateCompute(n);
         tab(n, *fOut);
-        
+
         // Possibly generate separated functions
         fCodeProducer.Tab(n+1);
         tab(n+1, *fOut);
-        if (fComputeFunctions->fCode.size() > 0) {    
+        if (fComputeFunctions->fCode.size() > 0) {
             fComputeFunctions->accept(&fCodeProducer);
         }
-    
+
     tab(n, *fOut); *fOut << "};\n" << endl;
-    
+
     // Generate user interface macros if needed
 	if (gUIMacroSwitch) {
 		tab(n, *fOut); *fOut << "#ifdef FAUST_UIMACROS";
@@ -294,14 +292,14 @@ void CPPScalarCodeContainer::generateCompute(int n)
     tab(n+1, *fOut); *fOut << subst("virtual void compute(int count, $0** inputs, $0** outputs) {", xfloat());
     tab(n+2, *fOut);
     fCodeProducer.Tab(n+2);
-    
+
     // Generates local variables declaration and setup
     fComputeBlockInstructions->accept(&fCodeProducer);
-    
+
     // Generates one single scalar loop
     ForLoopInst* loop = fCurLoop->getScalarLoop();
     loop->accept(&fCodeProducer);
-    
+
     tab(n+1, *fOut); *fOut << "}";
 }
 
@@ -312,7 +310,7 @@ CPPVectorCodeContainer::CPPVectorCodeContainer(const string& name, const string&
 
 CPPVectorCodeContainer::~CPPVectorCodeContainer()
 {}
-        
+
 void CPPVectorCodeContainer::generateCompute(int n)
 {
     // Generates declaration
@@ -320,13 +318,13 @@ void CPPVectorCodeContainer::generateCompute(int n)
     tab(n+1, *fOut); *fOut << subst("virtual void compute(int fullcount, $0** inputs, $0** outputs) {", xfloat());
     tab(n+2, *fOut);
     fCodeProducer.Tab(n+2);
-    
+
     // Sort arrays to be at the begining
     fComputeBlockInstructions->fCode.sort(sortFunction1);
-    
+
     // Generates local variables declaration and setup
     fComputeBlockInstructions->accept(&fCodeProducer);
-    
+
     // Prepare global loop
     StatementInst* block = NULL;
     if (gVectorLoopVariant == 0) {
@@ -334,11 +332,11 @@ void CPPVectorCodeContainer::generateCompute(int n)
     } else if (gVectorLoopVariant == 1) {
         block = generateDAGLoopVariant1();
     }
-    
+
     // Generate it
     assert(block);
     block->accept(&fCodeProducer);
-      
+
     tab(n+1, *fOut); *fOut << "}";
 }
 
@@ -357,23 +355,23 @@ void CPPOpenMPCodeContainer::generateCompute(int n)
     tab(n+1, *fOut); *fOut << subst("virtual void compute(int fullcount, $0** inputs, $0** outputs) {", xfloat());
     tab(n+2, *fOut);
     fCodeProducer.Tab(n+2);
-    
+
     // Transform stack array variables in struct variables
     //MoveStackArray2Struct();
-    
+
     // Sort arrays to be at the begining
-    fComputeBlockInstructions->fCode.sort(sortFunction1); 
-    
+    fComputeBlockInstructions->fCode.sort(sortFunction1);
+
     // Generates local variables declaration and setup
     fComputeBlockInstructions->accept(&fCodeProducer);
-    
+
     // Prepare global loop
     StatementInst* block = generateDAGLoopOMP();
-    
+
     // Generate it
     assert(block);
     block->accept(&fCodeProducer);
-      
+
     tab(n+1, *fOut); *fOut << "}";
 }
 
@@ -385,27 +383,27 @@ CPPWorkStealingCodeContainer::CPPWorkStealingCodeContainer(const string& name, c
 CPPWorkStealingCodeContainer::~CPPWorkStealingCodeContainer()
 {}
 
-void CPPWorkStealingCodeContainer::produceClass() 
+void CPPWorkStealingCodeContainer::produceClass()
 {
     /*
     // Transform stack array variables in struct variables
     MoveStackArray2Struct();
-    
+
     // Transform stack slow variables in struct variables
     MoveStackSlow2Struct();
     */
-    
+
     // Transform some stack variables in struct variables
     MoveStack2Struct();
-      
+
     // Specific init code
     CodeContainer::generateDAGLoopWSSAux3();
-      
+
     // Inherited method
     CPPCodeContainer::produceClass();
-   
-    int n = 0; 
-    //tab(n, *fOut); 
+
+    int n = 0;
+    //tab(n, *fOut);
     *fOut << "void computeThreadExternal(void* dsp, int num_thread) {";
         tab(n+1, *fOut); *fOut << "static_cast<" << fKlassName << "*>(dsp)->computeThread(num_thread);";
     tab(n, *fOut); *fOut << "}" << endl;
@@ -416,35 +414,35 @@ void CPPWorkStealingCodeContainer::generateCompute(int n)
     lclgraph dag;
     CodeLoop::sortGraph(fCurLoop, dag);
     computeForwardDAG(dag);
-    
+
     // Compute "compute" declaration
     tab(n+1, *fOut);
     tab(n+1, *fOut); *fOut << subst("virtual void compute(int fullcount, $0** inputs, $0** outputs) {", xfloat());
     tab(n+2, *fOut);
     fCodeProducer.Tab(n+2);
-    
+
     generateDAGLoopWSSAux2(fComputeBlockInstructions, false);
-  
+
     // Sort arrays to be at the begining
-    fComputeBlockInstructions->fCode.sort(sortFunction1); 
-    
+    fComputeBlockInstructions->fCode.sort(sortFunction1);
+
     // Generates local variables declaration and setup
     fComputeBlockInstructions->accept(&fCodeProducer);
-      
+
     tab(n+1, *fOut); *fOut << "}" << endl;
-    
+
     // Generates "computeThread" code
     tab(n+1, *fOut); *fOut << "void computeThread(int num_thread) {";
     tab(n+2, *fOut);
     fCodeProducer.Tab(n+2);
-    
+
     // Prepare global loop
     StatementInst* block = generateDAGLoopWSS(dag);
-    
+
     // Generate it
     assert(block);
     block->accept(&fCodeProducer);
-    
+
     tab(n+1, *fOut); *fOut << "}";
 }
 
