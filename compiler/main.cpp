085a66f83d582a2828b0d73c066ee5bac6aff0c4
Yann Orlarey <orlarey@grame.fr>
2010-10-16 16:57:34 +0200

    Faust version set to 2.0.a2

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -18,7 +18,7 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************
  ************************************************************************/
-#define FAUSTVERSION "0.9.30"
+#define FAUSTVERSION "2.0.a2"
 
 #include <stdio.h>
 #include <string.h>
