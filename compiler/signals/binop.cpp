7ceba1fe675966aed33ad5814072ff0dcd761c78
Tim Blechmann <tim@klingt.org>
2010-11-10 16:46:57 +0100

    bin ops: introduce notion of absorbing elements
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- compiler/signals/binop.cpp
+++ compiler/signals/binop.cpp
@@ -18,23 +18,30 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************
  ************************************************************************/
- 
- 
- 
+
+
+
 #include "binop.hh"
+#include "signals.hh"
 
 #include <llvm/Instructions.h>
 #include <llvm/Instruction.h>
 
 using namespace llvm;
-	
-static bool noNtrl(const Node& n) { return false; }
 
-BinOp* gBinOpTable[] = { 
+
+bool falsePredicate(Node const & a)
+{
+    return false;
+}
+
+static bool noNtrl(const Node& n) { return falsePredicate(n); }
+
+BinOp* gBinOpTable[] = {
 
 	new BinOp("+","add_vec","add_scal", "add nsw", "fadd", Instruction::Add, Instruction::FAdd, &addNode, &isZero, &isZero, 6),
 	new BinOp("-","sub_vec","sub_scal", "sub nsw", "fsub", Instruction::Sub, Instruction::FSub, &subNode, &noNtrl, &isZero, 6),
-	new BinOp("*","mul_vec","mul_scal", "mul nsw", "fmul", Instruction::Mul, Instruction::FMul, &mulNode, &isOne, &isOne, 7),
+	new BinOp("*","mul_vec","mul_scal", "mul nsw", "fmul", Instruction::Mul, Instruction::FMul, &mulNode, &isOne, &isOne, 7, isZero, isZero),
 	new BinOp("/","div_vec","div_scal", "sdiv", "fdiv", Instruction::SDiv, Instruction::FDiv, &divNode, &noNtrl, &isOne, 8),
 	new BinOp("%","mod_vec","mod_scal", "srem", "frem", Instruction::SRem, Instruction::FRem, &remNode, &noNtrl, &noNtrl, 8),
 
@@ -48,7 +55,7 @@ BinOp* gBinOpTable[] = {
 	new BinOp("==","eq_vec","eq_scal", "icmp eq", "fcmp eq", ICmpInst::ICMP_EQ, FCmpInst::FCMP_OEQ, &eqNode, &noNtrl, &noNtrl, 5),
 	new BinOp("!=","neq_vec","neq_scal", "icmp ne", "fcmp ne", ICmpInst::ICMP_NE, FCmpInst::FCMP_ONE, &neNode, &noNtrl, &noNtrl, 5),
 
-	new BinOp("&","and_vec","and_scal", "and", "and", Instruction::And, Instruction::And, &andNode, &isMinusOne, &isMinusOne, 7),
+	new BinOp("&","and_vec","and_scal", "and", "and", Instruction::And, Instruction::And, &andNode, &isMinusOne, &isMinusOne, 7, isZero, isZero),
 	new BinOp("|","or_vec","or_scal", "or", "or", Instruction::Or, Instruction::Or, &orNode, &isZero, &isZero, 6),
 	new BinOp("^","xor_vec","xor_scal", "xor", "xor", Instruction::Xor, Instruction::Xor, &xorNode, &noNtrl, &noNtrl, 7)
 };
@@ -88,25 +95,25 @@ BinOp* gBinOpTable[] = {
           case FCmpInst::FCMP_TRUE:  Out << "TRUE"; break;
 */
 
-BinOp* gBinOpLateqTable[] = { 
+BinOp* gBinOpLateqTable[] = {
 
 	new BinOp("+","add_vec","add_scal", "add nsw", "fadd", 0, 0, &addNode, &isZero, &isZero, 6),
 	new BinOp("-","sub_vec","sub_scal", "sub nsw", "fsub", 0, 0, &subNode, &noNtrl, &isZero, 6),
-	new BinOp("*","mul_vec","mul_scal", "mul nsw", "fmul", 0, 0, &mulNode, &isOne, &isOne, 7), // \DeclareMathSymbol{*}{\mathbin}{symbols}{"01}
+	new BinOp("*","mul_vec","mul_scal", "mul nsw", "fmul", 0, 0, &mulNode, &isOne, &isOne, 7, isZero, isZero), // \DeclareMathSymbol{*}{\mathbin}{symbols}{"01}
 	new BinOp("/","div_vec","div_scal", "div", "fdiv", 0, 0, &divNode, &noNtrl, &isOne, 9), // \frac{}{} used in generateBinOp
 	new BinOp("\\bmod","mod_vec","mod_scal", "srem", "frem", 0, 0, &remNode, &noNtrl, &noNtrl, 8),
-	
+
 	new BinOp("\\hiderel{\\ll}","shift_left_vec","shift_left_scal","","", 0, 0, &lshNode, &noNtrl, &isZero, 7),
 	new BinOp("\\hiderel{\\gg}","shift_right_vec","shift_right_scal","","", 0, 0, &rshNode, &noNtrl, &isZero, 7),
-	
+
 	new BinOp("\\hiderel{>}","gt_vec","gt_scal", "icmp ugt", "fcmp ugt", 0, 0, &gtNode, &noNtrl, &noNtrl, 5),
 	new BinOp("\\hiderel{<}","lt_vec","lt_scal", "icmp ult", "fcmp ult", 0, 0, &ltNode, &noNtrl, &noNtrl, 5),
 	new BinOp("\\hiderel{\\geq}","ge_vec","ge_scal", "icmp uge", "fcmp uge", 0, 0, &geNode, &noNtrl, &noNtrl, 5),
 	new BinOp("\\hiderel{\\leq}","le_vec","le_scal", "icmp ule", "fcmp ule", 0, 0, &leNode, &noNtrl, &noNtrl, 5),
 	new BinOp("\\hiderel{\\equiv}","eq_vec","eq_scal", "icmp eq", "fcmp eq", 0, 0, &eqNode, &noNtrl, &noNtrl, 5),
 	new BinOp("\\hiderel{\\neq}","neq_vec","neq_scal", "icmp ne", "fcmp ne", 0, 0, &neNode, &noNtrl, &noNtrl, 5),
-	
-	new BinOp("\\wedge","and_vec","and_scal", "and", "and", 0, 0, &andNode, &isMinusOne, &isMinusOne, 7),
+
+	new BinOp("\\wedge","and_vec","and_scal", "and", "and", 0, 0, &andNode, &isMinusOne, &isMinusOne, 7, isZero, isZero),
 	new BinOp("\\vee","or_vec","or_scal","or", "or", 0, 0, &orNode, &isZero, &isZero, 6),
 	new BinOp("\\veebar","xor_vec","xor_scal", "xor", "xor", 0, 0, &xorNode, &noNtrl, &noNtrl, 7)
 };
