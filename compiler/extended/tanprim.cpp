08f81ba4f8703df63ff0e6fe56f0c23d45d2fafd
Tim Blechmann <tim@klingt.org>
2010-11-10 12:29:57 +0100

    tan: slightly improve interval inference
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- compiler/extended/tanprim.cpp
+++ compiler/extended/tanprim.cpp
@@ -9,21 +9,30 @@ class TanPrim : public xtended
 {
 
  public:
- 
+
  	TanPrim() : xtended("tan") {}
-	
+
 	virtual unsigned int arity () { return 1; }
-	
+
 	virtual bool	needCache ()	{ return true; }
-	
+
 	virtual Type 	infereSigType (const vector<Type>& args)
 	{
 		assert (args.size() == 1);
-		return castInterval(floatCast(args[0]), interval());
+        interval srcInterval = args[0]->getInterval();
+        const double halfpi = M_PI/2;
+        interval resultInterval;
+
+        // the check can be improved to ensure that no infinity is in the range
+        if (srcInterval.valid) {
+            if ( (-halfpi < srcInterval.lo ) && (srcInterval.hi < halfpi) )
+                resultInterval = interval(tan(srcInterval.lo), tan(srcInterval.hi));
+        }
+		return castInterval(floatCast(args[0]), resultInterval);
 	}
-	
-	virtual void 	sigVisit (Tree sig, sigvisitor* visitor) {}	
-	
+
+	virtual void 	sigVisit (Tree sig, sigvisitor* visitor) {}
+
 	virtual int infereSigOrder (const vector<int>& args) {
 		return args[0];
 	}
@@ -36,12 +45,12 @@ class TanPrim : public xtended
 			return tree(symbol(), args[0]);
 		}
 	}
-    
+
     virtual ValueInst* generateCode(int variability, CodeContainer* container, const list<ValueInst*>& args, ::Type result, vector< ::Type>& types)
     {
         assert (args.size() == arity());
 		assert (types.size() == arity());
-        
+
         Typed::VarType result_type;
         if (result->nature() == kInt) result_type = Typed::kInt; else result_type = itfloat();
         vector<Typed::VarType> arg_types;
@@ -51,18 +60,18 @@ class TanPrim : public xtended
             if (((*it)->nature() == kInt)) t1 = Typed::kInt; else t1 = itfloat();
             arg_types.push_back(t1);
         }
-        
+
         return container->pushFunction(subst("tan$0", isuffix()), result_type, arg_types, args);
     }
-	
+
 	virtual string 	generateLateq (Lateq* lateq, const vector<string>& args, const vector<Type>& types)
 	{
 		assert (args.size() == arity());
 		assert (types.size() == arity());
-		
+
         return subst("\\tan\\left($0\\right)", args[0]);
 	}
-	
+
 };
 
 
