0458e306b100d3f473371d2d3ee115b28f76aae4
Yann Orlarey <orlarey@grame.fr>
2010-12-29 13:00:46 +0100

    GTK Knobs contributed by Hermann

--- architecture/jack-gtk.cpp
+++ architecture/jack-gtk.cpp
@@ -309,6 +309,7 @@ inline void UI::addCallback(float* zone, uiCallback foo, void* data)
 *******************************************************************************/
 
 #include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
 
 #define stackSize 256
 
@@ -317,6 +318,434 @@ inline void UI::addCallback(float* zone, uiCallback foo, void* data)
 #define kSingleMode 0
 #define kBoxMode 1
 #define kTabMode 2
+
+
+class GtkKnob
+{
+private:
+
+	double start_x, start_y, max_value;
+public:
+	GtkRange parent;
+	int last_quadrant;
+	GtkKnob();
+	~GtkKnob();
+	GtkWidget *gtk_knob_new_with_adjustment(GtkAdjustment *_adjustment);
+	
+};
+
+
+#define GTK_TYPE_KNOB          (gtk_knob_get_type())
+#define GTK_KNOB(obj)          (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_KNOB, GtkKnob))
+#define GTK_IS_KNOB(obj)       (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_KNOB))
+#define GTK_KNOB_CLASS(klass)  (G_TYPE_CHECK_CLASS_CAST ((klass),  GTK_TYPE_KNOB, GtkKnobClass))
+#define GTK_IS_KNOB_CLASS(obj) (G_TYPE_CHECK_CLASS_TYPE ((klass),  GTK_TYPE_KNOB))
+
+GtkKnob::GtkKnob()
+// GtkKnob constructor
+{
+
+}
+
+GtkKnob::~GtkKnob()
+{
+	// Nothing specific to do...
+}
+
+struct GtkKnobClass {
+	GtkRangeClass parent_class;
+	int knob_x;
+	int knob_y;
+	int knob_step;
+	int button_is;
+
+};
+
+//------forward declaration
+GType gtk_knob_get_type ();
+
+/****************************************************************
+ ** calculate the knop pointer with dead zone
+ */
+
+const double scale_zero = 20 * (M_PI/180); // defines "dead zone" for knobs
+
+//------------ calculate needed precision
+static int precision(double n)
+{
+	if (n < 0.009999) return 3;
+	else if (n < 0.099999) return 2;
+	else if (n < 0.999999) return 1;
+	else return 0;
+}
+
+
+static void knob_expose(GtkWidget *widget, int knob_x, int knob_y, int arc_offset)
+{
+	GtkAdjustment *adj = gtk_range_get_adjustment(GTK_RANGE(widget));
+	int knobx = widget->allocation.x + (widget->allocation.width - knob_x) * 0.5;
+	int knoby = widget->allocation.y + (widget->allocation.height - knob_y) * 0.5;
+	int knobx1 = widget->allocation.x + (widget->allocation.width) * 0.5;
+	int knoby1 = widget->allocation.y + (widget->allocation.height) * 0.5;
+	double knobstate = (adj->value - adj->lower) / (adj->upper - adj->lower);
+	double angle = scale_zero + knobstate * 2 * (M_PI - scale_zero);
+	const double pointer_off = 5;
+	double radius = min(knob_x-pointer_off, knob_y-pointer_off) / 2;
+	double lengh_x = (knobx+radius+pointer_off/2) - radius * sin(angle);
+	double lengh_y = (knoby+radius+pointer_off/2) + radius * cos(angle);
+	double radius1 = min(knob_x, knob_y) / 2;
+
+	if (GTK_WIDGET_HAS_FOCUS(widget)== TRUE) {
+		gtk_paint_focus(widget->style, widget->window, GTK_STATE_NORMAL, NULL, widget, NULL,
+		                knobx, knoby, knob_x, knob_y);
+	}
+	/** create a knob and a pointer rotating on the knob with cairo **/
+	cairo_t *cr = gdk_cairo_create(GDK_DRAWABLE(widget->window));
+	cairo_arc(cr,knobx1+arc_offset, knoby1+arc_offset, knob_x/2.1, 0, 2 * M_PI );
+	cairo_pattern_t*pat =
+		cairo_pattern_create_radial (knobx1+arc_offset-4,knoby1+arc_offset-4, 1,knobx1+arc_offset,knoby1+arc_offset, 15);
+	cairo_pattern_add_color_stop_rgb (pat, 0, 0.4, 0.4, 0.4);
+	cairo_pattern_add_color_stop_rgb (pat, 0.5, 0.15, 0.15, 0.15);
+	cairo_pattern_add_color_stop_rgb (pat, 1, 0, 0, 0);
+	cairo_set_line_width(cr, 2.0);
+	cairo_set_source (cr, pat);
+	cairo_fill_preserve (cr);
+	gdk_cairo_set_source_color(cr,  gtk_widget_get_style (widget)->fg);
+	cairo_stroke(cr);
+	
+	cairo_set_source_rgb(cr,  0.1, 0.1, 0.1);
+	cairo_set_line_width(cr, 5.0);
+	cairo_move_to(cr, knobx+radius1, knoby+radius1);
+	cairo_line_to(cr,lengh_x,lengh_y);
+	cairo_stroke(cr);
+	cairo_set_source_rgb(cr,  0.9, 0.9, 0.9);
+	cairo_set_line_width(cr, 1.0);
+	cairo_move_to(cr, knobx+radius1, knoby+radius1);
+	cairo_line_to(cr,lengh_x,lengh_y);
+	cairo_stroke(cr);
+	cairo_destroy(cr);
+}
+
+/****************************************************************
+ ** general expose events for all "knob" controllers
+ */
+
+//----------- draw the Knob when moved
+static gboolean gtk_knob_expose (GtkWidget *widget, GdkEventExpose *event)
+{
+	g_assert(GTK_IS_KNOB(widget));
+	GtkKnob *knob = GTK_KNOB(widget);
+	GtkKnobClass *klass =  GTK_KNOB_CLASS(GTK_OBJECT_GET_CLASS(widget));
+	knob_expose(widget, klass->knob_x, klass->knob_y, 0);
+	return TRUE;
+}
+
+/****************************************************************
+ ** set size for GdkDrawable per type
+ */
+
+static void gtk_knob_size_request (GtkWidget *widget, GtkRequisition *requisition)
+{
+	g_assert(GTK_IS_KNOB(widget));
+	GtkKnob *knob = GTK_KNOB(widget);
+	GtkKnobClass *klass =  GTK_KNOB_CLASS(GTK_OBJECT_GET_CLASS(widget));
+	requisition->width = klass->knob_x;
+	requisition->height = klass->knob_y;
+}
+
+/****************************************************************
+ ** set value from key bindings
+ */
+
+static void gtk_knob_set_value (GtkWidget *widget, int dir_down)
+{
+	g_assert(GTK_IS_KNOB(widget));
+
+	GtkAdjustment *adj = gtk_range_get_adjustment(GTK_RANGE(widget));
+
+	int oldstep = (int)(0.5f + (adj->value - adj->lower) / adj->step_increment);
+	int step;
+	int nsteps = (int)(0.5f + (adj->upper - adj->lower) / adj->step_increment);
+	if (dir_down)
+		step = oldstep - 1;
+	else
+		step = oldstep + 1;
+	float value = adj->lower + step * double(adj->upper - adj->lower) / nsteps;
+	gtk_widget_grab_focus(widget);
+	gtk_range_set_value(GTK_RANGE(widget), value);
+}
+
+/****************************************************************
+ ** keyboard bindings
+ */
+
+static gboolean gtk_knob_key_press (GtkWidget *widget, GdkEventKey *event)
+{
+	g_assert(GTK_IS_KNOB(widget));
+
+	GtkAdjustment *adj = gtk_range_get_adjustment(GTK_RANGE(widget));
+	switch (event->keyval) {
+	case GDK_Home:
+		gtk_range_set_value(GTK_RANGE(widget), adj->lower);
+		return TRUE;
+	case GDK_End:
+		gtk_range_set_value(GTK_RANGE(widget), adj->upper);
+		return TRUE;
+	case GDK_Up:
+		gtk_knob_set_value(widget, 0);
+		return TRUE;
+	case GDK_Right:
+		gtk_knob_set_value(widget, 0);
+		return TRUE;
+	case GDK_Down:
+		gtk_knob_set_value(widget, 1);
+		return TRUE;
+	case GDK_Left:
+		gtk_knob_set_value(widget, 1);
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+//------------ calculate value for display
+static double gtk_knob_get_value(GtkAdjustment *adj,double pos)
+{
+    if (adj->step_increment < 0.009999) pos = (floor (pos*1000))*0.001;
+    else if (adj->step_increment < 0.099999) pos = (floor (pos*100))*0.01;
+    else if (adj->step_increment < 0.999999) pos = (floor (pos*10))*0.1;
+    else pos = floor (pos);
+    return pos;
+
+}
+
+/****************************************************************
+ ** alternative (radial) knob motion mode (ctrl + mouse pressed)
+ */
+
+static void knob_pointer_event(GtkWidget *widget, gdouble x, gdouble y, int knob_x, int knob_y,
+                               bool drag, int state)
+{
+	static double last_y = 2e20;
+	GtkKnob *knob = GTK_KNOB(widget);
+	GtkAdjustment *adj = gtk_range_get_adjustment(GTK_RANGE(widget));
+	double radius =  min(knob_x, knob_y) / 2;
+	int  knobx = (widget->allocation.width - knob_x) / 2;
+	int  knoby = (widget->allocation.height - knob_y) / 2;
+	double posx = (knobx + radius) - x; // x axis right -> left
+	double posy = (knoby + radius) - y; // y axis top -> bottom
+	double value;
+	if (!drag) {
+		if (state & GDK_CONTROL_MASK) {
+			last_y = 2e20;
+			return;
+		} else {
+			last_y = posy;
+		}
+	}
+	if (last_y < 1e20) { // in drag started with Control Key
+		const double scaling = 0.005;
+		double scal = (state & GDK_SHIFT_MASK ? scaling*0.1 : scaling);
+		value = (last_y - posy) * scal;
+		last_y = posy;
+		gtk_range_set_value(GTK_RANGE(widget), adj->value - value * (adj->upper - adj->lower));
+		return;
+	}
+
+	double angle = atan2(-posx, posy) + M_PI; // clockwise, zero at 6 o'clock, 0 .. 2*M_PI
+	if (drag) {
+		// block "forbidden zone" and direct moves between quadrant 1 and 4
+		int quadrant = 1 + int(angle/M_PI_2);
+		if (knob->last_quadrant == 1 && (quadrant == 3 || quadrant == 4)) {
+			angle = scale_zero;
+		} else if (knob->last_quadrant == 4 && (quadrant == 1 || quadrant == 2)) {
+			angle = 2*M_PI - scale_zero;
+		} else {
+			if (angle < scale_zero) {
+				angle = scale_zero;
+			} else if (angle > 2*M_PI - scale_zero) {
+				angle = 2*M_PI - scale_zero;
+			}
+			knob->last_quadrant = quadrant;
+		}
+	} else {
+		if (angle < scale_zero) {
+			angle = scale_zero;
+		} else if (angle > 2*M_PI - scale_zero) {
+			angle = 2*M_PI - scale_zero;
+		}
+		knob->last_quadrant = 0;
+	}
+	angle = (angle - scale_zero) / (2 * (M_PI-scale_zero)); // normalize to 0..1
+	gtk_range_set_value(GTK_RANGE(widget), adj->lower + angle * (adj->upper - adj->lower));
+}
+
+/****************************************************************
+ ** mouse button pressed set value
+ */
+
+static gboolean gtk_knob_button_press (GtkWidget *widget, GdkEventButton *event)
+{
+	g_assert(GTK_IS_KNOB(widget));
+	GtkKnob *knob = GTK_KNOB(widget);
+	GtkKnobClass *klass =  GTK_KNOB_CLASS(GTK_OBJECT_GET_CLASS(widget));
+	GtkAdjustment *adj = gtk_range_get_adjustment(GTK_RANGE(widget));
+	GtkWidget * dialog,* spinner, *ok_button, *vbox, *toplevel;
+
+	switch (event->button) {
+	case 1:  // left button
+		gtk_widget_grab_focus(widget);
+		gtk_widget_grab_default (widget);
+		gtk_grab_add(widget);
+		klass->button_is = 1;
+		knob_pointer_event(widget, event->x, event->y, klass->knob_x, klass->knob_y,
+						   false, event->state);
+		break;
+	case 2: //wheel
+		klass->button_is = 2;
+		break;
+	case 3:  // right button 
+		klass->button_is = 3;
+		break;
+	default: // do nothing
+		break;
+	}
+	return TRUE;
+}
+
+/****************************************************************
+ ** mouse button release
+ */
+
+static gboolean gtk_knob_button_release (GtkWidget *widget, GdkEventButton *event)
+{
+	g_assert(GTK_IS_KNOB(widget));
+	GTK_KNOB_CLASS(GTK_OBJECT_GET_CLASS(widget))->button_is = 0;
+	if (GTK_WIDGET_HAS_GRAB(widget))
+		gtk_grab_remove(widget);
+	return FALSE;
+}
+
+/****************************************************************
+ ** set the value from mouse movement
+ */
+
+static gboolean gtk_knob_pointer_motion (GtkWidget *widget, GdkEventMotion *event)
+{
+	g_assert(GTK_IS_KNOB(widget));
+	GtkKnob *knob = GTK_KNOB(widget);
+	GtkKnobClass *klass =  GTK_KNOB_CLASS(GTK_OBJECT_GET_CLASS(widget));
+	GtkAdjustment *adj = gtk_range_get_adjustment(GTK_RANGE(widget));
+	gdk_event_request_motions (event);
+	
+	if (GTK_WIDGET_HAS_GRAB(widget)) {
+		knob_pointer_event(widget, event->x, event->y, klass->knob_x, klass->knob_y,
+						   true, event->state);
+	}
+	return FALSE;
+}
+
+/****************************************************************
+ ** set value from mouseweel
+ */
+
+static gboolean gtk_knob_scroll (GtkWidget *widget, GdkEventScroll *event)
+{
+	usleep(5000);
+	gtk_knob_set_value(widget, event->direction);
+	return FALSE;
+}
+
+/****************************************************************
+ ** init the GtkKnobClass
+ */
+
+static void gtk_knob_class_init (GtkKnobClass *klass)
+{
+	GtkWidgetClass *widget_class = GTK_WIDGET_CLASS(klass);
+
+	/** set here the sizes and steps for the used knob **/
+//--------- small knob size and steps
+	
+	klass->knob_x = 30;
+	klass->knob_y = 30;
+	klass->knob_step = 86;
+
+//--------- event button
+	klass->button_is = 0;
+
+//--------- connect the events with funktions
+	widget_class->expose_event = gtk_knob_expose;
+	widget_class->size_request = gtk_knob_size_request;
+	widget_class->button_press_event = gtk_knob_button_press;
+	widget_class->button_release_event = gtk_knob_button_release;
+	widget_class->motion_notify_event = gtk_knob_pointer_motion;
+	widget_class->key_press_event = gtk_knob_key_press;
+	widget_class->scroll_event = gtk_knob_scroll;
+}
+
+/****************************************************************
+ ** init the Knob type/size
+ */
+
+static void gtk_knob_init (GtkKnob *knob)
+{
+	GtkWidget *widget = GTK_WIDGET(knob);
+	GtkKnobClass *klass =  GTK_KNOB_CLASS(GTK_OBJECT_GET_CLASS(widget));
+
+	GTK_WIDGET_SET_FLAGS (GTK_WIDGET(knob), GTK_CAN_FOCUS);
+	GTK_WIDGET_SET_FLAGS (GTK_WIDGET(knob), GTK_CAN_DEFAULT);
+
+	widget->requisition.width = klass->knob_x;
+	widget->requisition.height = klass->knob_y;
+
+}
+
+/****************************************************************
+ ** redraw when value changed
+ */
+
+static gboolean gtk_knob_value_changed(gpointer obj)
+{
+	GtkWidget *widget = (GtkWidget *)obj;
+	gtk_widget_queue_draw(widget);
+	return FALSE;
+}
+
+/****************************************************************
+ ** create small knob
+ */
+
+GtkWidget *GtkKnob::gtk_knob_new_with_adjustment(GtkAdjustment *_adjustment)
+{
+	GtkWidget *widget = GTK_WIDGET( g_object_new (GTK_TYPE_KNOB, NULL ));
+	GtkKnob *knob = GTK_KNOB(widget);
+	knob->last_quadrant = 0;
+	if (widget) {
+		gtk_range_set_adjustment(GTK_RANGE(widget), _adjustment);
+		g_signal_connect(GTK_OBJECT(widget), "value-changed",
+		                 G_CALLBACK(gtk_knob_value_changed), widget);
+	}
+	return widget;
+}
+
+/****************************************************************
+ ** get the Knob type
+ */
+
+GType gtk_knob_get_type (void)
+{
+	static GType kn_type = 0;
+	if (!kn_type) {
+		static const GTypeInfo kn_info = {
+			sizeof(GtkKnobClass), NULL,  NULL, (GClassInitFunc)gtk_knob_class_init, NULL, NULL, sizeof (GtkKnob), 0, (GInstanceInitFunc)gtk_knob_init
+		};
+		kn_type = g_type_register_static(GTK_TYPE_RANGE,  "GtkKnob", &kn_info, (GTypeFlags)0);
+	}
+	return kn_type;
+}
+
+GtkKnob myGtkKnob;
 
 /**
  * rmWhiteSpaces(): Remove the leading and trailing white spaces of a string
@@ -440,6 +869,9 @@ class GTKUI : public UI
     static list<UI*>                    fGuiList;
     static map<float*, float>           fGuiSize;       // map widget zone with widget size coef
     static map<float*, string>          fTooltip;       // map widget zone with tooltip strings
+    static set<float*>                  fKnobSet;       // set of widget zone to be knobs
+    
+    bool isKnob(float* zone){return fKnobSet.count(zone) > 0;}
     
  protected :
     GtkWidget*  fWindow;
@@ -453,7 +885,6 @@ class GTKUI : public UI
 
         
  public :
-    
     static const gboolean expand = TRUE;
     static const gboolean fill = TRUE;
     static const gboolean homogene = FALSE;
@@ -522,6 +953,7 @@ list<UI*>                   UI::fGuiList;
 bool                        GTKUI::fInitialized = false;
 map<float*, float>          GTKUI::fGuiSize;
 map<float*, string>         GTKUI::fTooltip;
+set<float*>                 GTKUI::fKnobSet;       // set of widget zone to be knobs
 
 /**
 * Format tooltip string by replacing some white spaces by 
@@ -622,6 +1054,11 @@ void GTKUI::declare(float* zone, const char* key, const char* value)
     }
     else if (strcmp(key,"tooltip")==0) {
         fTooltip[zone] = formatTooltip(30,value) ;
+    }
+    else if (strcmp(key,"style")==0) {
+            if (strcmp(value,"knob") == 0) {
+				fKnobSet.insert(zone);
+            }
     }
 }
         
@@ -1006,13 +1443,45 @@ struct uiAdjustment : public uiItem
     }
 };
 
-static int precision(double n)
+// --------------------------- format knob value display ---------------------------
+
+struct uiValueDisplay : public uiItem
 {
-    if (n < 0.009999) return 3;
-    else if (n < 0.099999) return 2;
-    else if (n < 0.999999) return 1;
-    else return 0;
-}
+	GtkLabel* fLabel;
+	int	fPrecision ;
+
+	uiValueDisplay(UI* ui, float* zone, GtkLabel* label, int precision)
+		: uiItem(ui, zone), fLabel(label), fPrecision(precision) {}
+
+	virtual void reflectZone()
+		{
+			float v = *fZone;
+			fCache = v;
+			char s[64];
+			if (fPrecision <= 0)
+				snprintf(s, 63, "%d", int(v));
+
+			else if (fPrecision > 3)
+				snprintf(s, 63, "%f", v);
+
+			else if (fPrecision == 1)
+			{
+				const char* format[] = {"%.1f", "%.2f", "%.3f"};
+				snprintf(s, 63, format[1-1], v);
+			}
+			else if (fPrecision == 2)
+			{
+				const char* format[] = {"%.1f", "%.2f", "%.3f"};
+				snprintf(s, 63, format[2-1], v);
+			}
+			else
+			{
+				const char* format[] = {"%.1f", "%.2f", "%.3f"};
+				snprintf(s, 63, format[3-1], v);
+			}
+			gtk_label_set_text(fLabel, s);
+		}
+};
 
 // -------------------------- Vertical Slider -----------------------------------
 
@@ -1025,12 +1494,23 @@ void GTKUI::addVerticalSlider(const char* label, float* zone, float init, float 
 
     gtk_signal_connect (GTK_OBJECT (adj), "value-changed", GTK_SIGNAL_FUNC (uiAdjustment::changed), (gpointer) c);
     
-    GtkWidget* slider = gtk_vscale_new (GTK_ADJUSTMENT(adj));
+    GtkWidget* slider;
+    if (isKnob(zone)) { 
+		slider = gtk_vbox_new (FALSE, 0);
+		GtkWidget* re =myGtkKnob.gtk_knob_new_with_adjustment(GTK_ADJUSTMENT(adj));
+		GtkWidget* lw = gtk_label_new("");
+		new uiValueDisplay(this, zone, GTK_LABEL(lw),precision(step));
+		gtk_container_add (GTK_CONTAINER(slider), re);
+		gtk_container_add (GTK_CONTAINER(slider), lw);
+		gtk_widget_show_all(slider);
+	} else {
+		slider = gtk_vscale_new (GTK_ADJUSTMENT(adj));
+		gtk_scale_set_digits(GTK_SCALE(slider), precision(step));
+		float size = 160 * pow(2, fGuiSize[zone]);
+		gtk_widget_set_usize(slider, -1, size);
+	}
     gtk_range_set_inverted (GTK_RANGE(slider), TRUE);
-    gtk_scale_set_digits(GTK_SCALE(slider), precision(step));
-    float size = 160 * pow(2, fGuiSize[zone]);
-    gtk_widget_set_usize(slider, -1, size);
-
+    
     if (label && label[0]!=0) {
         openFrameBox(label);
         addWidget(label, slider);
@@ -1053,10 +1533,21 @@ void GTKUI::addHorizontalSlider(const char* label, float* zone, float init, floa
 
     gtk_signal_connect (GTK_OBJECT (adj), "value-changed", GTK_SIGNAL_FUNC (uiAdjustment::changed), (gpointer) c);
     
-    GtkWidget* slider = gtk_hscale_new (GTK_ADJUSTMENT(adj));
-    gtk_scale_set_digits(GTK_SCALE(slider), precision(step));
-    float size = 160 * pow(2, fGuiSize[zone]);
-    gtk_widget_set_usize(slider, size, -1);
+    GtkWidget* slider;
+    if (isKnob(zone)) { 
+		slider = gtk_vbox_new (FALSE, 0);
+		GtkWidget* re =myGtkKnob.gtk_knob_new_with_adjustment(GTK_ADJUSTMENT(adj));
+		GtkWidget* lw = gtk_label_new("");
+		new uiValueDisplay(this, zone, GTK_LABEL(lw),precision(step));
+		gtk_container_add (GTK_CONTAINER(slider), re);
+		gtk_container_add (GTK_CONTAINER(slider), lw);
+		gtk_widget_show_all(slider);
+	} else {
+		slider= gtk_hscale_new (GTK_ADJUSTMENT(adj));
+		gtk_scale_set_digits(GTK_SCALE(slider), precision(step));
+		float size = 160 * pow(2, fGuiSize[zone]);
+		gtk_widget_set_size_request(slider, size, -1);
+  	}  
     
     if (label && label[0]!=0) {
         openFrameBox(label);
@@ -1081,9 +1572,19 @@ void GTKUI::addNumEntry(const char* label, float* zone, float init, float min, f
 
     gtk_signal_connect (GTK_OBJECT (adj), "value-changed", GTK_SIGNAL_FUNC (uiAdjustment::changed), (gpointer) c);
     
-    GtkWidget* spinner = gtk_spin_button_new (GTK_ADJUSTMENT(adj), 0.005, precision(step));
+    GtkWidget* spinner;
+    if (isKnob(zone)) { 
+		spinner = gtk_vbox_new (FALSE, 0);
+		GtkWidget* re =myGtkKnob.gtk_knob_new_with_adjustment(GTK_ADJUSTMENT(adj));
+		GtkWidget* lw = gtk_label_new("");
+		new uiValueDisplay(this, zone, GTK_LABEL(lw),precision(step));
+		gtk_container_add (GTK_CONTAINER(spinner), re);
+		gtk_container_add (GTK_CONTAINER(spinner), lw);
+		gtk_widget_show_all(spinner);
+	} else {
+		spinner = gtk_spin_button_new (GTK_ADJUSTMENT(adj), 0.005, precision(step));
+	}
 
-    //gtk_widget_set_usize(slider, 160, -1);
     openFrameBox(label);
     addWidget(label, spinner);
     closeBox();
