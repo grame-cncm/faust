d3dd14e4e8467e187aa5d99d8dc1e1868021bbaf
Yann Orlarey <orlarey@grame.fr>
2011-01-04 13:13:54 +0100

    License information added to most architecture files and libraries

--- architecture/bench.cpp
+++ architecture/bench.cpp
@@ -1,5 +1,24 @@
-//#include "../bench/lmbench.h"
-// derived from bench.cpp to test parallel processing
+/************************************************************************
+ ************************************************************************
+    FAUST Architecture File
+	Copyright (C) 2003-2011 GRAME, Centre National de Creation Musicale
+    ---------------------------------------------------------------------
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as 
+	published by the Free Software Foundation; either version 2.1 of the 
+	License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+ 	License along with the GNU C Library; if not, write to the Free
+  	Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+  	02111-1307 USA. 
+ ************************************************************************
+ ************************************************************************/
 
 
 /* link with  */
--- architecture/pure.cpp
+++ architecture/pure.cpp
@@ -1,14 +1,30 @@
+/************************************************************************
+ ************************************************************************
+    FAUST Architecture File
+	Copyright (C) 2009-2011 Albert Graef <Dr.Graef@t-online.de>
+    ---------------------------------------------------------------------
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as 
+	published by the Free Software Foundation; either version 2.1 of the 
+	License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+ 	License along with the GNU C Library; if not, write to the Free
+  	Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+  	02111-1307 USA. 
+ ************************************************************************
+ ************************************************************************/
+
 /* Pure architecture for Faust. This is similar to the Q architecture, but
    uses double precision for the audio buffers and control variables. See
    http://pure-lang.googlecode.com for a Pure module which can load these
    extensions. */
 
-/* Copyright (c) 2009 Albert Graef
-
-   Copying and distribution of this file, with or without modification,
-   are permitted in any medium without royalty provided the copyright
-   notice and this notice are preserved.  This file is offered as-is,
-   without any warranty. */
 
 #include <stdlib.h>
 #include <math.h>
