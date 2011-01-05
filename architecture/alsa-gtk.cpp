9bad75ddc7e766d6fbb5fdd4e93d52b872747b0f
Yann Orlarey <orlarey@grame.fr>
2011-01-05 15:01:41 +0100

    GPL3 and more explicit Faust compiler exception

--- architecture/alsa-gtk.cpp
+++ architecture/alsa-gtk.cpp
@@ -4,19 +4,25 @@
 	Copyright (C) 2003-2011 GRAME, Centre National de Creation Musicale
     ---------------------------------------------------------------------
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as 
-	published by the Free Software Foundation; either version 2.1 of the 
-	License, or (at your option) any later version.
+    it under the terms of the GNU General Public License as published by 
+	the Free Software Foundation; either version 3 of the License, or 
+	(at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+    GNU General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
- 	License along with the GNU C Library; if not, write to the Free
-  	Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-  	02111-1307 USA. 
+    You should have received a copy of the GNU General Public License 
+	along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+	FAUST COMPILER EXCEPTION : the purpose of this Exception is to allow 
+	the use of this architecture file to produce of non-GPL (including 
+	proprietary) code. By this Exception the copyright holder of this file
+	allows the FAUST compiler (and only the FAUST compiler) to remove this
+	license information from the generated code, therefore producing 
+	"free of license information" code.
+
  ************************************************************************
  ************************************************************************/
 
