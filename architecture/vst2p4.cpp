a63377746ffd3f0fa3be084d571a94812c86d7f0
Yann Orlarey <orlarey@grame.fr>
2011-01-18 19:29:55 +0100

    Text of simplified BSD license added

--- architecture/vst2p4.cpp
+++ architecture/vst2p4.cpp
@@ -12,18 +12,40 @@
  ************************************************************************
     FAUST Architecture File
 	Copyright (C) 2007-2011 Remy Muller & Julius Smith
-    ---------------------------------------------------------------------
-    This Architecture section is free software; you can redistribute it 
-    and/or modify it under the terms of the 3-clause BSD license.
+	All rights reserved.
+    ----------------------------BSD License------------------------------
+	Redistribution and use in source and binary forms, with or without 
+	modification, are permitted provided that the following conditions 
+	are met:
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    3-clause BSD license for more details.
+    	* Redistributions of source code must retain the above copyright 
+		  notice, this list of conditions and the following disclaimer.
+    	* Redistributions in binary form must reproduce the above 
+		  copyright notice, this list of conditions and the following 
+		  disclaimer in the documentation and/or other materials provided 
+		  with the distribution.
+    	* Neither the name of Remy Muller or Julius Smith or of its 
+		  contributors may be used to endorse or promote products derived 
+		  from this software without specific prior written permission.
 
-	VST SDK : in order to compile a VST (TM) plugin with this 
-	architecture file you will need the official VST SDK from
-	Steinberg. Please check the corresponding license.
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+	COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+	STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+	OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    ----------------------------VST SDK----------------------------------
+	In order to compile a VST (TM) plugin with this architecture file 
+	you will need the proprietary VST SDK from Steinberg. Please check 
+	the corresponding license.
+
  ************************************************************************
  ************************************************************************/
 
