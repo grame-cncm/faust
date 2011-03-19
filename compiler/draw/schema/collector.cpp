323b61989361fc9c3df7e0f61c36aa8d2e87f983
Yann Orlarey <orlarey@grame.fr>
2011-03-19 18:43:13 +0100

    commented out code removed

--- compiler/draw/schema/collector.cpp
+++ compiler/draw/schema/collector.cpp
@@ -39,22 +39,8 @@ void collector::computeVisibleTraits()
                     fOutputs.insert(p->end);        // end become a real output too
                     modified = true;
                 }
-#if 0
-                if (fOutputs.count(p->end) > 0) {
-                    fWithInput.insert(*p);          // the cable is connected to a real output
-                    fOutputs.insert(p->start);      // start become a real output too
-                    modified = true;
-                }
-#endif
             }
             if (fWithOutput.count(*p) == 0) {       // not connected to a real input
-#if 0
-                if (fInputs.count(p->start) > 0) {
-                    fWithOutput.insert(*p);         // the cable is connected to a real input
-                    fInputs.insert(p->end);         // end become a real input too
-                    modified = true;
-                }
-#endif
                 if (fInputs.count(p->end) > 0) {
                     fWithOutput.insert(*p);         // the cable is connected to a real input
                     fInputs.insert(p->start);       // start become a real input too
