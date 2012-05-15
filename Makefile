8155ce22737a0d1ddd8eae3f051e45302cfd6d37
Yann Orlarey <yannorlarey@YannAir.local>
2012-05-15 22:42:39 +0200

    Version number is now 0.9.51

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-version := 0.9.50
+version := 0.9.51
 
 DESTDIR ?= 
 PREFIX ?= /usr/local
--- compiler/main.cpp
+++ compiler/main.cpp
@@ -18,7 +18,7 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************
  ************************************************************************/
-#define FAUSTVERSION "0.9.50"
+#define FAUSTVERSION "0.9.51"
 
 #include <stdio.h>
 #include <string.h>
