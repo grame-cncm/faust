6dc13de81584f4513f9a18ff137a9c5e4967e04e
Stephane Letz <letz@grame.fr>
2013-05-06 11:49:18 +0200

    Typo.

--- compiler/tlib/compatibility.hh
+++ compiler/tlib/compatibility.hh
@@ -28,7 +28,7 @@
 #define snprintf _snprintf
 
 int http_fetch(const char *url, char **fileBuf) { return -1; }
-const char *http_strerror(); { return NULL; }
+const char *http_strerror() { return NULL; }
 
 #ifdef WIN32
 	#undef min
