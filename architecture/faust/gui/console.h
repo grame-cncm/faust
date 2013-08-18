05ff97685df3f914469df47fa88001b1ca6382d1
Julius Smith <jos@ccrma.stanford.edu>
2013-08-17 20:37:09 -0700

    needed \n after printed input and output filenames in console.h

--- architecture/faust/gui/console.h
+++ architecture/faust/gui/console.h
@@ -256,8 +256,8 @@ public:
 	int 	files()         { return fFiles.size(); }
 	char* 	file (int n)	{ return fFiles[n]; }
 
-	char* input_file ()     { std::cout << "input file " << fFiles[0]; return fFiles[0]; }
-	char* output_file() 	{ std::cout << "output file " << fFiles[1]; return fFiles[1]; }
+	char* input_file ()     { std::cout << "input file " << fFiles[0] << "\n"; return fFiles[0]; }
+	char* output_file() 	{ std::cout << "output file " << fFiles[1] << "\n"; return fFiles[1]; }
 
 	void process_init()
 	{
