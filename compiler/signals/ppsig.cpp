e3fee372b357e630af4059008dc2e496f951f01e
Yann Orlarey <orlarey@grame.fr>
2013-11-25 16:12:11 +0100

    Prettyprint floats using optimal precision algorithm of T()

--- compiler/signals/ppsig.cpp
+++ compiler/signals/ppsig.cpp
@@ -21,6 +21,7 @@
 
 
 
+#include "Text.hh"
 #include "ppsig.hh"
 #include "binop.hh"
 #include "prim2.hh"
@@ -191,7 +192,7 @@ ostream& ppsig::print (ostream& fout) const
 	
 	else if ( getUserData(sig) ) 					{ printextended(fout, sig); }
 	else if ( isSigInt(sig, &i) ) 					{ fout << i; }
-    else if ( isSigReal(sig, &r) ) 					{ fout << r; }
+    else if ( isSigReal(sig, &r) ) 					{ fout << T(r); }
     else if ( isSigWaveform(sig) )                  { fout << "waveform{...}"; }
     else if ( isSigInput(sig, &i) ) 				{ fout << "IN[" << i << "]"; }
 	else if ( isSigOutput(sig, &i, x) ) 			{ printout(fout, i, x) ; }
