4fde2f93af275163fb3ba1bcce64ba8543ffeac3
Stephane Letz <letz@grame.fr>
2013-12-05 17:19:44 +0100

    Correct ControlUI.h.

--- architecture/faust/gui/ControlUI.h
+++ architecture/faust/gui/ControlUI.h
@@ -9,7 +9,8 @@ class ControlUI  : public UI {
 
     protected:
     
-        std::vector<FAUSTFLOAT*> fControlList;
+        std::vector<FAUSTFLOAT*> fControlIn;
+        std::vector<FAUSTFLOAT*> fControlOut;
     
         // -- widget's layouts
 
@@ -20,36 +21,36 @@ class ControlUI  : public UI {
 
         // -- active widgets
 
-        void addButton(const char* label, FAUSTFLOAT* zone) { fControlList.push_back(zone); }
-        void addCheckButton(const char* label, FAUSTFLOAT* zone) { fControlList.push_back(zone); }
-        void addVerticalSlider(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step) { fControlList.push_back(zone); };
-        void addHorizontalSlider(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step) { fControlList.push_back(zone); };
-        void addNumEntry(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step) { fControlList.push_back(zone); };
+        void addButton(const char* label, FAUSTFLOAT* zone) { fControlIn.push_back(zone); }
+        void addCheckButton(const char* label, FAUSTFLOAT* zone) { fControlIn.push_back(zone); }
+        void addVerticalSlider(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step) { fControlIn.push_back(zone); };
+        void addHorizontalSlider(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step) { fControlIn.push_back(zone); };
+        void addNumEntry(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step) { fControlIn.push_back(zone); };
 
         // -- passive widgets
 
-        void addHorizontalBargraph(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max) { fControlList.push_back(zone); };
-        void addVerticalBargraph(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max) { fControlList.push_back(zone); };
+        void addHorizontalBargraph(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max) { fControlOut.push_back(zone); };
+        void addVerticalBargraph(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max) { fControlOut.push_back(zone); };
         
     public:
        
         void encode_control(float* control_buffer, unsigned int size)
         {
-            assert(fControlList.size() < size);
+            assert(fControlOut.size() < size);
             // Encode control values in control_buffer
             int control_index = 0;
-            for (unsigned int i = 0; i < fControlList.size(); i++) {
-                control_buffer[control_index++] = *fControlList[i];
+            for (unsigned int i = 0; i < fControlOut.size(); i++) {
+                control_buffer[control_index++] = *fControlOut[i];
             };
         }
 
         void decode_control(float* control_buffer, unsigned int size)
         {
-            assert(fControlList.size() < size);
+            assert(fControlIn.size() < size);
             // Decode control values from control_buffer
             int control_index = 0;
-            for (unsigned int i = 0; i < fControlList.size(); i++) {
-               *fControlList[i] = control_buffer[control_index++];
+            for (unsigned int i = 0; i < fControlIn.size(); i++) {
+               *fControlIn[i] = control_buffer[control_index++];
             };
         }
 };
