2ec2b6830ccd194ab552a7784b12c7e8d3f45a98
Stephane Letz <letz@grame.fr>
2014-02-19 21:42:21 +0100

    Add --help in RemoteServer.

--- embedded/faustremote/RemoteServer/main.cpp
+++ embedded/faustremote/RemoteServer/main.cpp
@@ -18,8 +18,11 @@ int main(int argc, char* argv[])
 {
     int port = 7777;
     
-    if(argv[1] && !strcmp(argv[1],"--port"))
+    if(argv[1] && strcmp(argv[1],"--port") == 0) {
         port = atoi(argv[2]);
+    } else if(argv[1] && strcmp(argv[1],"--help") == 0) {
+        printf("Remoteserver --port XXX (default port is 7777) \n");
+    }
     
     Server server;
     
