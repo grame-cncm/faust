ee5da2be82800f30d97b3d18507fbd3b49120a70
Stephane Letz <letz@grame.fr>
2015-04-15 21:54:31 +0200

    Add licences.

--- architecture/faust/midi/midi-io.h
+++ architecture/faust/midi/midi-io.h
@@ -1,34 +1,51 @@
-//*****************************************//
-//  cmidiin.cpp
-//  by Gary Scavone, 2003-2004.
-//
-//  Simple program to test MIDI input and
-//  use of a user callback function.
-//
-//*****************************************//
+/************************************************************************
+    FAUST Architecture File
+    Copyright (C) 2003-2011 GRAME, Centre National de Creation Musicale
+    ---------------------------------------------------------------------
+    This Architecture section is free software; you can redistribute it
+    and/or modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 3 of
+    the License, or (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+    EXCEPTION : As a special exception, you may create a larger work
+    that contains this FAUST architecture section and distribute
+    that work under terms of your choice, so long as this FAUST
+    architecture section is not modified.
+
+
+ ************************************************************************
+ ************************************************************************/
+ 
 #include <iostream>
 #include <cstdlib>
 #include "faust/midi/RtMidi.cpp"
 #include "faust/midi/midi.h"
 
-enum MidiStatus {
-	
-    // channel voice messages
-    MIDI_NOTE_OFF           = 0x80,
-    MIDI_NOTE_ON            = 0x90,
-    MIDI_CONTROL_CHANGE     = 0xB0,
-    MIDI_PROGRAM_CHANGE     = 0xC0,
-    MIDI_PITCH_BEND         = 0xE0,
-    MIDI_AFTERTOUCH         = 0xD0,	// aka channel pressure
-    MIDI_POLY_AFTERTOUCH    = 0xA0	// aka key pressure
-
-};
-
 class MidiIO : public midi {
 
     private:
     
+        enum MidiStatus {
+	
+            // channel voice messages
+            MIDI_NOTE_OFF           = 0x80,
+            MIDI_NOTE_ON            = 0x90,
+            MIDI_CONTROL_CHANGE     = 0xB0,
+            MIDI_PROGRAM_CHANGE     = 0xC0,
+            MIDI_PITCH_BEND         = 0xE0,
+            MIDI_AFTERTOUCH         = 0xD0,	// aka channel pressure
+            MIDI_POLY_AFTERTOUCH    = 0xA0	// aka key pressure
+
+        };
+  
         RtMidiIn* fInput;
         RtMidiOut* fOutput;
         vector<midi*> fMidiInputs;
