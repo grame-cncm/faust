4cac36e7fc493158e92946b3a840c4f611ac2844
Stephane Letz <letz@grame.fr>
2015-06-02 03:47:37 +0200

    Add conditional NAN definition for Windows.

--- architecture/max-msp/max-msp64.cpp
+++ architecture/max-msp/max-msp64.cpp
@@ -61,6 +61,13 @@
 #include <unistd.h>
 #endif
 
+#ifdef WIN32
+#ifndef NAN
+    static const unsigned long __nan[2] = {0xffffffff, 0x7fffffff};
+    #define NAN (*(const float *) __nan)
+#endif
+#endif
+
 #define FAUSTFLOAT double
 
 #include "faust/gui/UI.h"
