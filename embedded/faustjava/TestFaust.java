30249849954837d86e627efbc50fb1be331c1be3
Stephane Letz <letz@grame.fr>
2015-06-15 13:33:04 +0200

    faustjava : test -I option.

--- embedded/faustjava/TestFaust.java
+++ embedded/faustjava/TestFaust.java
@@ -20,8 +20,11 @@ class TestFaust
 		System.out.println("Test libfaust");
         
         // noise generator
-        String prog = "random = +(12345)~*(1103515245); noise = random/2147483647.0; process = noise * vslider(\"Volume\", 0.5, 0, 1, 0.01)<:_,_;";
-        String argv = "-vec " + "-lv " + "1";
+        String prog = "random = +(12345)~*(1103515245); noise1 = random/2147483647.0; process = noise1 * vslider(\"Volume\", 0.5, 0, 1, 0.01)<:_,_;";
+        
+        String argv = "-vec " + "-lv " + "1 ";
+        //String argv = "-vec " + "-lv " + "1 " + "-I " + "/Documents/faust-sf/embedded/faustjava/test_lib/";
+        
         System.out.println(argv);
         
         llvm_dsp_factory factory = Faust.createCDSPFactoryFromStringAux("noise", prog, argv, "", 3);
