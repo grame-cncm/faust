b5bba7025c7452af726d16d645a6f82237376956
Stephane Letz <letz@grame.fr>
2015-10-07 15:55:23 +0200

    Android: correct exception handing in OSC code.

--- architecture/android/app/src/main/java/com/illposed/osc/OSCPortIn.java
+++ architecture/android/app/src/main/java/com/illposed/osc/OSCPortIn.java
@@ -112,9 +112,12 @@ public class OSCPortIn extends OSCPort implements Runnable {
 						continue;
 					}
 				}
-				final OSCPacket oscPacket = converter.convert(buffer,
-						packet.getLength());
-				dispatcher.dispatchPacket(oscPacket);
+                try {
+                    final OSCPacket oscPacket = converter.convert(buffer, packet.getLength());
+                    dispatcher.dispatchPacket(oscPacket);
+                } catch (IllegalArgumentException ex) {
+                    ex.printStackTrace();
+                }
 			} catch (IOException ex) {
 				ex.printStackTrace(); // XXX This may not be a good idea, as this could easily lead to a never ending series of exceptions thrown (due to the non-exited while loop), and because the user of the lib may want to handle this case himself
 			}
