fef7972d0ec6095a79a79fcd52646b00f97d8267
Stephane Letz <letz@grame.fr>
2015-10-15 13:30:48 +0200

    Cleanup.
    Former-commit-id: b0136b6b7b35abda2854493039aeda9a166812b0

--- compiler/generator/java/java_code_container.hh
+++ compiler/generator/java/java_code_container.hh
@@ -52,8 +52,6 @@ class JAVACodeContainer : public virtual CodeContainer {
         virtual void generateCompute(int tab) = 0;
         void produceInternal();
 
-        void printIncludeFile(ostream& fout);
-
         CodeContainer* createScalarContainer(const string& name, int sub_container_type);
 
         static CodeContainer* createContainer(const string& name, const string& super, int numInputs, int numOutputs, ostream* dst);
