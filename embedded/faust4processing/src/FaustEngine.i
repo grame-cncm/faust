484bfb730335bf830da15e3c07985e7ea0d292b2
Stephane Letz <letz@grame.fr>
2016-01-08 18:02:31 +0100

    Public header renaming.
    Former-commit-id: a1edcf8d94202dc6bef33393c32f5f75f1d00358

--- embedded/faust4processing/src/FaustEngine.i
+++ embedded/faust4processing/src/FaustEngine.i
@@ -2,9 +2,9 @@
 %module FaustEngine
 
 %{
-#include "../../../architecture/faust/dsp/FaustEngine.h"
+#include "../../../architecture/faust/dsp/faust-engine.h"
 %}
 
 /* Let's just grab the original header file here */
-%include "../../../architecture/faust/dsp/FaustEngine.h"
+%include "../../../architecture/faust/dsp/faust-engine.h"
 
--- embedded/faustjava/FaustEngine_wrap.cxx
+++ embedded/faustjava/FaustEngine_wrap.cxx
@@ -211,7 +211,7 @@ static void SWIGUNUSED SWIG_JavaThrowException(JNIEnv *jenv, SWIG_JavaExceptionC
 #define SWIG_contract_assert(nullreturn, expr, msg) if (!(expr)) {SWIG_JavaThrowException(jenv, SWIG_JavaIllegalArgumentException, msg); return nullreturn; } else
 
 
-#include "../../architecture/faust/dsp/FaustEngine.h"
+#include "../../architecture/faust/dsp/faust-engine.h"
 
 
 #ifdef __cplusplus
