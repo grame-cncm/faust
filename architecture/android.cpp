2a06a759c07184acd8dbd874f6fb51a03d0152c4
Stephane Letz <letz@grame.fr>
2016-01-11 12:24:20 +0100

    Creates faust-poly-engine.cpp to share polyphonic DSP implementation.

--- architecture/android.cpp
+++ architecture/android.cpp
@@ -17,20 +17,10 @@
  ************************************************************************
  ************************************************************************/
 
-/*
- * README:
- * The file only implements the native part of faust2android applications.
- * The native C API is documented at the end of this file in the "Native Faust
- * API" section.
- */
-
-#include <math.h>
-#include "faust/misc.h"
+#include "faust/gui/UI.h"
 #include "faust/audio/dsp.h"
+#include "faust/audio/audio.h"
 #include "faust/gui/meta.h"
-#include "faust/gui/jsonfaustui.h"
-#include "faust/gui/JSONUI.h"
-#include "faust/gui/APIUI.h"
 
 //**************************************************************
 // DSP class
@@ -40,11 +30,14 @@
 
 <<includeclass>>
 
-//**************************************************************
-// Polyphony
-//**************************************************************
+/*
+ * README:
+ * The file only implements the native part of faust2android applications.
+ * The native C API is documented at the end of this file in the "Native Faust
+ * API" section.
+ */
 
-#include "faust/dsp/poly-dsp.h"
+#include "faust/dsp/faust-poly-engine.cpp"
 
 //**************************************************************
 // Android Audio
@@ -57,190 +50,7 @@
 //**************************************************************
 
 #include <android/log.h>
-#include <stdio.h>
-#include <string.h>
 #include "dsp_faust.h"
-
-using namespace std;
-
-class FaustPolyEngine {
-    
-protected:
-
-    mydsp fMonoDSP;           // the monophonic Faust object
-    mydsp_poly* fPolyDSP;     // the polyphonic Faust object
-    APIUI fAPIUI;             // the UI description
-    JSONUI fJSON;
-    string fJSONString;
-    bool fRunning;
-    int fPolyMax;
-    audio* fDriver;
-    
-public:
-
-    FaustPolyEngine(int sampling_rate, int buffer_size)
-    :fJSON(fMonoDSP.getNumInputs(), fMonoDSP.getNumOutputs()), fRunning(false)
-    {
-        // configuring the UI
-        fMonoDSP.buildUserInterface(&fAPIUI);
-        fMonoDSP.buildUserInterface(&fJSON);
-        fJSONString = fJSON.JSON();
-
-        if (fJSONString.find("keyboard") != std::string::npos ||
-            fJSONString.find("poly") != std::string::npos){
-            fPolyMax = 6;
-            fPolyDSP = new mydsp_poly(buffer_size, fPolyMax);
-            fPolyDSP->init(sampling_rate);
-        } else {
-            fPolyMax = 0;
-            fPolyDSP = NULL;
-       }
-    }
-
-    virtual ~FaustPolyEngine()
-    {
-        delete fPolyDSP;
-        delete fDriver;
-    }
-
-    bool init()
-    {
-        return fDriver->init("Dummy", (fPolyMax == 0) ? &fMonoDSP : (dsp*)fPolyDSP);
-    }
-
-    bool start()
-    {
-        if (!fRunning) {
-            __android_log_print(ANDROID_LOG_ERROR, "Faust", "REAL start");
-            fRunning = fDriver->start();
-        }
-        return fRunning;
-    }
-
-    void stop()
-    {
-        if (fRunning) {
-            fRunning = false;
-            __android_log_print(ANDROID_LOG_ERROR, "Faust", "REAL stop");
-            fDriver->stop();
-        }
-    }
-
-    int keyOn(int pitch, int velocity)
-    {
-        if (fPolyMax > 0) {
-            fPolyDSP->keyOn(0, pitch, velocity);
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    int keyOff(int pitch)
-    {
-        if (fPolyMax > 0) {
-            fPolyDSP->keyOff(0, pitch);
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    int pitchBend(int refPitch, float pitch)
-    {
-        if (fPolyMax > 0) {
-            fPolyDSP->pitchBend(0, refPitch, pitch);
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    const char* getJSON()
-    {
-        return fJSONString.c_str();
-    }
-
-    int getParamsCount()
-    {
-        return fAPIUI.getParamsCount();
-    }
-
-    float getParam(const char* address)
-    {
-        if (fPolyMax > 0) {
-            return fPolyDSP->getValue(address);
-         } else {
-            return fAPIUI.getParamValue(fAPIUI.getParamIndex(address));
-        }
-    }
-
-    void setParam(const char* address, float value)
-    {
-        if (fPolyMax > 0) {
-            fPolyDSP->setValue(address, value);
-        } else {
-            fAPIUI.setParamValue(fAPIUI.getParamIndex(address), value);
-        }
-    }
-
-    int setVoiceParam(const char* address, int pitch, float value)
-    {
-        if (fPolyMax > 0) {
-            fPolyDSP->setValue(address, pitch, value);
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    int setVoiceGain(int pitch, float gain)
-    {
-        if (fPolyMax > 0) {
-            fPolyDSP->setVoiceGain(pitch, gain);
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-
-    const char* getParamAddress(int id)
-    {
-        return fAPIUI.getParamName(id);
-    }
-
-    void propagateAcc(int acc, float v)
-    {
-        fAPIUI.propagateAcc(acc, v);
-    }
-
-    void setAccConverter(int p, int acc, int curve, float amin, float amid, float amax)
-    {
-        __android_log_print(ANDROID_LOG_ERROR, "Faust", "setAccConverter %d %d %d %f %f %f", p, acc, curve, amin, amid, amax);
-        fAPIUI.setAccConverter(p, acc, curve, amin, amid, amax);
-    }
-
-    void propagateGyr(int gyr, float v)
-    {
-        fAPIUI.propagateGyr(gyr, v);
-    }
-
-    void setGyrConverter(int p, int gyr, int curve, float amin, float amid, float amax)
-    {
-        __android_log_print(ANDROID_LOG_ERROR, "Faust", "setGyrConverter %d %d %d %f %f %f", p, gyr, curve, amin, amid, amax);
-        fAPIUI.setGyrConverter(p, gyr, curve, amin, amid, amax);
-    }
-
-    float getCPULoad() { return fDriver->get_cpu_load(); }
-
-    int getScreenColor()
-    {
-        int c = fAPIUI.getScreenColor();
-        __android_log_print(ANDROID_LOG_ERROR, "Faust", "getScreenColor() = %d", c);
-        return c;
-    }
-
-};
 
 class AndroidEngine : public FaustPolyEngine {
 
