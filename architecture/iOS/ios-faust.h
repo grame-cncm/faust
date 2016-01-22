e3592cb84bb12bea8e6ce4af4ced457830c0c43b
Stephane Letz <letz@grame.fr>
2016-01-22 18:25:51 +0100

    Add default ios-faust.h in iOS project.

---
+++ architecture/iOS/ios-faust.h
@@ -0,0 +1,189 @@
+/* ------------------------------------------------------------
+author: "Grame"
+copyright: "(c)GRAME 2009"
+license: "BSD"
+name: "Noise"
+version: "1.1"
+Code generated with Faust 2.0.a41 (http://faust.grame.fr)
+------------------------------------------------------------ */
+
+#ifndef  __mydsp_H__
+#define  __mydsp_H__
+/************************************************************************
+
+	IMPORTANT NOTE : this file contains two clearly delimited sections :
+	the ARCHITECTURE section (in two parts) and the USER section. Each section
+	is governed by its own copyright and license. Please check individually
+	each section for license and copyright information.
+*************************************************************************/
+
+/*******************BEGIN ARCHITECTURE SECTION (part 1/2)****************/
+
+/************************************************************************
+    FAUST Architecture File
+	Copyright (C) 2003-2011 GRAME, Centre National de Creation Musicale
+    ---------------------------------------------------------------------
+    This Architecture section is free software; you can redistribute it
+    and/or modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 3 of
+	the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+	along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+	EXCEPTION : As a special exception, you may create a larger work
+	that contains this FAUST architecture section and distribute
+	that work under terms of your choice, so long as this FAUST
+	architecture section is not modified.
+
+
+ ************************************************************************
+ ************************************************************************/
+
+#import "FICocoaUI.h"
+
+/******************************************************************************
+*******************************************************************************
+
+							       VECTOR INTRINSICS
+
+*******************************************************************************
+*******************************************************************************/
+
+
+/******************************************************************************
+*******************************************************************************
+
+								USER INTERFACE
+
+*******************************************************************************
+*******************************************************************************/
+
+/********************END ARCHITECTURE SECTION (part 1/2)****************/
+
+/**************************BEGIN USER SECTION **************************/
+
+#ifndef FAUSTFLOAT
+#define FAUSTFLOAT float
+#endif  
+
+
+
+#ifndef FAUSTCLASS 
+#define FAUSTCLASS mydsp
+#endif
+
+class mydsp : public dsp {
+	
+  private:
+	
+	int iRec0[2];
+	FAUSTFLOAT fVslider0;
+	int fSamplingFreq;
+	
+  public:
+	
+	void static metadata(Meta* m) { 
+		m->declare("author", "Grame");
+		m->declare("copyright", "(c)GRAME 2009");
+		m->declare("license", "BSD");
+		m->declare("name", "Noise");
+		m->declare("version", "1.1");
+	}
+
+	virtual int getNumInputs() {
+		return 0;
+		
+	}
+	virtual int getNumOutputs() {
+		return 1;
+		
+	}
+	virtual int getInputRate(int channel) {
+		int rate;
+		switch (channel) {
+			default: {
+				rate = -1;
+				break;
+			}
+			
+		}
+		return rate;
+		
+	}
+	virtual int getOutputRate(int channel) {
+		int rate;
+		switch (channel) {
+			case 0: {
+				rate = 1;
+				break;
+			}
+			default: {
+				rate = -1;
+				break;
+			}
+			
+		}
+		return rate;
+		
+	}
+	
+	static void classInit(int samplingFreq) {
+		
+	}
+	
+	virtual void instanceInit(int samplingFreq) {
+		fSamplingFreq = samplingFreq;
+		fVslider0 = FAUSTFLOAT(0.);
+		for (int i0 = 0; (i0 < 2); i0 = (i0 + 1)) {
+			iRec0[i0] = 0;
+			
+		}
+		
+	}
+	
+	virtual void init(int samplingFreq) {
+		classInit(samplingFreq);
+		instanceInit(samplingFreq);
+	}
+	
+	virtual void buildUserInterface(UI* interface) {
+		interface->openVerticalBox("0x00");
+		interface->declare(&fVslider0, "style", "knob");
+		interface->addVerticalSlider("Volume", &fVslider0, 0.f, 0.f, 1.f, 0.1f);
+		interface->closeBox();
+		
+	}
+	
+	virtual void compute(int count, FAUSTFLOAT** inputs, FAUSTFLOAT** outputs) {
+		FAUSTFLOAT* output0 = outputs[0];
+		float fSlow0 = (4.65661e-10f * float(fVslider0));
+		for (int i = 0; (i < count); i = (i + 1)) {
+			iRec0[0] = (12345 + (1103515245 * iRec0[1]));
+			output0[i] = FAUSTFLOAT((fSlow0 * float(iRec0[0])));
+			iRec0[1] = iRec0[0];
+			
+		}
+		
+	}
+
+	
+};
+
+
+/***************************END USER SECTION ***************************/
+
+/*******************BEGIN ARCHITECTURE SECTION (part 2/2)***************/
+
+dsp* DSP;
+
+/********************END ARCHITECTURE SECTION (part 2/2)****************/
+
+
+
+#endif
