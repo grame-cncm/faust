59803307c241ee55dec0bff809b6261f97afd6a5
Stephane Letz <letz@grame.fr>
2016-02-07 10:20:55 +0100

    Add removeMidiIn method in MidiUI and midi_handler.

--- architecture/faust/midi/midi.h
+++ architecture/faust/midi/midi.h
@@ -102,6 +102,13 @@ class midi_handler : public midi {
         virtual ~midi_handler() {}
         
         virtual void addMidiIn(midi* midi_dsp) { fMidiInputs.push_back(midi_dsp); }
+        virtual void removeMidiIn(midi* midi_dsp) 
+        { 
+            std::vector<midi*>::iterator it = std::find(fMidiInputs.begin(), fMidiInputs.end(), midi_dsp);
+            if (it != fMidiInputs.end()) {
+                fMidiInputs.erase(it); 
+            }
+        }
         
         virtual bool start() {}
         virtual void stop() {}
