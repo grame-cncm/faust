5f7123e291ddc35fd29637a85adf601d13f64407
D. Fober <fober@grame.fr>
2016-02-19 16:52:55 +0100

    sensors added to qt architecture

--- architecture/ca-qt.cpp
+++ architecture/ca-qt.cpp
@@ -44,6 +44,7 @@
 #include "faust/gui/FUI.h"
 #include "faust/gui/JSONUI.h"
 #include "faust/gui/faustqt.h"
+#include "faust/gui/APIUI.h"
 #include "faust/misc.h"
 #ifdef IOS
 #include "faust/audio/coreaudio-ios-dsp.h"
@@ -95,6 +96,126 @@ ztimedmap GUI::gTimedZoneMap;
 /******************************************************************************
 *******************************************************************************
 
+                                Sensors section
+
+*******************************************************************************
+*******************************************************************************/
+#include <QAccelerometer>
+#include <QGyroscope>
+#include <QAccelerometerReading>
+#include <QGyroscopeReading>
+#include <QTimer>
+
+//------------------------------------------------------------------------
+class Sensor
+{
+	private:
+		QSensor*		fSensor;
+		int				fType;
+		QSensorReading*	fReader;
+		bool			fSkipDuplicates;
+		std::vector<float> fLastRead;
+
+		QSensor*		create(int type) const;
+	
+	public:
+		enum  { kSensorStart=1, kAccelerometer=1, kGyroscope, kSensorMax };
+
+				 Sensor(int type) : fSensor(0), fType(type), fReader(0), fSkipDuplicates(false)
+									{ fSensor = create (type); fSensor->connectToBackend(); }
+		virtual ~Sensor()			{ if (available()) activate (false); delete fSensor; }
+
+		int				isAccel() const		{ return fType == kAccelerometer; }
+		int				isGyro () const		{ return fType == kGyroscope; }
+		bool			available() const	{ return fSensor->isConnectedToBackend(); }
+		bool			active() const		{ return fSensor->isActive(); }
+		void			activate(bool state){ fSensor->setActive(state); fReader = fSensor->reading(); }
+		int				count();
+		float			value(int i) const	{ return fReader->value(i).value<float>(); }
+
+		void			skipDuplicates(bool state)	{ fSkipDuplicates = state;
+													  if (!state) fLastRead.clear();
+													}
+};
+
+
+//------------------------------------------------------------------------
+// count the number of values available for the sensor
+int	Sensor::count()
+{
+	int n = fReader ? fReader->valueCount() : 0;
+	if (n && fSkipDuplicates) {
+		bool newval = false;
+		if (int(fLastRead.size()) == n) {
+			for (int i=0; (i < n) && !newval; i++) {
+				newval = fLastRead[i] != value(i);
+			}
+		}
+		else newval = true;
+		if (newval) {
+			fLastRead.clear();
+			for (int i=0; i < n; i++)
+				fLastRead.push_back (value(i));
+		}
+		else n = 0;
+	}
+	return n;
+}
+
+//------------------------------------------------------------------------
+QSensor* Sensor::create (int type) const
+{
+	switch (type) {
+		case kAccelerometer:			return new QAccelerometer();
+		case kGyroscope:				return new QGyroscope();
+		default:
+			cerr << "unknown sensor type " << type << endl;
+	}
+	return 0;
+}
+
+//------------------------------------------------------------------------
+class Sensors : public QObject
+{
+	APIUI* fUI;
+	Sensor 	fAccel,	fGyro;
+	int		fTimerID;
+	public:
+		typedef std::map<int, Sensor*> TSensors;
+				 Sensors(APIUI* ui)
+				 		: fUI(ui), fAccel(Sensor::kAccelerometer), fGyro(Sensor::kGyroscope), fTimerID(0) {}
+		virtual ~Sensors()		{	killTimer(fTimerID); }
+		void 	start();
+	protected:
+		void timerEvent(QTimerEvent * );
+};
+
+//------------------------------------------------------------------------
+void Sensors::timerEvent(QTimerEvent * )
+{
+	if (fAccel.active()) {
+        int count = fAccel.count();
+        for (int i=0; (i< count) && (i < 3); i++)
+        	fUI->propagateAcc (i, fAccel.value(i));
+	}
+	if (fGyro.active()) {
+        int count = fGyro.count();
+        for (int i=0; (i< count) && (i < 3); i++)
+        	fUI->propagateGyr (i, fGyro.value(i));
+	}
+}
+
+//------------------------------------------------------------------------
+void Sensors::start() 
+{
+	if (fAccel.available()) fAccel.activate(true);
+	if (fGyro.available()) 	fGyro.activate(true);
+	if (fAccel.active() || fGyro.active()) fTimerID = startTimer(10);
+}
+
+/******************************************************************************
+*******************************************************************************
+
                                 MAIN PLAY THREAD
 
 *******************************************************************************
@@ -123,6 +244,8 @@ int main(int argc, char *argv[])
 	char name[256];
 	char rcfilename[256];
 	char* home = getenv("HOME");
+    APIUI apiui;
+    Sensors sensors (&apiui);
 
 	snprintf(name, 255, "%s", basename(argv[0]));
 	snprintf(rcfilename, 255, "%s/.%src", home, name);
@@ -169,6 +292,7 @@ int main(int argc, char *argv[])
     FUI finterface;
     DSP->buildUserInterface(&interface);
     DSP->buildUserInterface(&finterface);
+    DSP->buildUserInterface(&apiui);
 
 #ifdef MIDICTRL
     MidiUI midiinterface(name);
@@ -192,6 +316,7 @@ int main(int argc, char *argv[])
 	audio.init(name, DSP);
 	finterface.recallState(rcfilename);
 	audio.start();
+	sensors.start();
     
     printf("ins %d\n", audio.get_num_inputs());
     printf("outs %d\n", audio.get_num_outputs());
@@ -210,6 +335,7 @@ int main(int argc, char *argv[])
 	midiinterface.run();
 #endif
 	interface.run();
+	
 
     myApp.setStyleSheet(interface.styleSheet());
     myApp.exec();
