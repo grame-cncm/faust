853eea694381372abe3b47cee8620ffc51bc2c03
Stephane Letz <letz@grame.fr>
2016-07-26 17:40:30 +0200

    Improve 'impulse-tests'.

--- tests/impulse-tests/makeReferenceImpulses.sh
+++ tests/impulse-tests/makeReferenceImpulses.sh
@@ -13,5 +13,9 @@ cd codes-to-test
 for f in *.dsp; do
 	faust2impulse $f      > ../expected-responses/$f.scal.ir
 	faust2impulse -vec $f > ../expected-responses/$f.vec.ir
-    diff ../expected-responses/$f.scal.ir ../expected-responses/$f.vec.ir && echo "OK $f , scalar and vector IR are identical" || echo "ERROR $f, scalar and vector IR are DIFFERENT"
+    faust2impulse -sch $f > ../expected-responses/$f.sch.ir
+    diff ../expected-responses/$f.scal.ir ../expected-responses/$f.vec.ir && echo "OK $f, scalar and vector IR are identical" || echo "ERROR $f, scalar and vector IR are DIFFERENT"
+    diff ../expected-responses/$f.scal.ir ../expected-responses/$f.sch.ir && echo "OK $f, scalar and scheduler IR are identical" || echo "ERROR $f, scalar and scheduler IR are DIFFERENT"
+    rm ../expected-responses/$f.vec.ir
+    rm ../expected-responses/$f.sch.ir
 done
