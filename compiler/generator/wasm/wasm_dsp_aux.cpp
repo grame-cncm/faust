6ed3db3aef6188ec80968c5b6cd66ae8b06c90ba
Stephane Letz <letz@grame.fr>
2016-08-31 11:27:00 +0200

    New files for wasm backend.
    Former-commit-id: fb36090b07619fa9ae2a7fc572d7d041001ae2bb

---
+++ compiler/generator/wasm/wasm_dsp_aux.cpp
@@ -0,0 +1,117 @@
+/************************************************************************
+ ************************************************************************
+    FAUST compiler
+	Copyright (C) 2003-2014 GRAME, Centre National de Creation Musicale
+    ---------------------------------------------------------------------
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ ************************************************************************
+ ************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "compatibility.hh"
+#include "wasm_dsp_aux.hh"
+#include "libfaust.h"
+#include "Text.hh"
+
+typedef class faust_smartptr<wasm_dsp_factory> SDsp_factory;
+
+dsp_factory_table<SDsp_factory> gWasmFactoryTable;
+
+// C++ API
+
+EXPORT wasm_dsp_factory* createWasmDSPFactoryFromString(const string& name_app, const string& dsp_content, int argc, const char* argv[], string& error_msg)
+{
+    string expanded_dsp_content, sha_key;
+    
+    if ((expanded_dsp_content = expandDSPFromString(name_app, dsp_content, argc, argv, sha_key, error_msg)) == "") {
+        return 0;
+    } else {
+        
+        int argc1 = argc + 5;
+        const char* argv1[64];
+        
+        argv1[0] = "faust";
+        argv1[1] = "-lang";
+        argv1[2] = "wasm";
+        argv1[3] = "-o";
+        argv1[4] = "string";
+        
+        for (int i = 0; i < argc; i++) {
+            argv1[i+5] = argv[i];
+        }
+        
+        argv1[argc1] = 0;  // NULL terminated argv
+        
+        dsp_factory_table<SDsp_factory>::factory_iterator it;
+        wasm_dsp_factory* factory = 0;
+        
+        if (gWasmFactoryTable.getFactory(sha_key, it)) {
+            SDsp_factory sfactory = (*it).first;
+            sfactory->addReference();
+            return sfactory;
+        } else if ((factory = new wasm_dsp_factory(compile_faust_factory(argc1, argv1,
+                                                                            name_app.c_str(),
+                                                                            dsp_content.c_str(),
+                                                                            error_msg))) != 0) {
+            gWasmFactoryTable.setFactory(factory);
+            factory->setSHAKey(sha_key);
+            factory->setDSPCode(expanded_dsp_content);
+            return factory;
+        } else {
+            return 0;
+        }
+    }
+}
+
+EXPORT bool deleteInterpreterDSPFactory(wasm_dsp_factory* factory)
+{
+    return (factory) ? gWasmFactoryTable.deleteDSPFactory(factory): false;
+}
+
+// C API
+
+EXPORT const char* createWasmCDSPFactoryFromString(const char* name_app, const char* dsp_content, int argc, const char* argv[], char* error_msg)
+{
+    string error_msg_aux;
+    wasm_dsp_factory* factory = createWasmDSPFactoryFromString(name_app, dsp_content, argc, argv, error_msg_aux);
+    
+    if (factory) {
+        stringstream dst;
+        factory->write(&dst);
+        strncpy(error_msg, error_msg_aux.c_str(), 4096);
+        string str = flatten(dst.str());
+        char* cstr = (char*)malloc(str.length() + 1);
+        strcpy(cstr, str.c_str());
+        return cstr;
+        // And keep factory...
+    } else {
+        strncpy(error_msg, "libfaust.js fatal error...", 256);
+        return NULL;
+    }
+}
+
+/*
+EXPORT char* getCLibFaustVersion() { return (char*)FAUSTVERSION; }
+
+EXPORT void freeCDSP(void* ptr)
+{
+    free(ptr);
+}
+*/
---
+++ compiler/generator/wasm/wasm_dsp_aux.hh
@@ -0,0 +1,105 @@
+/************************************************************************
+ ************************************************************************
+    FAUST compiler
+	Copyright (C) 2003-2014 GRAME, Centre National de Creation Musicale
+    ---------------------------------------------------------------------
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ ************************************************************************
+ ************************************************************************/
+ 
+#ifndef WASM_DSP_AUX_H
+#define WASM_DSP_AUX_H
+
+#include <string>
+#include <cstdlib>
+#include "export.hh"
+#include "faust/dsp/dsp.h"
+#include "dsp_aux.hh"
+#include "dsp_factory.hh"
+
+using namespace std;
+
+class EXPORT wasm_dsp : public dsp {};
+
+class EXPORT wasm_dsp_factory : public dsp_factory, public faust_smartable {
+    
+    protected:
+        
+        dsp_factory_base* fFactory;
+        
+        virtual ~wasm_dsp_factory()
+        {
+            delete fFactory;
+        }
+        
+    public:
+        
+        wasm_dsp_factory(dsp_factory_base* factory):fFactory(factory)
+        {}
+    
+        std::string getName() { return fFactory->getName(); }
+        
+        std::string getSHAKey() { return fFactory->getSHAKey(); }
+        void setSHAKey(std::string sha_key) { fFactory->setSHAKey(sha_key); }
+        
+        std::string getDSPCode() { return fFactory->getDSPCode(); }
+        void setDSPCode(std::string code) { return fFactory->setDSPCode(code); }
+        
+        wasm_dsp* createDSPInstance() { return nullptr; }
+        
+        void write(std::ostream* out, bool small = false) { fFactory->write(out, small); }
+    
+};
+
+EXPORT wasm_dsp_factory* createWasmDSPFactoryFromString(const string& name_app, const string& dsp_content, int argc, const char* argv[], string& error_msg);
+
+EXPORT bool deleteWasmDSPFactory(wasm_dsp_factory* factory);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+     /**
+     * Create a Faust DSP WASM module and additional helpers functions from a DSP source code.
+     * 
+     * @param name_app - the name of the Faust program
+     * @param dsp_content - the Faust program as a string
+     * @param argc - the number of parameters in argv array
+     * @param argv - the array of parameters
+     * @param error_msg - the error string to be filled, has to be 256 characters long
+     *
+     * @return a valid DSP asm.js module and additional helpers functions as a string on success (to be deleted by the caller), otherwise a null pointer.
+     */ 
+    EXPORT const char* createWasmCDSPFactoryFromString(const char* name_app, const char* dsp_content, int argc, const char* argv[], char* error_msg);
+    
+    /**
+     * Get the library version.
+     * 
+     * @return the library version as a static string.
+     */
+    EXPORT char* getCLibFaustVersion();
+    
+    /**
+     * The free function to be used on memory returned by createAsmCDSPFactoryFromString.
+     * 
+     * @param ptr - the pointer to be deleted.
+     */
+    EXPORT void freeCDSP(void* ptr);
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif
