60f3ea6e0c506461049a954e59485c76041b0f01
Stephane Letz <letz@grame.fr>
2016-09-08 23:47:41 +0200

    Cleanup, documentation.
    Former-commit-id: ead4eee9cfed66d514be579af45c5aeed165a577

--- architecture/faust/dsp/dsp-optimizer.h
+++ architecture/faust/dsp/dsp-optimizer.h
@@ -1,12 +1,12 @@
 
 /************************************************************************
     FAUST Architecture File
-	Copyright (C) 2016 GRAME, Centre National de Creation Musicale
+    Copyright (C) 2016 GRAME, Centre National de Creation Musicale
     ---------------------------------------------------------------------
     This Architecture section is free software; you can redistribute it 
     and/or modify it under the terms of the GNU General Public License 
-	as published by the Free Software Foundation; either version 3 of 
-	the License, or (at your option) any later version.
+    as published by the Free Software Foundation; either version 3 of 
+    the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,14 +14,17 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License 
-	along with this program; If not, see <http://www.gnu.org/licenses/>.
+    along with this program; If not, see <http://www.gnu.org/licenses/>.
 
-	EXCEPTION : As a special exception, you may create a larger work 
-	that contains this FAUST architecture section and distribute  
-	that work under terms of your choice, so long as this FAUST 
-	architecture section is not modified. 
+    EXCEPTION : As a special exception, you may create a larger work 
+    that contains this FAUST architecture section and distribute  
+    that work under terms of your choice, so long as this FAUST 
+    architecture section is not modified. 
 
- ************************************************************************/
+************************************************************************/
+
+#ifndef __dsp_optimizer__
+#define __dsp_optimizer__
 
 #include <iostream>
 #include <sstream>
@@ -32,6 +35,10 @@
 
 #include "faust/dsp/llvm-dsp.h"
 #include "faust/dsp/dsp-bench.h"
+
+/*
+    A class to find optimal Faust compiler parameters for a given DSP.
+ */
 
 class dsp_optimizer {
 
@@ -155,27 +162,6 @@ class dsp_optimizer {
             t0.push_back("-scal");
             fOptionsTable.push_back(t0);
             
-            /*
-             std::stringstream num;
-             num << 32;
-             std::vector <string> t1;
-             t1.push_back("-sch");
-             t1.push_back("-vs");
-             t1.push_back(num.str());
-             fOptionsTable.push_back(t1);
-             
-             
-             std::stringstream num;
-             num << 512;
-             std::vector <string> t1;
-             t1.push_back("-vec");
-             t1.push_back("-lv");
-             t1.push_back("1");
-             t1.push_back("-vs");
-             t1.push_back(num.str());
-             fOptionsTable.push_back(t1);
-             */
-            
             // vec -lv 0
             for (int size = 4; size <= fVSIZE; size *= 2) {
                 std::stringstream num;
@@ -231,8 +217,8 @@ class dsp_optimizer {
             }
             
             /*
-             // sch
-             for (int size = 8; size <= fVSIZE; size *= 2) {
+            // sch
+            for (int size = 4; size <= fVSIZE; size *= 2) {
                  std::stringstream num;
                  num << size;
                  std::vector <std::string> t1;
@@ -244,8 +230,6 @@ class dsp_optimizer {
              */
         }
     
-        const char* getError() { return fError.c_str(); }
-        
         void printItem(const std::vector <std::string>& item)
         {
             for (int i = 0; i < item.size(); i++) {
@@ -285,18 +269,7 @@ class dsp_optimizer {
             if (!fFactory)  {
                 std::cout << "Cannot create factory : " << fError.c_str() << std::endl;
                 return false;
-            } else {
-                //std::cout << "Factory correctly generated" << endl;
             }
-            
-            /*
-             std::vector<std::string> path_list = getLibraryList(fFactory);
-             std::vector<std::string>::iterator it;
-             for (it = path_list.begin(); it != path_list.end(); it++) {
-                std::string file = *it;
-                std::cout << "file " << file.c_str() << endl;
-             }
-             */
             
             fDSP = fFactory->createDSPInstance();
             
@@ -341,7 +314,7 @@ class dsp_optimizer {
                  std::cout << "pthread_attr_setstacksize error " << res <<  std::endl;
             } 
             
-             std::cout << "setStackSize size = " << size <<  std::endl;
+            std::cout << "setStackSize size = " << size <<  std::endl;
         }
        
     public:
@@ -414,11 +387,13 @@ class dsp_optimizer {
                 return fOptionsTable[table_res[0].first];
             } else {
                 // Scalar...
-                std::cout << "findOptimize no options found..." << std::endl;
+                std::cout << "findOptimizedParameters no options found..." << std::endl;
                 return fOptionsTable[0];
             }
         }
     
+        const char* getError() { return fError.c_str(); }
+    
 };
 
-
+#endif
--- benchmark/bench-llvm.cpp
+++ benchmark/bench-llvm.cpp
@@ -45,5 +45,3 @@ int main(int argc, char* argv[])
     cout << endl;
   	return 0;
 }
-
-
