a1d4a3da3a2790c86e78e12c6cb911d569fd9068
Romain Michon <rmnmichon@gmail.com>
2016-11-02 13:59:34 -0700

    Android API based on a class

--- architecture/android/app/src/main/java/com/DspFaust/dsp_faust.java
+++ architecture/android/app/src/main/java/com/DspFaust/dsp_faust.java
@@ -6,21 +6,7 @@
  * the SWIG interface file instead.
  * ----------------------------------------------------------------------------- */
 
-package com.dsp_faust;
+package com.DspFaust;
 
-public class SWIGTYPE_p_void {
-  private transient long swigCPtr;
-
-  protected SWIGTYPE_p_void(long cPtr, @SuppressWarnings("unused") boolean futureUse) {
-    swigCPtr = cPtr;
-  }
-
-  protected SWIGTYPE_p_void() {
-    swigCPtr = 0;
-  }
-
-  protected static long getCPtr(SWIGTYPE_p_void obj) {
-    return (obj == null) ? 0 : obj.swigCPtr;
-  }
+public class dsp_faust {
 }
-
