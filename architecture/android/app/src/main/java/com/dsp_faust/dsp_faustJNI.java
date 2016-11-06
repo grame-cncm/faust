eac9fdb97973d3a9a3811ecafb4271c320f19f33
Yann Orlarey <orlarey@grame.fr>
2016-11-06 11:30:40 +0100

    Revert "Adding JAVA interface to previous commit and fixing bugs"
    This reverts commit fec435fd5f5e1b2324d77259da9ceff9c903db54.

--- architecture/android/app/src/main/java/com/dsp_faust/dsp_faustJNI.java
+++ architecture/android/app/src/main/java/com/dsp_faust/dsp_faustJNI.java
@@ -19,25 +19,25 @@ public class dsp_faustJNI {
     }
   }
 
-  public final static native long create(int jarg1, int jarg2);
-  public final static native void destroy(long jarg1);
-  public final static native boolean start(long jarg1);
-  public final static native void stop(long jarg1);
-  public final static native boolean isRunning(long jarg1);
-  public final static native int keyOn(long jarg1, int jarg2, int jarg3);
-  public final static native int keyOff(long jarg1, int jarg2);
-  public final static native void propagateMidi(long jarg1, int jarg2, double jarg3, int jarg4, int jarg5, int jarg6, int jarg7);
-  public final static native String getJSON(long jarg1);
-  public final static native int getParamsCount(long jarg1);
-  public final static native void setParamValue(long jarg1, String jarg2, float jarg3);
-  public final static native float getParamValue(long jarg1, String jarg2);
-  public final static native void setVoiceParamValue(long jarg1, String jarg2, int jarg3, float jarg4);
-  public final static native float getVoiceParamValue(long jarg1, String jarg2, int jarg3);
-  public final static native String getParamAddress(long jarg1, int jarg2);
-  public final static native void propagateAcc(long jarg1, int jarg2, float jarg3);
-  public final static native void setAccConverter(long jarg1, int jarg2, int jarg3, int jarg4, float jarg5, float jarg6, float jarg7);
-  public final static native void propagateGyr(long jarg1, int jarg2, float jarg3);
-  public final static native void setGyrConverter(long jarg1, int jarg2, int jarg3, int jarg4, float jarg5, float jarg6, float jarg7);
-  public final static native float getCPULoad(long jarg1);
-  public final static native int getScreenColor(long jarg1);
+  public final static native boolean init(int jarg1, int jarg2);
+  public final static native boolean start();
+  public final static native void stop();
+  public final static native void destroy();
+  public final static native boolean isRunning();
+  public final static native int keyOn(int jarg1, int jarg2);
+  public final static native int keyOff(int jarg1);
+  public final static native String getJSON();
+  public final static native int getParamsCount();
+  public final static native void setParamValue(String jarg1, float jarg2);
+  public final static native float getParamValue(String jarg1);
+  public final static native void setVoiceParamValue(String jarg1, int jarg2, float jarg3);
+  public final static native float getVoiceParamValue(String jarg1, int jarg2);
+  public final static native String getParamAddress(int jarg1);
+  public final static native void propagateAcc(int jarg1, float jarg2);
+  public final static native void setAccConverter(int jarg1, int jarg2, int jarg3, float jarg4, float jarg5, float jarg6);
+  public final static native void propagateGyr(int jarg1, float jarg2);
+  public final static native void setGyrConverter(int jarg1, int jarg2, int jarg3, float jarg4, float jarg5, float jarg6);
+  public final static native void propagateMidi(int jarg1, double jarg2, int jarg3, int jarg4, int jarg5, int jarg6);
+  public final static native float getCPULoad();
+  public final static native int getScreenColor();
 }
