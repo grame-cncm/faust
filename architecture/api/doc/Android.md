ed7f6ada269b54684000fd95b984942241722fab
Romain Michon <rmnmichon@gmail.com>
2016-11-27 09:06:06 -0800

    Addding more features to automatic documentation of faust2api

--- architecture/api/doc/Android.md
+++ architecture/api/doc/Android.md
@@ -1,6 +1,2 @@
 # Faust Android API
 
-## List of Parameters
-
-This list gives the ID number of the parameter first and the parameter address next.
-
--- architecture/path-printer.cpp
+++ architecture/path-printer.cpp
@@ -17,9 +17,25 @@ int main(int argc, char *argv[] )
 {
   FaustPolyEngine fPolyEngine(new dummy_audio());
 
+  std::cout << "## Parameters List\n\n";
+  
+  std::cout << "### Main Parameters\n\n";
   for(int i=0; i<fPolyEngine.getParamsCount(); i++){
     std::cout << "* **" << i << "**: `" << fPolyEngine.getParamAddress(i) << "`" << "\n";
   }
+  std::cout << "\n";
+
+  #if POLY2
+  // kind of funny, but it's the fastest way
+  MapUI *idpVoice = fPolyEngine.keyOn(100,100);
+
+  std::cout << "### Independent Voices" << "\n\n";
+  for(int i=0; i<idpVoice->getParamsCount(); i++){
+    std::cout << "* **" << i << "**: `" << idpVoice->getParamAdress(i) << "`" << "\n";
+  }
+
+  fPolyEngine.keyOff(100); // just to be really clean...
+  #endif
   
   return 0;
 }
