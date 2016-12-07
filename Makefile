303633f877c97c237ad5eb037f0e152bd9afb2a8
Yann Orlarey <orlarey@grame.fr>
2016-12-07 04:36:30 +0100

    Version set to 0.9.94

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-version := 0.9.93
+version := 0.9.94
 
 DESTDIR ?= 
 PREFIX ?= /usr/local
--- compiler/main.cpp
+++ compiler/main.cpp
@@ -18,7 +18,7 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************
  ************************************************************************/
-#define FAUSTVERSION "0.9.92"
+#define FAUSTVERSION "0.9.94"
 
 #include <stdlib.h>
 #include <stdio.h>
