4fb987c9b6526d0e6bd43be0a3cbe29707eeac47
Stephane Letz <letz@grame.fr>
2016-12-19 15:47:03 +0100

    Make LLVM backend work on Linux again with LLVM 3.5.

--- compiler/generator/llvm/llvm_code_container.cpp
+++ compiler/generator/llvm/llvm_code_container.cpp
@@ -56,6 +56,9 @@ LLVMCodeContainer::LLVMCodeContainer(const string& name, int numInputs, int numO
     fKlassName = name;
     fContext = new LLVMContext();
     fModule = new Module(LVVM_BACKEND_NAME, getContext());
+#if (defined(LLVM_34) || defined(LLVM_35))
+    fModule->setDataLayout("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128");
+#endif
     fBuilder = new IRBuilder<>(getContext());
     
 #if (defined(LLVM_34) || defined(LLVM_35) || defined(LLVM_36) || defined(LLVM_37) || defined(LLVM_38) || defined(LLVM_39))
