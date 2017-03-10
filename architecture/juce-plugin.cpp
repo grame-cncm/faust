0503bc9d7308939099f6a985c7d87ca363ce1617
Stephane Letz <letz@grame.fr>
2017-03-10 12:39:12 +0100

    Fixes in JUCE architecture files for Windows compatibility.

--- architecture/juce-plugin.cpp
+++ architecture/juce-plugin.cpp
@@ -26,16 +26,16 @@
 #include <math.h>
 #include <algorithm>
 
+#define JUCE_CORE_INCLUDE_NATIVE_HEADERS 1
+#include "JuceLibraryCode/JuceHeader.h"
+
 #include "faust/gui/MapUI.h"
 #include "faust/misc.h"
 #include "faust/dsp/dsp-adapter.h"
 #include "faust/gui/MidiUI.h"
 
-// Those 2 files include <windows.h>, they have to be include before JuceHeader.h
 #include "faust/dsp/timed-dsp.h"
 #include "faust/dsp/poly-dsp.h"
-
-#include "JuceLibraryCode/JuceHeader.h"
 
 #include "faust/gui/JuceGUI.h"
 #include "faust/gui/JuceParameterUI.h"
