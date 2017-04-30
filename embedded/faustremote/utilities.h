def37f2bd211ae2d1be827f11b87446f927c76b5
Stephane Letz <letz@grame.fr>
2017-04-30 11:08:27 +0200

    Add missing pathToContent code.

--- embedded/faustremote/utilities.h
+++ embedded/faustremote/utilities.h
@@ -23,10 +23,30 @@
 #define _utilities_H
 
 #include <string>
+#include <iostream>
+#include <fstream> 
 
 using namespace std;
 
-string pathToContent(const string& path); // Implemented in libfaust
+inline std::string pathToContent(const std::string& path)
+{
+    std::ifstream file(path.c_str(), std::ifstream::binary);
+    
+    file.seekg(0, file.end);
+    int size = int(file.tellg());
+    file.seekg(0, file.beg);
+    
+    // And allocate buffer to that a single line can be read...
+    char* buffer = new char[size + 1];
+    file.read(buffer, size);
+    
+    // Terminate the string
+    buffer[size] = 0;
+    string result = buffer;
+    file.close();
+    delete [] buffer;
+    return result;
+}
 
 std::string searchIP();
 
