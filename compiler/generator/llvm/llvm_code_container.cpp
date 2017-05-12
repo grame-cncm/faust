d4b09d87a01d814a437fb0ef95f8c8576cf8228b
Stephane Letz <letz@grame.fr>
2017-05-12 16:11:02 +0200

    Correct LLVM backend for LLVM 3.6 version.

--- compiler/generator/llvm/llvm_code_container.cpp
+++ compiler/generator/llvm/llvm_code_container.cpp
@@ -53,7 +53,7 @@ LLVMCodeContainer::LLVMCodeContainer(const string& name, int numInputs, int numO
     fKlassName = name;
     fContext = new LLVMContext();
     fModule = new Module(string(LVVM_BACKEND_NAME) + ": v" + string(FAUSTVERSION), getContext());
-#if (defined(LLVM_34) || defined(LLVM_35))
+#if (defined(LLVM_34) || defined(LLVM_35) || defined(LLVM_36))
     fModule->setDataLayout("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128");
 #endif
     fBuilder = new IRBuilder<>(getContext());
