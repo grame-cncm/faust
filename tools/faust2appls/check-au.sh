d238e4e1b5b1d03777d5167f4d533b82878120fb
IOhannes m zmÃ¶lnig <zmoelnig@umlautQ.umlaeute.mur.at>
2017-06-08 15:47:03 +0200

    removed some obvious bashisms

--- tools/faust2appls/check-au.sh
+++ tools/faust2appls/check-au.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 failures=0
-for i in `seq 0 $1`; do
+for i in $(seq 0 $1); do
 	auvaltool -v aufx cabl Gram 1> /dev/null 2> /dev/null
 	if [ $? -gt 0 ]; then
-		let "failures+=1"
+		failures=$((failures+1))
 	fi
 done
 echo $failures
