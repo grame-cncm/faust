722f55e93a66adddfef2d52e70f1fb1a9320d330
Yann Orlarey <orlarey@grame.fr>
2017-06-27 18:41:38 +0200

    Added atomic exchange between SF0 and SF0cache pointers. SF0 will be nullptr during the compute() method. This is to prevent the architecture for changing the soundfile pointer while it is in use.

--- compiler/generator/compile_scal.cpp
+++ compiler/generator/compile_scal.cpp
@@ -715,10 +715,14 @@ string ScalarCompiler::generateHBargraph(Tree sig, Tree path, Tree min, Tree max
 string ScalarCompiler::generateSoundfile(Tree sig, Tree path)
 {
     string v = getFreshID("SF");
-    fClass->addDeclCode(subst("Soundfile* \t$0;", v));
-    fClass->addDeclCode(subst("Soundfile* \t$0cache;", v));
+    fClass->addIncludeFile("<atomic>");
+    fClass->addDeclCode(subst("std::atomic<Soundfile*> \t$0;", v));
+    //fClass->addDeclCode(subst("Soundfile* \t$0cache;", v));
 	addUIWidget(reverse(tl(path)), uiWidget(hd(path), tree(v), sig));
     fClass->addInitUICode(subst("$0 = &defaultsound;", v));
+    fClass->addFirstPrivateDecl(subst("$0cache", v));
+    fClass->addZone2(subst("Soundfile* $0cache = $0.exchange(nullptr);", v));
+    fClass->addZone4(subst("$0 = $0cache;", v));
     return v;
 }
 
