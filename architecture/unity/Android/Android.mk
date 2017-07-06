3f544a69b4ea08e327af66a62b76ee1f34ff8203
Théophile Dupré <dupre@magellan-pro.local>
2017-07-06 14:24:07 +0200

    adjustement in faust2unity.. files

--- architecture/unity/Android/Android.mk
+++ architecture/unity/Android/Android.mk
@@ -6,8 +6,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE            := libFaustPlugin_$(FILE)
 LOCAL_SRC_FILES         := $(LOCAL_PATH)/$(FILE).cpp
-LOCAL_CPPFLAGS          := -frtti -fexceptions -std=c++11
-LOCAL_CFLAGS            := -std=c11 -O3 -ffast-math -fPIC
+LOCAL_CPPFLAGS          := -frtti -fexceptions
+LOCAL_CFLAGS            := -O3 -ffast-math -fPIC
 LOCAL_LDLIBS            := -latomic
 
 
