grame-cncm/faust

commit abc20108fbcbfb4662077d065b830932555971cc

Separate JSONUIDecoder class, cleanup Bela.cpp architecture.

architecture/faust/gui/JSONUI.h
@@ -50,9 +50,13 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
         std::stringstream fUI;
         std::stringstream fMeta;
         std::vector<std::pair <std::string, std::string> > fMetaAux;
+        std::string fVersion;
+        std::string fOptions;
         std::string fName;
         std::string fExpandedCode;
         std::string fSHAKey;
+        std::string fDSPSize;
+        std::map<std::string, int> fPathTable;
     
         char fCloseUIPar;
         char fCloseMetaPar;
@@ -83,7 +87,14 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
             }
         }
         
-        void init(const std::string& name, int inputs, int outputs, const std::string& sha_key, const std::string& dsp_code)
+        void init(const std::string& name,
+                  int inputs, int outputs,
+                  const std::string& sha_key,
+                  const std::string& dsp_code,
+                  const std::string& version,
+                  const std::string& options,
+                  const std::string& size,
+                  const std::map<std::string, int>& path_table)
         {
             fTab = 1;
             
@@ -101,6 +112,10 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
             fOutputs = outputs;
             fExpandedCode = dsp_code;
             fSHAKey = sha_key;
+            fDSPSize = size;
+            fPathTable = path_table;
+            fVersion = version;
+            fOptions = options;
         }
         
         inline std::string flatten(const std::string& src)
@@ -122,27 +137,45 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
             }
             return dst.str();
         }
+    
+        inline std::string getAddressIndex(const std::string& path)
+        {
+            if (fPathTable.find(path) != fPathTable.end()) {
+                std::stringstream num; num << fPathTable[path];
+                return num.str();
+            } else {
+                return "-1";
+            }
+        }
       
      public:
      
-        JSONUIAux(const std::string& name, int inputs, int outputs, const std::string& sha_key, const std::string& dsp_code)
+        JSONUIAux(const std::string& name,
+                  int inputs,
+                  int outputs,
+                  const std::string& sha_key,
+                  const std::string& dsp_code,
+                  const std::string& version,
+                  const std::string& options,
+                  const std::string& size,
+                  const std::map<std::string, int>& path_table)
         {
-            init(name, inputs, outputs, sha_key, dsp_code);
+            init(name, inputs, outputs, sha_key, dsp_code, size, version, options, path_table);
         }
 
         JSONUIAux(const std::string& name, int inputs, int outputs)
         {
-            init(name, inputs, outputs, "", "");
+            init(name, inputs, outputs, "", "", "", "", "", std::map<std::string, int>());
         }
 
         JSONUIAux(int inputs, int outputs)
         {
-            init("", inputs, outputs, "", "");
+            init("", inputs, outputs, "", "","", "", "", std::map<std::string, int>());
         }
         
         JSONUIAux()
         {
-            init("", -1, -1, "", "");
+            init("", -1, -1, "", "", "", "", "", std::map<std::string, int>());
         }
  
         virtual ~JSONUIAux() {}
@@ -195,11 +228,19 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
     
         virtual void addGenericButton(const char* label, const char* name)
         {
+            std::string path = buildPath(label);
+            std::string index = getAddressIndex(path);
+            
             fUI << fCloseUIPar;
             tab(fTab, fUI); fUI << "{";
             tab(fTab + 1, fUI); fUI << "\"type\": \"" << name << "\",";
             tab(fTab + 1, fUI); fUI << "\"label\": \"" << label << "\"" << ",";
-            tab(fTab + 1, fUI); fUI << "\"address\": \"" << buildPath(label) << "\"" << ((fMetaAux.size() > 0) ? "," : "");
+            tab(fTab + 1, fUI); fUI << "\"address\": \"" << path << "\"" << ",";
+            if (fPathTable.size() > 0) {
+                tab(fTab + 1, fUI); fUI << "\"index\": \"" << index << "\"" << ((fMetaAux.size() > 0) ? "," : "");
+            } else {
+                tab(fTab + 1, fUI); fUI << ((fMetaAux.size() > 0) ? "," : "");
+            }
             addMeta(fTab + 1, false);
             tab(fTab, fUI); fUI << "}";
             fCloseUIPar = ',';
@@ -217,11 +258,17 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
 
         virtual void addGenericEntry(const char* label, const char* name, REAL init, REAL min, REAL max, REAL step)
         {
+            std::string path = buildPath(label);
+            std::string index = getAddressIndex(path);
+            
             fUI << fCloseUIPar;
             tab(fTab, fUI); fUI << "{";
             tab(fTab + 1, fUI); fUI << "\"type\": \"" << name << "\",";
             tab(fTab + 1, fUI); fUI << "\"label\": \"" << label << "\"" << ",";
-            tab(fTab + 1, fUI); fUI << "\"address\": \"" << buildPath(label) << "\"" << ",";
+            tab(fTab + 1, fUI); fUI << "\"address\": \"" << path << "\"" << ",";
+            if (fPathTable.size() > 0) {
+                tab(fTab + 1, fUI); fUI << "\"index\": \"" << index << "\"" << ",";
+            }
             addMeta(fTab + 1);
             tab(fTab + 1, fUI); fUI << "\"init\": \"" << init << "\",";
             tab(fTab + 1, fUI); fUI << "\"min\": \"" << min << "\",";
@@ -250,11 +297,17 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
     
         virtual void addGenericBargraph(const char* label, const char* name, REAL min, REAL max) 
         {
+            std::string path = buildPath(label);
+            std::string index = getAddressIndex(path);
+            
             fUI << fCloseUIPar;
             tab(fTab, fUI); fUI << "{";
             tab(fTab + 1, fUI); fUI << "\"type\": \"" << name << "\",";
             tab(fTab + 1, fUI); fUI << "\"label\": \"" << label << "\"" << ",";
-            tab(fTab + 1, fUI); fUI << "\"address\": \"" << buildPath(label) << "\"" << ",";
+            tab(fTab + 1, fUI); fUI << "\"address\": \"" << path << "\"" << ",";
+            if (fPathTable.size() > 0) {
+                tab(fTab + 1, fUI); fUI << "\"index\": \"" << index << "\"" << ",";
+            }
             addMeta(fTab + 1);
             tab(fTab + 1, fUI); fUI << "\"min\": \"" << min << "\",";
             tab(fTab + 1, fUI); fUI << "\"max\": \"" << max << "\"";
@@ -294,6 +347,11 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
             fJSON << "{";
             fTab += 1;
             tab(fTab, fJSON); fJSON << "\"name\": \"" << fName << "\",";
+            if (fVersion != "") { tab(fTab, fJSON); fJSON << "\"version\": \"" << fVersion << "\","; }
+            if (fOptions != "") { tab(fTab, fJSON); fJSON << "\"options\": \"" << fOptions << "\","; }
+            tab(fTab, fJSON); fJSON << "\"version\": \"" << fVersion << "\",";
+            tab(fTab, fJSON); fJSON << "\"options\": \"" << fOptions << "\",";
+            if (fDSPSize != "") { tab(fTab, fJSON); fJSON << "\"size\": \"" << fDSPSize << "\","; }
             if (fSHAKey != "") { tab(fTab, fJSON); fJSON << "\"sha_key\": \"" << fSHAKey << "\","; }
             if (fExpandedCode != "") { tab(fTab, fJSON); fJSON << "\"code\": \"" << fExpandedCode << "\","; }
             tab(fTab, fJSON); fJSON << "\"inputs\": \"" << fInputs << "\","; 
@@ -318,8 +376,16 @@ class JSONUI : public JSONUIAux<FAUSTFLOAT>
 {
     public :
     
-        JSONUI(const std::string& name, int inputs, int outputs, const std::string& sha_key, const std::string& dsp_code):
-        JSONUIAux<FAUSTFLOAT>(name, inputs, outputs, sha_key, dsp_code)
+        JSONUI(const std::string& name,
+               int inputs,
+               int outputs,
+               const std::string& sha_key,
+               const std::string& dsp_code,
+               const std::string& version,
+               const std::string& options,
+               const std::string& size,
+               const std::map<std::string, int>& path_table):
+        JSONUIAux<FAUSTFLOAT>(name, inputs, outputs, sha_key, dsp_code, version, options, size, path_table)
         {}
         
         JSONUI(const std::string& name, int inputs, int outputs):

architecture/bela.cpp
@@ -60,7 +60,7 @@ using namespace std;
 #include "faust/gui/MidiUI.h"
 #include "faust/midi/bela-midi.h"
 
-const char * const pinNamesStrings[] =
+const char* const pinNamesStrings[] =
 {
   "ANALOG_0",
   "ANALOG_1",
@@ -122,8 +122,8 @@ enum EInputPin
 
 /**************************************************************************************
 
-  BelaWidget : object used by BelaUI to ensures the connection between an Bela parameter
-  and a faust widget
+  BelaWidget : object used by BelaUI to ensures the connection between a Bela parameter
+  and a Faust widget
 
 ***************************************************************************************/
 
@@ -133,8 +133,8 @@ protected:
   EInputPin fBelaPin;
   FAUSTFLOAT* fZone;  // Faust widget zone
   const char* fLabel; // Faust widget label
-  float fMin;         // Faust widget minimal value
-  float fRange;       // Faust widget value range (max-min)
+  FAUSTFLOAT fMin;         // Faust widget minimal value
+  FAUSTFLOAT fRange;       // Faust widget value range (max-min)
 
   public:
   BelaWidget()
@@ -153,7 +153,7 @@ protected:
   , fRange(w.fRange)
   {}
 
-  BelaWidget(EInputPin pin, FAUSTFLOAT* z, const char* l, float lo, float hi)
+  BelaWidget(EInputPin pin, FAUSTFLOAT* z, const char* l, FAUSTFLOAT lo, FAUSTFLOAT hi)
   : fBelaPin(pin)
   , fZone(z)
   , fLabel(l)
@@ -203,21 +203,21 @@ protected:
 /**************************************************************************************
 
   BelaUI : Faust User Interface builder. Passed to buildUserInterface BelaUI allows
-  the mapping between BELA pins and faust widgets. It relies on specific
+  the mapping between BELA pins and Faust widgets. It relies on specific
   metadata "...[BELA:DIGITAL_0]..." in the widget's label for that. For example any
-  faust widget with metadata [BELA:DIGITAL_0] will be controlled by DIGITAL_0
+  Faust widget with metadata [BELA:DIGITAL_0] will be controlled by DIGITAL_0
   (the second knob).
 
 ***************************************************************************************/
 
-// The maximun number of mappings between Bela parameters and faust widgets
+// The maximum number of mappings between Bela parameters and Faust widgets
 #define MAXBELAWIDGETS 8
 
 class BelaUI : public UI
 {
-  int fIndex; // number of BelaWidgets collected so far
-  EInputPin fBelaPin; // current pin id
-  BelaWidget fTable[MAXBELAWIDGETS]; // kind of static list of BelaWidgets
+  int fIndex;                           // number of BelaWidgets collected so far
+  EInputPin fBelaPin;                   // current pin id
+  BelaWidget fTable[MAXBELAWIDGETS];    // kind of static list of BelaWidgets
 
   // check if the widget is linked to a Bela parameter and, if so, add the corresponding BelaWidget
   void addBelaWidget(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT lo, FAUSTFLOAT hi)
@@ -268,14 +268,14 @@ class BelaUI : public UI
 
   // -- passive widgets
   virtual void addHorizontalBargraph(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT lo, FAUSTFLOAT hi) { skip(); }
-  virtual void addVerticalBargraph  (const char* label, FAUSTFLOAT* zone, FAUSTFLOAT lo, FAUSTFLOAT hi) { skip(); }
+  virtual void addVerticalBargraph(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT lo, FAUSTFLOAT hi) { skip(); }
 
 // -- metadata declarations
   virtual void declare(FAUSTFLOAT* z, const char* k, const char* id) {
     if (strcasecmp(k,"BELA") == 0) {
       for(int i=0;i<kNumInputPins;i++) {
         if (strcasecmp(id, pinNamesStrings[i]) == 0) {
-          fBelaPin = (EInputPin) i;
+          fBelaPin = (EInputPin)i;
         }
       }
     }
@@ -303,16 +303,16 @@ ztimedmap GUI::gTimedZoneMap;
 
 ***************************************************************************************/
 
-unsigned int gNumBuffers = 0; // the number of de-interleaved buffers for audio and analog i/o
-float *gInputBuffers = NULL, *gOutputBuffers = NULL; //de-interleaved input/output buffers for the audio and analog inputs
-float* gFaustIns[10]; // array of pointers to gInputBuffer data
-float* gFaustOuts[10]; // array of pointers to gOutputBuffers data
+unsigned int gNumBuffers = 0;   // the number of de-interleaved buffers for audio and analog i/o
+FAUSTFLOAT *gInputBuffers = NULL, *gOutputBuffers = NULL; //de-interleaved input/output buffers for the audio and analog inputs
+FAUSTFLOAT* gFaustIns[10];      // array of pointers to gInputBuffer data
+FAUSTFLOAT* gFaustOuts[10];     // array of pointers to gOutputBuffers data
 mydsp fDSP;
 BelaUI fUI;
 bela_midi fMIDI;
 MidiUI* fMidiUI;
 
-bool setup(BelaContext *context, void *userData)
+bool setup(BelaContext* context, void* userData)
 {
   gNumBuffers = context->audioInChannels
                 + context->audioOutChannels
@@ -326,8 +326,8 @@ bool setup(BelaContext *context, void *userData)
   fDSP.init(context->audioSampleRate);
   fDSP.buildUserInterface(&fUI); // Maps Bela Analog/Digital IO and faust widgets
 
-  gInputBuffers = (float *) malloc(gNumBuffers * context->audioFrames * sizeof(float));
-  gOutputBuffers = (float *) malloc(gNumBuffers * context->audioFrames * sizeof(float));
+  gInputBuffers = (FAUSTFLOAT *) malloc(gNumBuffers * context->audioFrames * sizeof(FAUSTFLOAT));
+  gOutputBuffers = (FAUSTFLOAT *) malloc(gNumBuffers * context->audioFrames * sizeof(FAUSTFLOAT));
 
   if(gInputBuffers == NULL || gOutputBuffers == NULL)
   {
@@ -355,7 +355,7 @@ bool setup(BelaContext *context, void *userData)
   return true;
 }
 
-void render(BelaContext *context, void *userData)
+void render(BelaContext* context, void* userData)
 {
   // De-interleave the input data
   for(unsigned int frame = 0; frame < context->audioFrames; frame++)
@@ -367,7 +367,7 @@ void render(BelaContext *context, void *userData)
       if(ch >= context->audioInChannels) // handle analogChannels
       {
         unsigned int m = frame/2;
-        float mIn = (float) context->analogIn[m * context->analogInChannels + (ch-context->audioInChannels)];
+        FAUSTFLOAT mIn = (FAUSTFLOAT) context->analogIn[m * context->analogInChannels + (ch-context->audioInChannels)];
         gInputBuffers[ch * context->audioFrames + frame] = mIn;
       }
       else // handle audioInChannels
@@ -405,7 +405,7 @@ void render(BelaContext *context, void *userData)
   }
 }
 
-void cleanup(BelaContext *context, void *userData)
+void cleanup(BelaContext* context, void* userData)
 {
   if(gInputBuffers != NULL)
     free(gInputBuffers);

commit a01b09f4ccd74cb185f17b13fdeab4d34436eaea

Correct JSONUI class, after regression introduced in abc20108fbcbfb4662077d065b830932555971cc.

architecture/faust/gui/JSONUI.h
@@ -230,7 +230,7 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
         }
     
         // -- active widgets
-    
+  
         virtual void addGenericButton(const char* label, const char* name)
         {
             std::string path = buildPath(label);
@@ -240,11 +240,11 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
             tab(fTab, fUI); fUI << "{";
             tab(fTab + 1, fUI); fUI << "\"type\": \"" << name << "\",";
             tab(fTab + 1, fUI); fUI << "\"label\": \"" << label << "\"" << ",";
-            tab(fTab + 1, fUI); fUI << "\"address\": \"" << path << "\"" << ",";
+            tab(fTab + 1, fUI); fUI << "\"address\": \"" << path << "\"";
             if (fPathTable.size() > 0) {
                 tab(fTab + 1, fUI); fUI << "\"index\": \"" << index << "\"" << ((fMetaAux.size() > 0) ? "," : "");
             } else {
-                tab(fTab + 1, fUI); fUI << ((fMetaAux.size() > 0) ? "," : "");
+                fUI << ((fMetaAux.size() > 0) ? "," : "");
             }
             addMeta(fTab + 1, false);
             tab(fTab, fUI); fUI << "}";

compiler/Makefile.unix
@@ -528,7 +528,7 @@ generator/code_container.o: parser/sourcereader.hh boxes/boxes.hh evaluate/loopD
 generator/code_container.o: generator/floats.hh generator/function_builder.hh parallelize/code_loop.hh
 generator/code_container.o: generator/json_instructions.hh dsp_factory.hh generator/export.hh signals/recursivness.hh
 generator/code_container.o: generator/type_manager.hh generator/text_instructions.hh generator/fir_to_fir.hh
-generator/code_container.o: generator/fir/fir_instructions.hh
+generator/code_container.o: generator/fir/fir_instructions.hh generator/struct_manager.hh
 generator/contextor.o: generator/contextor.hh errors/exception.hh
 generator/dag_instructions_compiler.o: generator/dag_instructions_compiler.hh generator/instructions_compiler.hh
 generator/dag_instructions_compiler.o: global.hh parser/sourcereader.hh boxes/boxes.hh tlib/tlib.hh tlib/symbol.hh
@@ -816,13 +816,14 @@ libcode.o: generator/cpp/cpp_gpu_code_container.hh generator/fir/fir_code_contai
 libcode.o: generator/interpreter/interpreter_code_container.cpp generator/interpreter/interpreter_code_container.hh
 libcode.o: generator/interpreter/interpreter_dsp_aux.hh generator/interpreter/fir_interpreter.hh
 libcode.o: generator/interpreter/interpreter_bytecode.hh generator/interpreter/interpreter_optimizer.hh
-libcode.o: generator/dsp_aux.hh generator/interpreter/interpreter_instructions.hh generator/java/java_code_container.hh
-libcode.o: generator/java/java_instructions.hh generator/js/js_code_container.hh generator/js/js_instructions.hh
-libcode.o: generator/llvm/llvm_code_container.hh generator/llvm/llvm_instructions.hh generator/llvm/llvm_dsp_aux.hh
-libcode.o: generator/smartpointer.h utils/TMutex.h generator/llvm/clang_code_container.hh
-libcode.o: generator/wasm/wasm_code_container.hh generator/wasm/wasm_instructions.hh generator/wasm/was_instructions.hh
-libcode.o: generator/wasm/wast_code_container.hh generator/wasm/wast_instructions.hh
-libcode.o: generator/rust/rust_code_container.hh generator/rust/rust_instructions.hh
+libcode.o: generator/dsp_aux.hh generator/interpreter/interpreter_instructions.hh generator/struct_manager.hh
+libcode.o: generator/java/java_code_container.hh generator/java/java_instructions.hh generator/js/js_code_container.hh
+libcode.o: generator/js/js_instructions.hh generator/llvm/llvm_code_container.hh generator/llvm/llvm_instructions.hh
+libcode.o: generator/llvm/llvm_dsp_aux.hh generator/smartpointer.h utils/TMutex.h
+libcode.o: generator/llvm/clang_code_container.hh generator/wasm/wasm_code_container.hh
+libcode.o: generator/wasm/wasm_instructions.hh generator/wasm/was_instructions.hh generator/wasm/wast_code_container.hh
+libcode.o: generator/wasm/wast_instructions.hh generator/rust/rust_code_container.hh
+libcode.o: generator/rust/rust_instructions.hh
 global.o: global.hh parser/sourcereader.hh boxes/boxes.hh tlib/tlib.hh tlib/symbol.hh garbageable.hh
 global.o: errors/exception.hh tlib/node.hh tlib/tree.hh tlib/num.hh tlib/list.hh tlib/shlysis.hh signals/signals.hh
 global.o: signals/binop.hh generator/interpreter/fir_opcode.hh tlib/property.hh signals/sigtype.hh tlib/smartpointer.hh
@@ -841,8 +842,9 @@ global.o: generator/vec_code_container.hh generator/omp_code_container.hh genera
 global.o: generator/cpp/cpp_code_container.hh generator/cpp/cpp_instructions.hh generator/cpp/opencl_instructions.hh
 global.o: generator/fir/fir_code_container.hh generator/interpreter/interpreter_instructions.hh
 global.o: generator/interpreter/fir_interpreter.hh generator/interpreter/interpreter_bytecode.hh
-global.o: generator/java/java_code_container.hh generator/java/java_instructions.hh generator/js/js_code_container.hh
-global.o: generator/js/js_instructions.hh generator/rust/rust_code_container.hh generator/rust/rust_instructions.hh
+global.o: generator/struct_manager.hh generator/java/java_code_container.hh generator/java/java_instructions.hh
+global.o: generator/js/js_code_container.hh generator/js/js_instructions.hh generator/rust/rust_code_container.hh
+global.o: generator/rust/rust_instructions.hh
 generator/asmjs/asmjs_code_container.o: generator/asmjs/asmjs_code_container.hh generator/code_container.hh
 generator/asmjs/asmjs_code_container.o: generator/instructions.hh signals/binop.hh tlib/node.hh tlib/symbol.hh
 generator/asmjs/asmjs_code_container.o: garbageable.hh errors/exception.hh generator/interpreter/fir_opcode.hh
@@ -931,7 +933,8 @@ generator/interpreter/interpreter_code_container.o: generator/interpreter/interp
 generator/interpreter/interpreter_code_container.o: generator/interpreter/interpreter_instructions.hh
 generator/interpreter/interpreter_code_container.o: generator/typing_instructions.hh generator/fir_to_fir.hh
 generator/interpreter/interpreter_code_container.o: generator/fir/fir_instructions.hh generator/type_manager.hh
-generator/interpreter/interpreter_code_container.o: generator/instructions_compiler.hh generator/occurences.hh
+generator/interpreter/interpreter_code_container.o: generator/struct_manager.hh generator/instructions_compiler.hh
+generator/interpreter/interpreter_code_container.o: generator/occurences.hh
 generator/interpreter/interpreter_dsp_aux.o: tlib/compatibility.hh generator/interpreter/interpreter_dsp_aux.hh
 generator/interpreter/interpreter_dsp_aux.o: generator/interpreter/fir_interpreter.hh
 generator/interpreter/interpreter_dsp_aux.o: generator/interpreter/interpreter_bytecode.hh
@@ -997,6 +1000,7 @@ generator/wasm/wasm_code_container.o: generator/export.hh generator/wasm/wasm_in
 generator/wasm/wasm_code_container.o: generator/wasm/was_instructions.hh generator/text_instructions.hh
 generator/wasm/wasm_code_container.o: generator/type_manager.hh generator/fir_to_fir.hh
 generator/wasm/wasm_code_container.o: generator/fir/fir_instructions.hh generator/typing_instructions.hh
+generator/wasm/wasm_code_container.o: generator/struct_manager.hh
 generator/wasm/wasm_dsp_aux.o: tlib/compatibility.hh generator/wasm/wasm_dsp_aux.hh generator/export.hh
 generator/wasm/wasm_dsp_aux.o: generator/dsp_aux.hh errors/exception.hh dsp_factory.hh generator/Text.hh
 generator/wasm/wast_code_container.o: generator/wasm/wast_code_container.hh generator/code_container.hh
@@ -1012,6 +1016,7 @@ generator/wasm/wast_code_container.o: generator/export.hh generator/wasm/wast_in
 generator/wasm/wast_code_container.o: generator/wasm/was_instructions.hh generator/text_instructions.hh
 generator/wasm/wast_code_container.o: generator/type_manager.hh generator/fir_to_fir.hh
 generator/wasm/wast_code_container.o: generator/fir/fir_instructions.hh generator/typing_instructions.hh
+generator/wasm/wast_code_container.o: generator/struct_manager.hh
 generator/rust/rust_code_container.o: generator/rust/rust_code_container.hh generator/code_container.hh
 generator/rust/rust_code_container.o: generator/instructions.hh signals/binop.hh tlib/node.hh tlib/symbol.hh
 generator/rust/rust_code_container.o: garbageable.hh errors/exception.hh generator/interpreter/fir_opcode.hh