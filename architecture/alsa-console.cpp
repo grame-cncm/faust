ed0553a23048158dea45107a67d1770f7239fc27
Romain Michon <rmnmichon@gmail.com>
2018-03-09 15:00:55 -0800

    Adding basic MIDI and polyphonu support to faust2alsaconsole

--- architecture/alsa-console.cpp
+++ architecture/alsa-console.cpp
@@ -1,8 +1,8 @@
 /************************************************************************
 
-	IMPORTANT NOTE : this file contains two clearly delimited sections : 
-	the ARCHITECTURE section (in two parts) and the USER section. Each section 
-	is governed by its own copyright and license. Please check individually 
+	IMPORTANT NOTE : this file contains two clearly delimited sections :
+	the ARCHITECTURE section (in two parts) and the USER section. Each section
+	is governed by its own copyright and license. Please check individually
 	each section for license and copyright information.
 *************************************************************************/
 
@@ -12,9 +12,9 @@
     FAUST Architecture File
     Copyright (C) 2003-2016 GRAME, Centre National de Creation Musicale
     ---------------------------------------------------------------------
-    This Architecture section is free software; you can redistribute it 
-    and/or modify it under the terms of the GNU General Public License 
-    as published by the Free Software Foundation; either version 3 of 
+    This Architecture section is free software; you can redistribute it
+    and/or modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 3 of
     the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
@@ -22,13 +22,13 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License 
+    You should have received a copy of the GNU General Public License
     along with this program; If not, see <http://www.gnu.org/licenses/>.
 
-    EXCEPTION : As a special exception, you may create a larger work 
-    that contains this FAUST architecture section and distribute  
-    that work under terms of your choice, so long as this FAUST 
-    architecture section is not modified. 
+    EXCEPTION : As a special exception, you may create a larger work
+    that contains this FAUST architecture section and distribute
+    that work under terms of your choice, so long as this FAUST
+    architecture section is not modified.
 
  ************************************************************************
  ************************************************************************/
@@ -53,6 +53,14 @@
 #include "faust/gui/httpdUI.h"
 #endif
 
+// Always include this file, otherwise -poly only mode does not compile....
+#include "faust/gui/MidiUI.h"
+
+#ifdef MIDICTRL
+#include "faust/midi/rt-midi.h"
+#include "faust/midi/RtMidi.cpp"
+#endif
+
 /**************************BEGIN USER SECTION **************************/
 /******************************************************************************
 *******************************************************************************
@@ -66,10 +74,14 @@
 
 <<includeclass>>
 
+#ifdef POLY
+#include "faust/dsp/poly-dsp.h"
+#endif
+
 /***************************END USER SECTION ***************************/
 
 /*******************BEGIN ARCHITECTURE SECTION (part 2/2)***************/
-					
+
 mydsp* DSP;
 
 std::list<GUI*> GUI::fGuiList;
@@ -84,7 +96,7 @@ int main(int argc, char *argv[] )
     char  rcfilename[256];
 	char* home = getenv("HOME");
 	snprintf(rcfilename, 255, "%s/.%src", home, appname);
-	
+
 	DSP = new mydsp();
 	if (DSP==0) {
         std::cerr << "Unable to allocate Faust DSP object" << std::endl;
@@ -109,21 +121,21 @@ int main(int argc, char *argv[] )
 
 	alsaaudio audio (argc, argv, DSP);
 	audio.init(appname, DSP);
-	finterface->recallState(rcfilename);	
+	finterface->recallState(rcfilename);
 	audio.start();
-	
+
 #ifdef HTTPCTRL
 	httpdinterface->run();
 #endif
-	
+
 #ifdef OSCCTRL
 	oscinterface->run();
 #endif
 	interface->run();
-	
+
 	audio.stop();
 	finterface->saveState(rcfilename);
-    
+
     // desallocation
     delete interface;
     delete finterface;
@@ -137,4 +149,3 @@ int main(int argc, char *argv[] )
   	return 0;
 }
 /********************END ARCHITECTURE SECTION (part 2/2)****************/
-
