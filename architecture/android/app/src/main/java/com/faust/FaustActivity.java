341bd3c562b4dcb9271a82bc939313613bc59244
Romain Michon <rmnmichon@gmail.com>
2018-08-28 15:44:56 +0200

    Fixing tiny bug in faust2android

--- architecture/android/app/src/main/java/com/faust/FaustActivity.java
+++ architecture/android/app/src/main/java/com/faust/FaustActivity.java
@@ -159,7 +159,7 @@ public class FaustActivity extends Activity {
                 try {
                     String[] assetsList = assets.list("");
                     for (int i = 0; i < assetsList.length; i++) {
-                        if (assetsList[i].contains(".aif") || assetsList[i].contains(".wav") || internalStorageList[i].contains(".flac")) {
+                        if (assetsList[i].contains(".aif") || assetsList[i].contains(".wav") || assetsList[i].contains(".flac")) {
                             InputStream in = null;
                             OutputStream out = null;
                             in = assets.open(assetsList[i]);
