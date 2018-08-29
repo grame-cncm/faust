3e610e908853bca1618d0d7604d8519b6da6f68d
Romain Michon <rmnmichon@gmail.com>
2018-08-29 14:33:05 +0200

    Tiny fix in Android

--- architecture/android/app/src/main/java/com/faust/Radio.java
+++ architecture/android/app/src/main/java/com/faust/Radio.java
@@ -2,6 +2,7 @@ package com.faust;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
