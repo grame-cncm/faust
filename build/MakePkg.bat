bfdedd5f91499f94125f494a4801400d7b4556cb
Dominique Fober <fober@grame.fr>
2018-11-26 14:10:13 +0100

    fix incorrect folder name

--- build/MakePkg.bat
+++ build/MakePkg.bat
@@ -1,9 +1,6 @@
-/rem
-/rem This simple script is intended to package faust without Makefile
-/rem
 
 echo "Warning, this script must be called from the build folder (e.g. faustdir)"
-cmake -DUSE_LLVM_CONFIG=off -DPACK=on -C ../bakckends/most.cmake -C ../targets/most.cmake ..
+cmake -DUSE_LLVM_CONFIG=off -DPACK=on -C ../backends/most.cmake -C ../targets/most.cmake ..
 cmake --build . --config Release --  /maxcpucount:4
 cpack -G NSIS64
 move Faust-*.exe ..
