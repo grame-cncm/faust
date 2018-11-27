fbccd2f28543f7da75b7b9879584559fb6c28619
Stephane Letz <letz@grame.fr>
2018-11-27 17:04:52 +0100

    Raise faustgen~ version.

--- embedded/faustgen/src/faustgen~.h
+++ embedded/faustgen/src/faustgen~.h
@@ -60,7 +60,7 @@
 #include "ext_drag.h"
 
 #define DEFAULT_SOURCE_CODE "import(\"stdfaust.lib\");\nprocess=_,_;"
-#define FAUSTGEN_VERSION "1.30"
+#define FAUSTGEN_VERSION "1.31"
 #define FAUST_PDF_DOCUMENTATION "faust-quick-reference.pdf"
 #define FAUST_PDF_LIBRARY "library.pdf"
 
