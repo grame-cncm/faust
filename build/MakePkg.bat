abbd75d16ad76c60ab5bef272e41bf548c09d92c
Dominique Fober <fober@grame.fr>
2019-03-05 09:35:32 +0100

    improve packaging script

--- build/MakePkg.bat
+++ build/MakePkg.bat
@@ -1,6 +1,15 @@
 
-echo "Warning, this script must be called from the build folder (e.g. faustdir)"
+@echo off
+
+IF NOT EXIST faust.sln (
+	echo "Warning, this script must be called from the build folder (e.g. faustdir)"
+	GOTO END
+)
+
+
 cmake -DUSE_LLVM_CONFIG=off -DPACK=on -C ../backends/most.cmake -C ../targets/most.cmake ..
 cmake --build . --config Release --  /maxcpucount:4
 cpack -G NSIS64
 move Faust-*.exe ..
+
+:END
