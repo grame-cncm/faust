9f980af1bf4812cf61660debc089f79d5eed1a86
Stephane Letz <letz@grame.fr>
2019-03-31 09:26:53 +0200

    Simplify stackOverflowDetector. Format and cleanup QTUI.h

--- compiler/evaluate/eval.cpp
+++ compiler/evaluate/eval.cpp
@@ -266,8 +266,7 @@ static Tree eval(Tree exp, Tree visited, Tree localValEnv)
 
     if (!getEvalProperty(exp, localValEnv, result)) {
         gGlobal->gLoopDetector.detect(cons(exp, localValEnv));
-        int stack = 0;
-        gGlobal->gStackOverflowDetector.detect(&stack);
+        gGlobal->gStackOverflowDetector.detect();
         // cerr << "ENTER eval("<< *exp << ") with env " << *localValEnv << endl;
         result = realeval(exp, visited, localValEnv);
         setEvalProperty(exp, localValEnv, result);
