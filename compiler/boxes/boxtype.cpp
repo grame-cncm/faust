4989d3600f290fd5b79548b45e84c7d591200181
Yann Orlarey <orlarey@gmail.com>
2019-06-26 18:40:51 +0200

    [NEW] Better messages for block-diagram composition errors.
    Use the defNameProperty, when available, to improve block-diagram composition error messages (suggested by DS and JOS).

--- compiler/boxes/boxtype.cpp
+++ compiler/boxes/boxtype.cpp
@@ -40,6 +40,7 @@
 #include "boxes.hh"
 #include "exception.hh"
 #include "global.hh"
+#include "names.hh"
 #include "ppbox.hh"
 #include "prim2.hh"
 #include "xtended.hh"
@@ -75,6 +76,95 @@ bool getBoxType(Tree box, int* inum, int* onum)
             return false;
         }
     }
+}
+
+
+/**
+ * Produces "1 output" or "n outputs" according to n
+ * \param n: the number of outputs
+ * \return the corresponding string
+ */
+
+static string outputs(int n)
+{
+    stringstream msg;
+    msg << n << ((n == 1) ? " output" : " outputs");
+    return msg.str();
+}
+
+
+/**
+ * Produces "1 input" or "n inputs" according to n
+ * \param n: the number of inputs
+ * \return the corresponding string
+ */
+
+static string inputs(int n)
+{
+    stringstream msg;
+    msg << n << ((n == 1) ? " input" : " inputs");
+    return msg.str();
+}
+
+/**
+ * Compute a parametric type error message
+ * \param a: the LHS expression
+ * \param b: the RHS expression
+ * \param o: the number of outputs of a
+ * \param i: the number of inputs of b
+ * \param opcode = the composition operation sign
+ * \param opname = the full name of the composition operation
+ * \param msg  = indication for the user (ie " must be equals to ")
+ * \return the error message as a string
+ */
+
+static string computeTypeErrorMessage(Tree a, Tree b, int o, int i, const string opcode, const string opname,
+                                      const string msg)
+{
+    stringstream error;
+    string       aStr("A"), bStr("B");
+    Tree         aID, bID;
+    if (getDefNameProperty(a, aID)) aStr = tree2str(aID);
+    if (getDefNameProperty(b, bID)) bStr = tree2str(bID);
+    error << "ERROR in " << opname << " " << aStr << opcode << bStr << endl
+          << "The number of outputs [" << o << "] of " << aStr << msg << "the number of inputs [" << i << "] of "
+          << bStr << endl
+          << endl
+          << "Here  " << aStr << " = " << boxpp(a) << "; has " << outputs(o) << endl
+          << "while " << bStr << " = " << boxpp(b) << "; has " << inputs(i) << endl;
+    return error.str();
+}
+
+
+/**
+ * Compute a parametric type error message for recursions
+ * \param a: the LHS expression
+ * \param b: the RHS expression
+ * \param u: the number of inputs of a
+ * \param v: the number of outputs of a
+ * \param x: the number of inputs of b
+ * \param y: the number of outputs of b
+ * \return the error message as a string
+ */
+
+static string computeTypeRecErrorMessage(Tree a, Tree b, int u, int v, int x, int y)
+{
+    stringstream msg;
+    string       aStr("A"), bStr("B");
+    Tree         aID, bID;
+
+    if (getDefNameProperty(a, aID)) aStr = tree2str(aID);
+    if (getDefNameProperty(b, bID)) bStr = tree2str(bID);
+
+    
+    msg << "ERROR in recursive composition " << aStr << '~' << bStr << endl;
+    if (v < x)
+        msg << "The number of outputs [" << v << "] of " << aStr << " must be at least the number of inputs [" << x << "] of " << bStr << ". ";
+    if (u < y)
+        msg << "The number of inputs [" << u << "] of " << aStr << " must be at least the number of outputs [" << y << "] of " << bStr << ". ";
+
+    msg << endl << endl << "Here  " << aStr << " = " << boxpp(a) << "; has " << inputs(u) << " and " << outputs(v) << "," << endl << "while " << bStr << " = " << boxpp(b) << "; has " << inputs(x) << " and " << outputs(y) << "." << endl;
+    return msg.str();
 }
 
 /**
@@ -200,11 +290,8 @@ static bool infereBoxType(Tree t, int* inum, int* onum)
         if (!getBoxType(b, &x, &y)) return false;
 
         if (v != x) {
-            stringstream error;
-            error << "ERROR in sequential composition (A:B)" << endl
-                  << "The number of outputs (" << v << ") of A = " << boxpp(a) << endl
-                  << "must be equal to the number of inputs (" << x << ") of B : " << boxpp(b) << endl;
-            throw faustexception(error.str());
+            throw faustexception(
+                computeTypeErrorMessage(a, b, v, x, ":", "sequential composition", " must be equals to "));
         } else {
             *inum = u;
             *onum = y;
@@ -223,27 +310,9 @@ static bool infereBoxType(Tree t, int* inum, int* onum)
         if (!getBoxType(a, &u, &v)) return false;
         if (!getBoxType(b, &x, &y)) return false;
 
-        if (v == 0) {
-            stringstream error;
-            error << "Connection error in : " << boxpp(t) << endl
-                  << "The first expression : " << boxpp(a) << " has no outputs" << endl;
-            throw faustexception(error.str());
-        }
-
-        if (x == 0) {
-            stringstream error;
-            error << "Connection error in : " << boxpp(t) << endl
-                  << "The second expression : " << boxpp(b) << " has no inputs" << endl;
-            throw faustexception(error.str());
-        }
-
-        if (x % v != 0) {
-            stringstream error;
-            error << "Connection error in : " << boxpp(t) << endl
-                  << "The number of outputs (" << v
-                  << ") of the first expression should be a divisor of the number of inputs (" << x
-                  << ") of the second expression" << endl;
-            throw faustexception(error.str());
+        if ((v == 0) || (x == 0) || (x % v != 0)) {
+            throw faustexception(
+                (computeTypeErrorMessage(a, b, v, x, "<:", "split composition", " must be a divisor of ")));
         }
 
         *inum = u;
@@ -254,27 +323,9 @@ static bool infereBoxType(Tree t, int* inum, int* onum)
         if (!getBoxType(a, &u, &v)) return false;
         if (!getBoxType(b, &x, &y)) return false;
 
-        if (v == 0) {
-            stringstream error;
-            error << "Connection error in : " << boxpp(t) << endl
-                  << "The first expression : " << boxpp(a) << " has no outputs" << endl;
-            throw faustexception(error.str());
-        }
-
-        if (x == 0) {
-            stringstream error;
-            error << "Connection error in : " << boxpp(t) << endl
-                  << "The second expression : " << boxpp(b) << " has no inputs" << endl;
-            throw faustexception(error.str());
-        }
-
-        if (v % x != 0) {
-            stringstream error;
-            error << "Connection error in : " << boxpp(t) << endl
-                  << "The number of outputs (" << v
-                  << ") of the first expression should be a multiple of the number of inputs (" << x
-                  << ") of the second expression" << endl;
-            throw faustexception(error.str());
+        if ((v == 0) || (x == 0) || (v % x != 0)) {
+            throw faustexception(
+                computeTypeErrorMessage(a, b, v, x, ":>", "merge composition", " must be a multiple of "));
         }
 
         *inum = u;
@@ -285,17 +336,7 @@ static bool infereBoxType(Tree t, int* inum, int* onum)
         if (!getBoxType(a, &u, &v)) return false;
         if (!getBoxType(b, &x, &y)) return false;
         if ((x > v) || (y > u)) {
-            stringstream error;
-            error << "Connection error in : " << boxpp(t) << endl;
-            if (x > v)
-                error << "The number of outputs (" << v
-                      << ") of the first expression should be greater or equal to the number of inputs (" << x
-                      << ") of the second expression" << endl;
-            if (y > u)
-                error << "The number of inputs (" << u
-                      << ") of the first expression should be greater or equal to the number of outputs (" << y
-                      << ") of the second expression" << endl;
-            throw faustexception(error.str());
+            throw faustexception(computeTypeRecErrorMessage(a, b, u, v, x, y));
         }
         *inum = max(0, u - y);
         *onum = v;
