grame-cncm/faust

commit 5422fd464f8bdce17abf3004654eedd9fcc13dfb

Update JSONUI, SimpleParser and JSONUIDecoder.

architecture/faust/gui/JSONUI.h
@@ -29,6 +29,7 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <algorithm>
 
@@ -42,7 +43,7 @@
  ******************************************************************************/
 
 template <typename REAL>
-class JSONUIAux : public PathBuilder, public Meta, public UI
+class JSONUIAux : public PathBuilder, public Meta, public UIReal<REAL>
 {
 
     protected:
@@ -168,6 +169,9 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
                   const std::map<std::string, int>& path_table)
         {
             fTab = 1;
+            
+            fUI << std::setprecision(std::numeric_limits<REAL>::max_digits10);
+            fMeta << std::setprecision(std::numeric_limits<REAL>::max_digits10);
             
             // Start Meta generation
             fMeta.str("");
@@ -382,6 +386,7 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
         {
             fTab = 0;
             std::stringstream JSON;
+            JSON << std::setprecision(std::numeric_limits<REAL>::max_digits10);
             JSON << "{";
             fTab += 1;
             tab(fTab, JSON); JSON << "\"name\": \"" << fName << "\",";
@@ -428,7 +433,7 @@ class JSONUIAux : public PathBuilder, public Meta, public UI
 
 // Externally available class using FAUSTFLOAT
 
-class JSONUI : public JSONUIAux<FAUSTFLOAT>
+class JSONUI : public JSONUIAux<FAUSTFLOAT>, public UI
 {
     public :
     
@@ -463,6 +468,76 @@ class JSONUI : public JSONUIAux<FAUSTFLOAT>
         
         JSONUI():JSONUIAux<FAUSTFLOAT>()
         {}
+    
+        virtual void openTabBox(const char* label)
+        {
+            JSONUIAux<FAUSTFLOAT>::openTabBox(label);
+        }
+        virtual void openHorizontalBox(const char* label)
+        {
+            JSONUIAux<FAUSTFLOAT>::openTabBox(label);
+        }
+        virtual void openVerticalBox(const char* label)
+        {
+            JSONUIAux<FAUSTFLOAT>::openVerticalBox(label);
+        }
+        virtual void closeBox()
+        {
+            JSONUIAux<FAUSTFLOAT>::closeBox();
+        }
+        
+        // -- active widgets
+        
+        virtual void addButton(const char* label, FAUSTFLOAT* zone)
+        {
+            JSONUIAux<FAUSTFLOAT>::addButton(label, zone);
+        }
+        virtual void addCheckButton(const char* label, FAUSTFLOAT* zone)
+        {
+            JSONUIAux<FAUSTFLOAT>::addCheckButton(label, zone);
+        }
+        virtual void addVerticalSlider(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step)
+        {
+            JSONUIAux<FAUSTFLOAT>::addVerticalSlider(label, zone, init, min, max, step);
+        }
+        virtual void addHorizontalSlider(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step)
+        {
+            JSONUIAux<FAUSTFLOAT>::addHorizontalSlider(label, zone, init, min, max, step);
+        }
+        virtual void addNumEntry(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step)
+        {
+            JSONUIAux<FAUSTFLOAT>::addNumEntry(label, zone, init, min, max, step);
+        }
+        
+        // -- passive widgets
+        
+        virtual void addHorizontalBargraph(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max)
+        {
+            JSONUIAux<FAUSTFLOAT>::addHorizontalBargraph(label, zone, min, max);
+        }
+        virtual void addVerticalBargraph(const char* label, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max)
+        {
+            JSONUIAux<FAUSTFLOAT>::addVerticalBargraph(label, zone, min, max);
+        }
+        
+        // -- soundfiles
+        
+        virtual void addSoundfile(const char* label, const char* filename, Soundfile** sf_zone)
+        {
+            JSONUIAux<FAUSTFLOAT>::addSoundfile(label, filename, sf_zone);
+        }
+        
+        // -- metadata declarations
+        
+        virtual void declare(FAUSTFLOAT* zone, const char* key, const char* val)
+        {
+            JSONUIAux<FAUSTFLOAT>::declare(zone, key, val);
+        }
+    
+        virtual void declare(const char* key, const char* val)
+        {
+            JSONUIAux<FAUSTFLOAT>::declare(key, val);
+        }
     
         virtual ~JSONUI() {}
     

architecture/faust/gui/JSONUIDecoder.h
@@ -92,9 +92,7 @@ struct JSONUIDecoderAux {
     };
 
     typedef std::vector<ExtZoneParam*> controlMap;
-    
-    REAL STR2REAL(const std::string& str) { return REAL(std::strtod(str.c_str(), nullptr)); }
- 
+  
     std::string fName;
     std::string fFileName;
     std::string fJSON;
@@ -110,7 +108,7 @@ struct JSONUIDecoderAux {
     Soundfile** fSoundfiles;
     
     int fNumInputs, fNumOutputs, fSRIndex;
-    int fInputItems, fOutputItems, fSoundfileItems;
+    int fSoundfileItems;
     int fDSPSize;
     
     controlMap fPathInputTable;     // [path, ZoneParam]
@@ -123,14 +121,14 @@ struct JSONUIDecoderAux {
     bool isOutput(const std::string& type) { return (type == "hbargraph" || type == "vbargraph"); }
     bool isSoundfile(const std::string& type) { return (type == "soundfile"); }
     
-    std::string getString(std::map<std::string, std::string >& map, const std::string& key)
+    std::string getString(std::map<std::string, std::pair<std::string, double> >& map, const std::string& key)
     {
-        return (map.find(key) != map.end()) ? map[key] : "";
+        return (map.find(key) != map.end()) ? map[key].first : "";
     }
     
-    int getInt(std::map<std::string, std::string >& map, const std::string& key)
+    int getInt(std::map<std::string, std::pair<std::string, double> >& map, const std::string& key)
     {
-        return (map.find(key) != map.end()) ? std::atoi(map[key].c_str()) : -1;
+        return (map.find(key) != map.end()) ? int(map[key].second) : -1;
     }
     
     void setReflectZoneFun(int index, ReflectFunction fun)
@@ -147,15 +145,15 @@ struct JSONUIDecoderAux {
     {
         fJSON = json;
         const char* p = fJSON.c_str();
-        std::map<std::string, std::string > meta_data0;
+        std::map<std::string, std::pair<std::string, double> > meta_data1;
         std::map<std::string, std::vector<std::string> > meta_data2;
-        parseJson(p, meta_data0, fMetadata, meta_data2, fUiItems);
+        parseJson(p, meta_data1, fMetadata, meta_data2, fUiItems);
         
-        // meta_data0 contains <name : val>, <inputs : val>, <ouputs : val> pairs etc...
-        fName = getString(meta_data0, "name");
-        fFileName = getString(meta_data0, "filename");
-        fVersion = getString(meta_data0, "version");
-        fCompileOptions = getString(meta_data0, "compile_options");
+        // meta_data1 contains <name : val>, <inputs : val>, <ouputs : val> pairs etc...
+        fName = getString(meta_data1, "name");
+        fFileName = getString(meta_data1, "filename");
+        fVersion = getString(meta_data1, "version");
+        fCompileOptions = getString(meta_data1, "compile_options");
         
         if (meta_data2.find("library_list") != meta_data2.end()) {
             fLibraryList = meta_data2["library_list"];
@@ -164,22 +162,15 @@ struct JSONUIDecoderAux {
             fIncludePathnames = meta_data2["include_pathnames"];
         }
         
-        fDSPSize = getInt(meta_data0, "size");
-        fNumInputs = getInt(meta_data0, "inputs");
-        fNumOutputs = getInt(meta_data0, "outputs");
-        fSRIndex = getInt(meta_data0, "sr_index");
+        fDSPSize = getInt(meta_data1, "size");
+        fNumInputs = getInt(meta_data1, "inputs");
+        fNumOutputs = getInt(meta_data1, "outputs");
+        fSRIndex = getInt(meta_data1, "sr_index");
        
-        fInputItems = 0;
-        fOutputItems = 0;
         fSoundfileItems = 0;
-        
         for (auto& it : fUiItems) {
             std::string type = it.type;
-            if (isInput(type)) {
-                fInputItems++;
-            } else if (isOutput(type)) {
-                fOutputItems++;          
-            } else if (isSoundfile(type)) {
+            if (isSoundfile(type)) {
                 fSoundfileItems++;
             }
         }
@@ -191,13 +182,13 @@ struct JSONUIDecoderAux {
             std::string type = it.type;
             // Meta data declaration for input items
             if (isInput(type)) {
-                ZoneParam* param = new ZoneParam(std::atoi(it.index.c_str()));
+                ZoneParam* param = new ZoneParam(it.index);
                 fPathInputTable.push_back(param);
-                param->fZone = STR2REAL(it.init);
+                param->fZone = it.init;
             }
             // Meta data declaration for output items
             else if (isOutput(type)) {
-                ZoneParam* param = new ZoneParam(std::atoi(it.index.c_str()));
+                ZoneParam* param = new ZoneParam(it.index);
                 fPathOutputTable.push_back(param);
                 param->fZone = REAL(0);
             }
@@ -228,7 +219,7 @@ struct JSONUIDecoderAux {
         int item = 0;
         for (auto& it : fUiItems) {
             if (isInput(it.type)) {
-                static_cast<ZoneParam*>(fPathInputTable[item++])->fZone = STR2REAL(it.init);
+                static_cast<ZoneParam*>(fPathInputTable[item++])->fZone = it.init;
             }
         }
     }
@@ -236,9 +227,9 @@ struct JSONUIDecoderAux {
     void resetUserInterface(char* memory_block, Soundfile* defaultsound = nullptr)
     {
         for (auto& it : fUiItems) {
-            int offset = std::atoi(it.index.c_str());
+            int offset = it.index;
             if (isInput(it.type)) {
-                *REAL_ADR(offset) = STR2REAL(it.init);
+                *REAL_ADR(offset) = it.init;
             } else if (isSoundfile(it.type)) {
                 if (*SOUNDFILE_ADR(offset) == nullptr) {
                     *SOUNDFILE_ADR(offset) = defaultsound;
@@ -265,10 +256,10 @@ struct JSONUIDecoderAux {
         for (auto& it : fUiItems) {
             
             std::string type = it.type;
-            REAL init = STR2REAL(it.init);
-            REAL min = STR2REAL(it.min);
-            REAL max = STR2REAL(it.max);
-            REAL step = STR2REAL(it.step);
+            REAL init = it.init;
+            REAL min = it.min;
+            REAL max = it.max;
+            REAL step = it.step;
             
             // Meta data declaration for input items
             if (isInput(type)) {
@@ -323,6 +314,7 @@ struct JSONUIDecoderAux {
                 countSound++;
             }
         }
+        
         setlocale(LC_ALL, tmp_local);
     }
     
@@ -332,16 +324,14 @@ struct JSONUIDecoderAux {
         char* tmp_local = setlocale(LC_ALL, nullptr);
         setlocale(LC_ALL, "C");
         
-        int countSound = 0;
-        
         for (auto& it : fUiItems) {
             
             std::string type = it.type;
-            int offset = std::atoi(it.index.c_str());
-            REAL init = STR2REAL(it.init);
-            REAL min = STR2REAL(it.min);
-            REAL max = STR2REAL(it.max);
-            REAL step = STR2REAL(it.step);
+            int offset = it.index;
+            REAL init = it.init;
+            REAL min = it.min;
+            REAL max = it.max;
+            REAL step = it.step;
             
             // Meta data declaration for input items
             if (isInput(type)) {
@@ -387,11 +377,8 @@ struct JSONUIDecoderAux {
             } else if (type == "close") {
                 REAL_UI(ui_interface)->closeBox();
             }
-            
-            if (isSoundfile(type)) {
-                countSound++;
-            }
         }
+        
         setlocale(LC_ALL, tmp_local);
     }
     

architecture/faust/gui/SimpleParser.h
@@ -48,12 +48,15 @@ struct itemInfo {
     std::string label;
     std::string url;
     std::string address;
-    std::string index;
-    std::string init;
-    std::string min;
-    std::string max;
-    std::string step;
+    int index;
+    double init;
+    double min;
+    double max;
+    double step;
     std::vector<std::pair<std::string, std::string> > meta;
+    
+    itemInfo():index(0), init(0.), min(0.), max(0.), step(0.)
+    {}
 };
 
 // ---------------------------------------------------------------------
@@ -164,6 +167,7 @@ static bool parseDouble(const char*& p, double& x)
     } else {
         p = saved;
     }
+ 
     return valid;
 }
 
@@ -372,13 +376,13 @@ static bool parseItemMetaData(const char*& p, std::vector<std::pair<std::string,
 // "name" : "...", "inputs" : "...", "outputs" : "...", ...
 // and store the result as key/value
 /// ---------------------------------------------------------------------
-static bool parseGlobalMetaData(const char*& p, std::string& key, std::string& value, std::map<std::string, std::string>& metadatas, std::vector<std::string>& items)
+static bool parseGlobalMetaData(const char*& p, std::string& key, std::string& value, double& dbl, std::map<std::string, std::string>& metadatas, std::vector<std::string>& items)
 {
     if (parseDQString(p, key)) {
         if (key == "meta") {
             return parseMetaData(p, metadatas);
         } else {
-            return parseChar(p, ':') && (parseDQString(p, value) || parseList(p, items));
+            return parseChar(p, ':') && (parseDQString(p, value) || parseList(p, items) || parseDouble(p, dbl));
         }
     } else {
         return false;
@@ -396,6 +400,7 @@ static bool parseUI(const char*& p, std::vector<itemInfo>& uiItems, int& numItem
    
         std::string label;
         std::string value;
+        double dbl = 0;
         
         do {
             if (parseDQString(p, label)) {
@@ -429,8 +434,8 @@ static bool parseUI(const char*& p, std::vector<itemInfo>& uiItems, int& numItem
                 }
                 
                 else if (label == "index") {
-                    if (parseChar(p, ':') && parseDQString(p, value)) {
-                        uiItems[numItems].index = value;
+                    if (parseChar(p, ':') && parseDouble(p, dbl)) {
+                        uiItems[numItems].index = dbl;
                     }
                 }
                 
@@ -441,26 +446,26 @@ static bool parseUI(const char*& p, std::vector<itemInfo>& uiItems, int& numItem
                 }
                 
                 else if (label == "init") {
-                    if (parseChar(p, ':') && parseDQString(p, value)) {
-                        uiItems[numItems].init = value;
+                    if (parseChar(p, ':') && parseDouble(p, dbl)) {
+                        uiItems[numItems].init = dbl;
                     }
                 }
                 
                 else if (label == "min") {
-                    if (parseChar(p, ':') && parseDQString(p, value)) {
-                        uiItems[numItems].min = value;
+                    if (parseChar(p, ':') && parseDouble(p, dbl)) {
+                        uiItems[numItems].min = dbl;
                     }
                 }
                 
                 else if (label == "max") {
-                    if (parseChar(p, ':') && parseDQString(p, value)) {
-                        uiItems[numItems].max = value;
+                    if (parseChar(p, ':') && parseDouble(p, dbl)) {
+                        uiItems[numItems].max = dbl;
                     }
                 }
                 
                 else if (label == "step"){
-                    if (parseChar(p, ':') && parseDQString(p, value)) {
-                        uiItems[numItems].step = value;
+                    if (parseChar(p, ':') && parseDouble(p, dbl)) {
+                        uiItems[numItems].step = dbl;
                     }
                 }
                 
@@ -498,7 +503,7 @@ static bool parseUI(const char*& p, std::vector<itemInfo>& uiItems, int& numItem
 // and store the result in map Metadatas and vector containing the items of the interface. Returns true if parsing was successfull.
 /// ---------------------------------------------------------------------
 static bool parseJson(const char*& p,
-                      std::map<std::string, std::string>& metaDatas0,
+                      std::map<std::string, std::pair<std::string, double> >& metaDatas0,
                       std::map<std::string, std::string>& metaDatas1,
                       std::map<std::string, std::vector<std::string> >& metaDatas2,
                       std::vector<itemInfo>& uiItems)
@@ -508,15 +513,18 @@ static bool parseJson(const char*& p,
     do {
         std::string key;
         std::string value;
+        double dbl = 0;
         std::vector<std::string> items;
-        if (parseGlobalMetaData(p, key, value, metaDatas1, items)) {
+        if (parseGlobalMetaData(p, key, value, dbl, metaDatas1, items)) {
             if (key != "meta") {
                 // keep "name", "inputs", "outputs" key/value pairs
                 if (items.size() > 0) {
                     metaDatas2[key] = items;
                     items.clear();
+                } else if (value != "") {
+                    metaDatas0[key].first = value;
                 } else {
-                    metaDatas0[key] = value;
+                    metaDatas0[key].second = dbl;
                 }
             }
         } else if (key == "ui") {

compiler/generator/Text.hh
@@ -207,6 +207,7 @@ inline string flatten(const string& src)
     return dst;
 }
 
+// To be used for WASM or SOUL
 inline string flattenJSON(const string& src)
 {
     string dst;
@@ -215,6 +216,26 @@ inline string flattenJSON(const string& src)
             case '"':
                 dst += "\\\"";
                 break;
+            case '\\':
+                dst += "\\";
+                break;
+            case '\'':
+                dst += "\\'";
+                break;
+            default:
+                dst += src[i];
+                break;
+        }
+    }
+    return dst;
+}
+
+// To be used for JavaScript
+inline string flattenJSON1(const string& src)
+{
+    string dst;
+    for (size_t i = 0; i < src.size(); i++) {
+        switch (src[i]) {
             case '\\':
                 dst += "\\";
                 break;

compiler/generator/code_container.cpp
@@ -441,59 +441,6 @@ BlockInst* CodeContainer::flattenFIR(void)
     return global_block;
 }
 
-void CodeContainer::generateMetaData(JSONUI* json)
-{
-    // Add global metadata
-    for (MetaDataSet::iterator i = gGlobal->gMetaDataSet.begin(); i != gGlobal->gMetaDataSet.end(); i++) {
-        if (i->first != tree("author")) {
-            stringstream str1, str2;
-            str1 << *(i->first);
-            str2 << **(i->second.begin());
-            string res1 = str1.str();
-            string res2 = unquote(str2.str());
-            json->declare(res1.c_str(), res2.c_str());
-        } else {
-            for (set<Tree>::iterator j = i->second.begin(); j != i->second.end(); j++) {
-                if (j == i->second.begin()) {
-                    stringstream str1, str2;
-                    str1 << *(i->first);
-                    str2 << **j;
-                    string res1 = str1.str();
-                    string res2 = unquote(str2.str());
-                    json->declare(res1.c_str(), res2.c_str());
-                } else {
-                    stringstream str2;
-                    str2 << **j;
-                    string res2 = unquote(str2.str());
-                    json->declare("contributor", res2.c_str());
-                }
-            }
-        }
-    }
-}
-
-void CodeContainer::generateJSONFile()
-{
-    JSONInstVisitor json_visitor;
-    generateJSON(&json_visitor);
-    ofstream xout(subst("$0.json", gGlobal->makeDrawPath()).c_str());
-    xout << json_visitor.JSON();
-}
-
-void CodeContainer::generateJSON(JSONInstVisitor* visitor)
-{
-    // Prepare compilation options
-    stringstream compile_options;
-    gGlobal->printCompilationOptions(compile_options);
-
-    // "name", "filename" found in medata
-    visitor->init("", "", fNumInputs, fNumOutputs, -1, "", "", FAUSTVERSION, compile_options.str(),
-                  gGlobal->gReader.listLibraryFiles(), gGlobal->gImportDirList, -1, std::map<std::string, int>());
-
-    generateUserInterface(visitor);
-    generateMetaData(visitor);
-}
-
 BlockInst* CodeContainer::inlineSubcontainersFunCalls(BlockInst* block)
 {
     // Rename 'sig' in 'dsp' and remove 'dsp' allocation

compiler/generator/code_container.hh
@@ -288,10 +288,71 @@ class CodeContainer : public virtual Garbageable {
                               TextInstVisitor* producer);
 
     void generateDAGLoop(BlockInst* loop_code, DeclareVarInst* count);
+    
+    template <typename REAL>
+    void generateMetaData(JSONUIAux<REAL>* json)
+    {
+        // Add global metadata
+        for (MetaDataSet::iterator i = gGlobal->gMetaDataSet.begin(); i != gGlobal->gMetaDataSet.end(); i++) {
+            if (i->first != tree("author")) {
+                stringstream str1, str2;
+                str1 << *(i->first);
+                str2 << **(i->second.begin());
+                string res1 = str1.str();
+                string res2 = unquote(str2.str());
+                json->declare(res1.c_str(), res2.c_str());
+            } else {
+                for (set<Tree>::iterator j = i->second.begin(); j != i->second.end(); j++) {
+                    if (j == i->second.begin()) {
+                        stringstream str1, str2;
+                        str1 << *(i->first);
+                        str2 << **j;
+                        string res1 = str1.str();
+                        string res2 = unquote(str2.str());
+                        json->declare(res1.c_str(), res2.c_str());
+                    } else {
+                        stringstream str2;
+                        str2 << **j;
+                        string res2 = unquote(str2.str());
+                        json->declare("contributor", res2.c_str());
+                    }
+                }
+            }
+        }
+    }
 
-    void generateJSONFile();
-    void generateMetaData(JSONUI* json);
-    void generateJSON(JSONInstVisitor* visitor);
+    template <typename REAL>
+    void generateJSONFile()
+    {
+        JSONInstVisitor<REAL> json_visitor;
+        generateJSON(&json_visitor);
+        ofstream xout(subst("$0.json", gGlobal->makeDrawPath()).c_str());
+        xout << json_visitor.JSON();
+    }
+    
+    template <typename REAL>
+    void generateJSON(JSONInstVisitor<REAL>* visitor)
+    {
+        // Prepare compilation options
+        stringstream compile_options;
+        gGlobal->printCompilationOptions(compile_options);
+        
+        // "name", "filename" found in medata
+        visitor->init("", "", fNumInputs, fNumOutputs, -1, "", "", FAUSTVERSION, compile_options.str(),
+                      gGlobal->gReader.listLibraryFiles(), gGlobal->gImportDirList, -1, std::map<std::string, int>());
+        
+        generateUserInterface(visitor);
+        generateMetaData(visitor);
+    }
+    
+    template <typename REAL>
+    string generateJSON()
+    {
+        JSONInstVisitor<REAL> json_visitor;
+        generateUserInterface(&json_visitor);
+        generateMetaData(&json_visitor);
+        return json_visitor.JSON(true);
+    }
 
     DeclareFunInst* generateCalloc();
     DeclareFunInst* generateFree();

compiler/generator/dag_instructions_compiler.cpp
@@ -131,7 +131,11 @@ void DAGInstructionsCompiler::compileMultiSignal(Tree L)
 
     // Generate JSON
     if (gGlobal->gPrintJSONSwitch) {
-        fContainer->generateJSONFile();
+        if (gGlobal->gFloatSize == 1) {
+            fContainer->generateJSONFile<float>();
+        } else {
+            fContainer->generateJSONFile<double>();
+        }
     }
 }
 

compiler/generator/instructions_compiler.cpp
@@ -414,10 +414,14 @@ void InstructionsCompiler::compileMultiSignal(Tree L)
 
     // Generate JSON (which checks for non duplicated path)
     if (gGlobal->gPrintJSONSwitch) {
-        fContainer->generateJSONFile();
+        if (gGlobal->gFloatSize == 1) {
+            fContainer->generateJSONFile<float>();
+        } else {
+            fContainer->generateJSONFile<double>();
+        }
     } else {
         // Checks for non duplicated path
-        JSONInstVisitor path_checker;
+        JSONInstVisitor<float> path_checker;
         fContainer->generateUserInterface(&path_checker);
     }
 

commit 5f5dd36f8e6c465bd632a3e8b4787523457ef408

Fix wrong cut/paste introduced in 5422fd464f8bdce17abf3004654eedd9fcc13dfb.

architecture/faust/gui/JSONUI.h
@@ -475,7 +475,7 @@ class JSONUI : public JSONUIAux<FAUSTFLOAT>, public UI
         }
         virtual void openHorizontalBox(const char* label)
         {
-            JSONUIAux<FAUSTFLOAT>::openTabBox(label);
+            JSONUIAux<FAUSTFLOAT>::openHorizontalBox(label);
         }
         virtual void openVerticalBox(const char* label)
         {

architecture/faust/gui/JSONUIDecoder.h
@@ -295,7 +295,7 @@ struct JSONUIDecoderAux {
             } else if (type == "soundfile") {
                 REAL_UI(ui_interface)->addSoundfile(it.label.c_str(), it.url.c_str(), &fSoundfiles[countSound]);
             } else if (type == "hbargraph") {
-                 REAL_UI(ui_interface)->addHorizontalBargraph(it.label.c_str(), &static_cast<ZoneParam*>(fPathOutputTable[countOut])->fZone, min, max);
+                REAL_UI(ui_interface)->addHorizontalBargraph(it.label.c_str(), &static_cast<ZoneParam*>(fPathOutputTable[countOut])->fZone, min, max);
             } else if (type == "vbargraph") {
                 REAL_UI(ui_interface)->addVerticalBargraph(it.label.c_str(), &static_cast<ZoneParam*>(fPathOutputTable[countOut])->fZone, min, max);
             } else if (type == "nentry") {