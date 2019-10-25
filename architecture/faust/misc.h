88e8bdbad617a3f989c0547e8e03f4fd5119a7e5
Stephane Letz <letz@grame.fr>
2019-10-25 12:03:44 +0200

    Correct faust2android -osc mode.

--- architecture/faust/misc.h
+++ architecture/faust/misc.h
@@ -53,8 +53,7 @@ static int int2pow2(int x) { int r = 0; while ((1<<r) < x) r++; return r; }
 
 static long lopt(char* argv[], const char* name, long def)
 {
-    int i;
-    for (i = 0; argv[i]; i++) if (!strcmp(argv[i], name)) return std::atoi(argv[i+1]);
+    for (int i = 0; argv[i]; i++) if (!strcmp(argv[i], name)) return std::atoi(argv[i+1]);
     return def;
 }
 
@@ -70,8 +69,7 @@ static long lopt1(int argc, char* argv[], const char* longname, const char* shor
 
 static const char* lopts(char* argv[], const char* name, const char* def)
 {
-    int i;
-    for (i = 0; argv[i]; i++) if (!strcmp(argv[i], name)) return argv[i+1];
+    for (int i = 0; argv[i]; i++) if (!strcmp(argv[i], name)) return argv[i+1];
     return def;
 }
 
@@ -87,8 +85,7 @@ static const char* lopts1(int argc, char* argv[], const char* longname, const ch
 
 static bool isopt(char* argv[], const char* name)
 {
-    int i;
-    for (i = 0; argv[i]; i++) if (!strcmp(argv[i], name)) return true;
+    for (int i = 0; argv[i]; i++) if (!strcmp(argv[i], name)) return true;
     return false;
 }
 
