grame-cncm/faust

commit 023e963d0c09731ec9d9cf7d386f46e4c4dddba2

[FIX] Crash bug in uiCocoaItem fixed.

architecture/iOS/iOS/FICocoaUI.h
@@ -350,9 +350,10 @@ public:
     int                     fBoxType;
     float                   fLastX;
     float                   fLastY;
+    float                   fDummy;
     
     uiBox(GUI* ui, FIMainViewController* controller, const char* name, int boxType)
-    : uiCocoaItem(ui, NULL, controller, name)
+    : uiCocoaItem(ui, &fDummy, controller, name)
     {
         float tabOffset = 0;
         fBoxType = boxType;
@@ -360,6 +361,7 @@ public:
         fLastY = 0.f;
         fTabView = nil;
         fLabel = nil;
+        fDummy = 0.f;
         
         if (boxType == kTabLayout)
         {

architecture/faust/gui/GUI.h
@@ -29,6 +29,7 @@
 #include <map>
 #include <vector>
 #include <iostream>
+#include <assert.h>
 
 #ifdef _WIN32
 # pragma warning (disable: 4100)
@@ -54,7 +55,11 @@ typedef void (*uiCallback)(FAUSTFLOAT val, void* data);
 struct uiItemBase
 {
     
-    uiItemBase(GUI* ui, FAUSTFLOAT* zone) {}
+    uiItemBase(GUI* ui, FAUSTFLOAT* zone)
+    {
+        assert(ui);
+        assert(zone);
+    }
     
     virtual ~uiItemBase()
     {}

commit 83871888ee388ed89682be54a57974063cb35115

[FIX] Incorrect behaviour with uiBox introduced in 023e963d0c09731ec9d9cf7d386f46e4c4dddba2. Set version to 2.19.9.

Makefile
@@ -1,4 +1,4 @@
-version := 2.19.8
+version := 2.19.9
 
 system	?= $(shell uname -s)
 

architecture/iOS/iOS/FICocoaUI.h
@@ -206,47 +206,7 @@ public:
     }
     
     // Constructor / Destuctor
-    uiCocoaItem(GUI* ui, float* zone, FIMainViewController* controller, const char* name)
-    : uiItem(ui, zone)
-    {
-        fName = [[NSString alloc] initWithString:[NSString stringWithCString:name encoding:NSASCIIStringEncoding]];
-        fLabel = nil;
-        fHidden = false;
-        fParent = NULL;
-        
-        fx = 0.f;
-        fy = 0.f;
-        fw = 0.f;
-        fh = 0.f;
-        
-        fAbstractX = 0.f;
-        fAbstractY = 0.f;
-        fAbstractW = 0.f;
-        fAbstractH = 0.f;
-        
-        fSelected = false;
-        
-        fInitAssignationType = kAssignationNone;
-        fInitAssignationCurve = kAssignationNone;
-        
-        fInitR = 0.f;
-        fInitG = 0.f;
-        fInitB = 1.f;
-        
-        fInitMinCurve = -100.f;
-        fInitMidCurve = 0.f;
-        fInitMaxCurve = 100.f;
-        
-        fHideOnGUI = false;
-        fInit = 0.f;
-        if (zone) {
-            fItemCount = gItemCount++;
-        } else {
-            fItemCount = -1;
-        }
-        
-        resetParameters();
-    }
+    uiCocoaItem(GUI* ui, float* zone, FIMainViewController* controller, const char* name);
     
     ~uiCocoaItem()
     {
@@ -350,10 +310,10 @@ public:
     int                     fBoxType;
     float                   fLastX;
     float                   fLastY;
-    float                   fDummy;
+    static float            gDummy;
     
     uiBox(GUI* ui, FIMainViewController* controller, const char* name, int boxType)
-    : uiCocoaItem(ui, &fDummy, controller, name)
+    : uiCocoaItem(ui, &gDummy, controller, name)
     {
         float tabOffset = 0;
         fBoxType = boxType;
@@ -361,7 +321,6 @@ public:
         fLastY = 0.f;
         fTabView = nil;
         fLabel = nil;
-        fDummy = 0.f;
         
         if (boxType == kTabLayout)
         {
@@ -577,10 +536,53 @@ public:
     }
 };
 
+// Constructor / Destuctor
+uiCocoaItem::uiCocoaItem(GUI* ui, float* zone, FIMainViewController* controller, const char* name)
+: uiItem(ui, zone)
+{
+    fName = [[NSString alloc] initWithString:[NSString stringWithCString:name encoding:NSASCIIStringEncoding]];
+    fLabel = nil;
+    fHidden = false;
+    fParent = NULL;
+    
+    fx = 0.f;
+    fy = 0.f;
+    fw = 0.f;
+    fh = 0.f;
+    
+    fAbstractX = 0.f;
+    fAbstractY = 0.f;
+    fAbstractW = 0.f;
+    fAbstractH = 0.f;
+    
+    fSelected = false;
+    
+    fInitAssignationType = kAssignationNone;
+    fInitAssignationCurve = kAssignationNone;
+    
+    fInitR = 0.f;
+    fInitG = 0.f;
+    fInitB = 1.f;
+    
+    fInitMinCurve = -100.f;
+    fInitMidCurve = 0.f;
+    fInitMaxCurve = 100.f;
+    
+    fHideOnGUI = false;
+    fInit = 0.f;
+    if (zone != &uiBox::gDummy) {
+        fItemCount = gItemCount++;
+    } else {
+        fItemCount = -1;
+    }
+    
+    resetParameters();
+}
+
 // -------------------------- Knob -----------------------------------
 
 class uiKnob : public uiCocoaItem
-{   
+{
     
 public :
     
@@ -1678,7 +1680,7 @@ public:
     {
         /* 
             29/11/18: used by startMotion to activate the accelerometer and/or gyroscope
-            correction bo make it work even if screen mode is activated
+            correction to make it work even if screen mode is activated
         */
         
         /*

architecture/iOS/iOS/FIMainViewController.mm
@@ -77,6 +77,7 @@ int sample_rate = 0;
 int buffer_size = 0;
 BOOL openWidgetPanel = YES;
 int uiCocoaItem::gItemCount = 0;
+float uiBox::gDummy = 0;
 
 - (void)didReceiveMemoryWarning
 {
@@ -263,6 +264,7 @@ int uiCocoaItem::gItemCount = 0;
     _motionManager = nil;
     _selectedWidget = nil;
     [self loadWidgetsPreferences];
+    
     if (_assignatedWidgets.size() > 0 || uiinterface->isScreenUI()) [self startMotion];
 }
 

build/CMakeLists.txt
@@ -3,7 +3,7 @@ project (faust)
 
 #######################################
 # versions management
-set (VERSION 2.19.8)
+set (VERSION 2.19.9)
 macro (get_major_minor_patch version)
 	string( REGEX REPLACE "([0-9]*)\\.([0-9]*)\\.([0-9]*)" "\\1"  VERSION_MAJOR ${version} )
 	string( REGEX REPLACE "([0-9]*)\\.([0-9]*)\\.([0-9]*)" "\\2"  VERSION_MINOR ${version} )

build/Makefile
@@ -11,7 +11,7 @@ system := $(shell echo $(system) | grep MINGW > /dev/null && echo MINGW || echo 
 # output directories
 FAUSTDIR ?= faustdir
 IOSDIR   := iosdir
-VERSION := 2.19.8
+VERSION := 2.19.9
 
 #===============================================================
 # current generator and backends

compiler/generator/export.hh
@@ -22,7 +22,7 @@
 #ifndef __export__
 #define __export__
 
-#define FAUSTVERSION "2.19.8"
+#define FAUSTVERSION "2.19.9"
 
 #ifdef _WIN32
 #define EXPORT __declspec(dllexport)

documentation/man/man1/faust.1
@@ -1,6 +1,6 @@
 .\" Automatically generated by Pandoc 2.7.3
 .\"
-.TH "man" "1" "" "Version 2.19.8 (24-November-2019)" "Faust man page"
+.TH "man" "1" "" "Version 2.19.9 (26-November-2019)" "Faust man page"
 .hy
 .SH NAME
 .PP

windows/faust.rc
@@ -51,8 +51,8 @@ END
 //
 
 VS_VERSION_INFO VERSIONINFO
- FILEVERSION 2,19,8,0
- PRODUCTVERSION 2,19,8,0
+ FILEVERSION 2,19,9,0
+ PRODUCTVERSION 2,19,9,0
  FILEFLAGSMASK 0x17L
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -68,12 +68,12 @@ BEGIN
         BLOCK "040c04b0"
         BEGIN
             VALUE "FileDescription", "Faust compiler"
-            VALUE "FileVersion", "2, 19, 8, 0"
+            VALUE "FileVersion", "2, 19, 9, 0"
             VALUE "InternalName", "Faust"
             VALUE "LegalCopyright", "Copyright (C) 2009-2018 - Grame"
             VALUE "OriginalFilename", "faust.exe"
             VALUE "ProductName", "Faust"
-            VALUE "ProductVersion", "2, 19, 8, 0"
+            VALUE "ProductVersion", "2, 19, 9, 0"
         END
     END
     BLOCK "VarFileInfo"

windows/faustdll.rc
@@ -51,8 +51,8 @@ END
 //
 
 VS_VERSION_INFO VERSIONINFO
- FILEVERSION 2,19,8,0
- PRODUCTVERSION 2,19,8,0
+ FILEVERSION 2,19,9,0
+ PRODUCTVERSION 2,19,9,0
  FILEFLAGSMASK 0x17L
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -68,12 +68,12 @@ BEGIN
         BLOCK "040c04b0"
         BEGIN
             VALUE "FileDescription", "Faust compiler"
-            VALUE "FileVersion", "2, 19, 8, 0"
+            VALUE "FileVersion", "2, 19, 9, 0"
             VALUE "InternalName", "Faust"
             VALUE "LegalCopyright", "Copyright (C) 2009-2018 - Grame"
             VALUE "OriginalFilename", "faust.exe"
             VALUE "ProductName", "Faust"
-            VALUE "ProductVersion", "2, 19, 8, 0"
+            VALUE "ProductVersion", "2, 19, 9, 0"
         END
     END
     BLOCK "VarFileInfo"