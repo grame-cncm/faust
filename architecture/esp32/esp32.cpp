78f5b79dc5dfa40a08168521189b9c55d78a0754
Stephane Letz <letz@grame.fr>
2020-01-22 15:31:58 +0100

    Add Thomas Hopman MIDI code in esp32 architecture.

--- architecture/esp32/esp32.cpp
+++ architecture/esp32/esp32.cpp
@@ -77,6 +77,11 @@
 #define DIV_S32 4.6566129e-10
 #define clip(sample) std::max(-MULT_S32, std::min(MULT_S32, ((int32_t)(sample * MULT_S32))));
 
+#if MIDICTRL
+std::list<GUI*> GUI::fGuiList;
+ztimedmap GUI::gTimedZoneMap;
+#endif
+
 AudioFaust::AudioFaust(int sample_rate, int buffer_size)
 {
     fBS = buffer_size;
@@ -128,6 +133,12 @@ AudioFaust::AudioFaust(int sample_rate, int buffer_size)
     } else {
         fOutChannel = NULL;
     }
+    
+#if MIDICTRL
+    fMIDIHandler = new esp32_midi();
+    fMIDIInterface = new MidiUI(fMIDIHandler);
+    fDSP->buildUserInterface(fMIDIInterface);
+#endif
 }
 
 AudioFaust::~AudioFaust()
@@ -144,12 +155,20 @@ AudioFaust::~AudioFaust()
     
     delete fDSP;
     delete fUI;
+    
+#if MIDICTRL
+    delete fMIDIInterface;
+    delete fMIDIHandler;
+#endif
 }
 
 bool AudioFaust::start()
 {
-    xTaskCreate(audioTaskHandler, "Faust DSP Task", 1024, (void*)this, 5, &fHandle);
-    return true;
+#if MIDICTRL
+    fMIDIHandler->startMidi();
+    fMIDIInterface->run();
+#endif
+    return (xTaskCreatePinnedToCore(audioTaskHandler, "Faust DSP Task", 1024, (void*)this, 24, &fHandle, 0) == pdPASS);
 }
 
 void AudioFaust::stop()
@@ -158,6 +177,9 @@ void AudioFaust::stop()
         vTaskDelete(fHandle);
         fHandle = NULL;
     }
+#if MIDICTRL
+    fMIDIHandler->stopMidi();
+#endif
 }
 
 void AudioFaust::setParamValue(const std::string& path, float value)
@@ -174,7 +196,7 @@ void AudioFaust::configureI2S(int sample_rate, int buffer_size, i2s_pin_config_t
         .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
         .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL3, // high interrupt priority
         .dma_buf_count = 3,
         .dma_buf_len = buffer_size,
         .use_apll = true
