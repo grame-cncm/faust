8dd0c1e9bba4cfafbae1ab5f2b15041a3ed3b5cf
Faust Release <letz@grame.fr>
2020-07-23 10:25:02 +0200

    add missing targets (used by win64 release)

---
+++ build/targets/all-win64.cmake
@@ -0,0 +1,15 @@
+# this file may be used to select different target
+# values are among ON or OFF 
+
+set ( INCLUDE_EXECUTABLE  ON  CACHE STRING  "Include faust compiler" FORCE )
+set ( INCLUDE_STATIC      ON  CACHE STRING  "Include static faust library" FORCE )
+set ( INCLUDE_DYNAMIC     ON  CACHE STRING  "Include dynamic faust library" FORCE )
+
+set ( INCLUDE_OSC         ON  CACHE STRING  "Include Faust OSC static library" FORCE )
+set ( INCLUDE_HTTP        ON  CACHE STRING  "Include Faust HTTPD library" FORCE )
+
+set ( OSCDYNAMIC          ON  CACHE STRING  "Include Faust OSC dynamic library" FORCE )
+set ( HTTPDYNAMIC         ON  CACHE STRING  "Include Faust HTTP dynamic library" FORCE )
+
+set ( INCLUDE_ITP         OFF  CACHE STRING  "Include Faust Machine library" FORCE )
+set ( ITPDYNAMIC          OFF  CACHE STRING  "Include Faust Machine library" FORCE )
