0b895e39670df47265e3a9773a418d627158b79e
Dominique Fober <fober@grame.fr>
2020-09-18 09:32:22 +0200

    test errors with dsp code

--- build/emcc/tests/testApi-new.js
+++ build/emcc/tests/testApi-new.js
@@ -1,4 +1,5 @@
 var options = "-I libraries/";
+var errCode = "foo";
 
 //----------------------------------------------------------------------------
 // Misc. functions
@@ -18,19 +19,39 @@ function misc (faust, log, code )
 	}
 }
 
-function run (engine, log, code) {
+async function createDsp (faust, log, code) {
+	log ("\nCreating DSP instance:");
+	log ( "createDSPFactory: ");
+	let faustmodule = await faust.createDSPFactory  ("test", code, options, false);
+	if (typeof faustmodule == "string") {
+		log ("createDSPFactory error: " + faustmodule);
+		return;
+	}
+
+	log ( "faustmodule poly: " + faustmodule.poly);
+	log ( "createDSPInstance: ");
+	let instance = await faust.createDSPInstance (faustmodule);
+	log ( "  getNumInputs : " + instance.api.getNumInputs());
+	log ( "  getNumOutputs: " + instance.api.getNumOutputs());
+}
+
+async function run (engine, log, code) {
 	let faust = new FaustCompiler (engine);
 	log ( "libfaust version: " + faust.version());
 	misc (faust, log, code);
-	log ( "createDSPFactory: ");
-	faust.createDSPFactory  ("test", code, options, false).then (faustmodule => {
-		log ( "createDSPInstance: ");
-		faust.createDSPInstance (faustmodule).then (instance => {
-			log ( "  getNumInputs : " + instance.api.getNumInputs());
-			log ( "  getNumOutputs: " + instance.api.getNumOutputs());
-			log ("\nEnd of API tests");
-		});
-	});
+	await createDsp (faust, log, code); //.then ( () => { log ("\nEnd of API tests"); } );
+	await createDsp (faust, log, errCode).catch (e => {log(e); }); 
+	log ("\nEnd of API tests");
+	//.then ( () => { log ("\nEnd of API tests"); } );
+	// log ( "createDSPFactory: ");
+	// faust.createDSPFactory  ("test", code, options, false).then (faustmodule => {
+	// 	log ( "createDSPInstance: ");
+	// 	faust.createDSPInstance (faustmodule).then (instance => {
+	// 		log ( "  getNumInputs : " + instance.api.getNumInputs());
+	// 		log ( "  getNumOutputs: " + instance.api.getNumOutputs());
+	// 		log ("\nEnd of API tests");
+	// 	});
+	// });
 }
 
 if ((typeof process !== 'undefined') && (process.release.name === 'node')) {
