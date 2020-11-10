0967b137fb39d8c7185d7d0e69d4d78cca01d5a8
Dominique Fober <fober@grame.fr>
2020-11-10 23:23:26 +0100

    hide libfaust api

--- build/emcc/npm/dist/libfaust.d.ts
+++ build/emcc/npm/dist/libfaust.d.ts
@@ -1,43 +1,43 @@
+/************************************************************************
+ ************************************************************************
+    FAUST compiler and web audio interface
+    Copyright (C) 2003-2020 GRAME, Centre National de Creation Musicale
+    ---------------------------------------------------------------------
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-interface WasmModule     {}
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-interface Factory {
-	module  : WasmModule;
-	error  : string;
-}
-interface Expand {
-	dsp  : string;
-	shakey  : string;
-	error  : string;
-}
-interface Aux {
-	success  : boolean;
-	error  : string;
-}
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ ************************************************************************
+ ************************************************************************/
+ 
 
-interface IntVector 	{ size(): number; get(i: number) : number; }
+/*
+	This file describes the API to instantiate a low level Faust library.
+	This library provides access to the Faust compiler but the compiler interface
+	is not public. Thus LibFaust is an opaque reference that is required by
+	higher level APIs.
+*/
 
-interface FaustWasm {
-	data : 		IntVector;
-	helper :	string;
-}
+declare namespace Faust {
 
+    type FaustModule = any;
+    type LibFaust = any;
 
-interface LibFaust {
-	version() : string;
-
-	createDSPFactory(name_app: string, dsp_content: string, args: string, internal_memory: boolean) : Factory;
-
-	expandDSP(name_app: string, dsp_content: string, args: string) : Expand	
-
-	generateAuxFiles(name_app: string, dsp_content: string, args: string) : Aux;
-
-	deleteAllDSPFactories(): void;
-
-	getWasmModule(module: WasmModule) : FaustWasm;
-	freeWasmModule(module: WasmModule) : void;
-	// freeMemory (mem: any) : void;		// check if still necessary
-
-	cleanupAfterException(): void;
-	getErrorAfterException() : string;
+	/**
+	 * Instantiates the Faust Library.
+	 *
+	 * @param {string} module - the Faust Wasm module, typically retrieved with an async call to FaustModule()
+	 * @returns {LibFaust} an opaque reference to the Faust library
+	 */
+  	
+	interface createLibFaust { (module: FaustModule): LibFaust }	
 }
