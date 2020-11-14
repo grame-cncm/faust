a4921d9a14e1e5269438432246396c077b31299c
Stephane Letz <letz@grame.fr>
2020-11-14 15:34:14 +0100

    Fix previous incorrect commit in 'getnode'.

--- build/emcc/npm/exemples/sapi.js
+++ build/emcc/npm/exemples/sapi.js
@@ -5,5 +5,5 @@ async function getnode(audioCtx, module, code, voices) {
     if (voices > 1)
         return Faust.createAudioNodeFactory().compilePolyNode(audioCtx, "Faust", compiler, code, null, "-I libraries", voices, sp, 1024);
     else
-        return Faust.createAudioNodeFactory().compileMonoNode(audioCtx, "Faust", compiler, code, null, "-I libraries", sp, 1024);
+        return Faust.createAudioNodeFactory().compileMonoNode(audioCtx, "Faust", compiler, code, "-I libraries", sp, 1024);
 }
