1e30e794d52d28855ed10e1652cb9fa5e1fd4d9e
Stephane Letz <letz@grame.fr>
2020-11-14 15:26:38 +0100

    Correct poly.html and poly.html examples.

--- build/emcc/npm/exemples/sapi.js
+++ build/emcc/npm/exemples/sapi.js
@@ -3,7 +3,7 @@ async function getnode(audioCtx, module, code, voices) {
     let sp = typeof (window.AudioWorkletNode) == "undefined";
     let compiler = Faust.createCompiler(Faust.createLibFaust(module));
     if (voices > 1)
-        return Faust.createAudioNodeFactory().compilePolyNode(audioCtx, "Faust", compiler, code, "-I libraries", voices, sp, 1024);
+        return Faust.createAudioNodeFactory().compilePolyNode(audioCtx, "Faust", compiler, code, null, "-I libraries", voices, sp, 1024);
     else
-        return Faust.createAudioNodeFactory().compileMonoNode(audioCtx, "Faust", compiler, code, "-I libraries", sp, 1024);
+        return Faust.createAudioNodeFactory().compileMonoNode(audioCtx, "Faust", compiler, code, null, "-I libraries", sp, 1024);
 }
