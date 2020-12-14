e168721aff21703179f1aae3e4fadf34676ed114
Stephane Letz <letz@grame.fr>
2020-12-14 10:25:53 +0100

    Use LGPL instead of GPL in the TS code.

--- javascript/src/FaustUtilities.ts
+++ javascript/src/FaustUtilities.ts
@@ -4,24 +4,20 @@
     Copyright (C) 2003-2020 GRAME, Centre National de Creation Musicale
     ---------------------------------------------------------------------
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Lesser General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  ************************************************************************
  ************************************************************************/
-
-/************************************************************************
-This file describes the high level interface to the Faust compiler.
-************************************************************************/
 
 ///<reference path="FaustCompiler.ts"/>
 ///<reference path="FaustUtilities.d.ts"/>
