c9353484647bd8664fa853e5299da4ca74d76c63
Stephane Letz <letz@grame.fr>
2021-02-05 16:40:41 +0100

    Formatting.

--- tools/faust2appls/usage.sh
+++ tools/faust2appls/usage.sh
@@ -18,8 +18,8 @@ function require() {
 function options() {
 	while [ "$1" ]
 	do
-		option $1
-		shift
+        option $1
+        shift
 	done
 }
 
@@ -31,26 +31,26 @@ function option() {
 		doc=$*
 		if [ -z "$doc" ]
 		then
-			case $option in
-				"-httpd") doc="activates HTTP control";;
-				"-osc") doc="activates OSC control";;
-				"-midi") doc="activates MIDI control";;
-				"-qrcode") doc="activates QR code generation";;
-				"-poly") doc="produces a polyphonic DSP, ready to be used with MIDI events";;
-				"-nvoices <num>") doc="produces a polyphonic DSP with <num> voices, ready to be used with MIDI events";;
-				"-us <factor>") doc="upsample the DSP by a factor";;
-				"-ds <factor>") doc="downsample the DSP by a factor";;
-				"-filter <filter>") doc="use a filter for upsampling or downsampling [0..4]";;
-				"-source") doc="to only create the source folder";;
-				"-soundfile") doc="when compiling a DSP using the 'soundfile' primitive, add required resources";;
-				"-nodeploy") doc="skip self-contained application generation (using 'macdeployqt')";;
-       			"-effect <effect.dsp>") doc="generates a polyphonic DSP connected to a global output effect, ready to be used with MIDI or OSC";;
-        		"-effect auto") doc="generates a polyphonic DSP connected to a global output effect defined as 'effect' in <file.dsp>, ready to be used with MIDI or OSC";;
-				"Faust options") doc="any option (e.g. -vec -vs 8...). See the Faust compiler documentation.";;
-			esac
-		fi
-		echo "   $option : $doc"
-	else
-		echo "Options:"
-	fi
+            case $option in
+                "-httpd") doc="activates HTTP control";;
+                "-osc") doc="activates OSC control";;
+                "-midi") doc="activates MIDI control";;
+                "-qrcode") doc="activates QR code generation";;
+                "-poly") doc="produces a polyphonic DSP, ready to be used with MIDI events";;
+                "-nvoices <num>") doc="produces a polyphonic DSP with <num> voices, ready to be used with MIDI events";;
+                "-us <factor>") doc="upsample the DSP by a factor";;
+                "-ds <factor>") doc="downsample the DSP by a factor";;
+                "-filter <filter>") doc="use a filter for upsampling or downsampling [0..4]";;
+                "-source") doc="to only create the source folder";;
+                "-soundfile") doc="when compiling a DSP using the 'soundfile' primitive, add required resources";;
+                "-nodeploy") doc="skip self-contained application generation (using 'macdeployqt')";;
+                "-effect <effect.dsp>") doc="generates a polyphonic DSP connected to a global output effect, ready to be used with MIDI or OSC";;
+                "-effect auto") doc="generates a polyphonic DSP connected to a global output effect defined as 'effect' in <file.dsp>, ready to be used with MIDI or OSC";;
+                "Faust options") doc="any option (e.g. -vec -vs 8...). See the Faust compiler documentation.";;
+            esac
+        fi
+        echo "   $option : $doc"
+    else
+        echo "Options:"
+    fi
 }
