1c2e2f678cd798df4afb44dc722ac93a123255c5
Stas Shtin <antisvin@gmail.com>
2021-02-15 17:42:59 +0300

    Soundfile corrections (#550)
    * Removed unused iostream include
    
    * Use an empty buffer of 1k frames like docs describe

--- architecture/faust/gui/Soundfile.h
+++ architecture/faust/gui/Soundfile.h
@@ -25,7 +25,6 @@
 #ifndef __Soundfile__
 #define __Soundfile__
 
-#include <iostream>
 #include <string.h>
 #include <vector>
 
@@ -33,7 +32,7 @@
 #define FAUSTFLOAT float
 #endif
 
-#define BUFFER_SIZE 16384
+#define BUFFER_SIZE 1024
 #define SAMPLE_RATE 44100
 #define MAX_CHAN 64
 #define MAX_SOUNDFILE_PARTS 256
