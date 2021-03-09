2543906b35641f8482a91a53fe0c220af614c151
Antonin Dudermel <antonin.dudermel@caramail.fr>
2021-03-09 14:56:54 +0100

    bugfix in interval computation
    union between the new interval and the old one in recursive type
    approximation instead of erasing

--- compiler/signals/sigtyperules.cpp
+++ compiler/signals/sigtyperules.cpp
@@ -65,12 +65,12 @@ TupletType derefRecCert(Type t);
 static interval arithmetic(int opcode, const interval& x, const interval& y);
 
 // Uncomment to activate type inferrence tracing
-// #define TRACE(x) x
-#define TRACE(x) \
+#define TRACE(x) x
+/*#define TRACE(x)                              \
     {            \
         ;        \
     }
-
+*/
 /**
  * The empty type environment (also property key for closed term type)
  */
@@ -85,7 +85,7 @@ void typeAnnotation(Tree sig, bool causality)
 {
     // to move into compiler option (set to 0 to disable recursive interval computation)
     const int AGE_LIMIT = 5;
-    const int UP_ITER = 3;
+    const int UP_ITER = 1;
     
     gGlobal->gCausality = causality;
     Tree sl             = symlist(sig);
@@ -165,32 +165,29 @@ void typeAnnotation(Tree sig, bool causality)
     cerr << "find an upperbound of the least fixpoint" << endl;
     
     while (!finished) {
-        // init recursive types
         CTree::startNewVisit();
+        
+        // init recursive types
         for (int i = 0; i < n; i++) {
             setSigType(vrec[i], vtype[i]);
             cerr << i << "-" << *getSigType(vrec[i]) << endl;
             vrec[i]->setVisited();
         }
         
-        // cerr << "compute recursive types" << endl;
+        cerr << "compute recursive types" << endl;
         for (int i = 0; i < n; i++) {
             newType = T(vdef[i], gGlobal->NULLTYPEENV);
             newTuplet.clear();
             oldRecType = derefRecCert(getSigType(vrec[i]));
             newRecType = derefRecCert(newType);
             
-            // prevent widened interval to be erased by smaller intervals
-            // and create an infinite loop
+            // ensure increasing intervals
             for (int j = 0; j < vdefSizes[i]; j++) {
                 newTuplet.push_back(newRecType[j]);
                 newI = newRecType[j]->getInterval();
                 oldI = oldRecType[j]->getInterval();
-                // cerr << "inspecting " << newTuplet[j] << endl;
-                if (vAgeMin[i][j] > AGE_LIMIT)
-                    newI.lo = oldI.lo;
-                if (vAgeMax[i][j] > AGE_LIMIT)
-                    newI.hi = oldI.hi;
+                TRACE(cerr << gGlobal->TABBER << "inspecting " << newTuplet[j] << endl;)
+                newI = reunion(newI, oldI);
                 newTuplet[j] = newTuplet[j]->promoteInterval(newI);
             }
             vtype[i] = new TupletType(newTuplet);
@@ -205,7 +202,7 @@ void typeAnnotation(Tree sig, bool causality)
             oldRecType = derefRecCert(getSigType(vrec[i]));
 
             // faustassert(newRecType.arity() == oldRecType.arity() && newRecType.arity() == vdefSizes[i]);
-            // cerr << i << "-" << *vrec[i] << ":" << *getSigType(vrec[i]) << " => " << *vtype[i] << endl;
+            cerr << i << "-" << *vrec[i] << ":" << *getSigType(vrec[i]) << " => " << *vtype[i] << endl;
             if (vtype[i] != getSigType(vrec[i])) {
                 finished = false;
                 for (int j = 0; j < vdefSizes[i]; j++) {
@@ -213,24 +210,26 @@ void typeAnnotation(Tree sig, bool causality)
                     newI = newRecType[j]->getInterval();
                     oldI = oldRecType[j]->getInterval();
 
-                    cerr << "inspecting " << newTuplet[j] << endl;
+                    TRACE(cerr << gGlobal->TABBER << "inspecting " << newTuplet[j] << endl;)
                     if (newI.lo != oldI.lo) {
+                        faustassert(newI.lo < oldI.lo);
                         vAgeMin[i][j]++;
                         if (vAgeMin[i][j] > AGE_LIMIT) {
-                            cerr << "low widening of " << newTuplet[j] << endl;
+                            TRACE(cerr << gGlobal->TABBER << "low widening of " << newTuplet[j] << endl;)
                             newI.lo = vUp[i][j]->getInterval().lo;
                         }
                     }
 
                     if (newI.hi != oldI.hi) {
+                        faustassert(newI.hi > oldI.hi);
                         vAgeMax[i][j]++;
                         if (vAgeMax[i][j] > AGE_LIMIT) {
-                            cerr << "up widening of " << newTuplet[j] << endl;
+                            TRACE(cerr << gGlobal->TABBER << "up widening of " << newTuplet[j] << endl;)
                             newI.hi = vUp[i][j]->getInterval().hi;
                         }
                     }
                     newTuplet[j] = newTuplet[j]->promoteInterval(newI);
-                    cerr << "widening ended : " << newTuplet[j] << endl;
+                    TRACE(cerr << gGlobal->TABBER << "widening ended : " << newTuplet[j] << endl;)
                 }
                 vtype[i] = new TupletType(newTuplet);
             }
@@ -238,6 +237,7 @@ void typeAnnotation(Tree sig, bool causality)
     }
     // type full term
     T(sig, gGlobal->NULLTYPEENV);
+    cerr << "type success : " << endl << "BYE" << endl;
 }
 
 void annotationStatistics()
