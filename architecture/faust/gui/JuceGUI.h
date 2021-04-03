626be2b06bc510a4d816fedf5dadc5aad86a44d3
Grégoire Locqueville <glocq@users.noreply.github.com>
2021-04-03 13:21:08 +0200

    Add header gards to JuceGUI.h

--- architecture/faust/gui/JuceGUI.h
+++ architecture/faust/gui/JuceGUI.h
@@ -22,6 +22,9 @@
  architecture section is not modified.
  ************************************************************************/
 
+#ifndef JUCE_GUI_H
+#define JUCE_GUI_H
+
 #ifndef FAUSTFLOAT
 #define FAUSTFLOAT float
 #endif
@@ -2050,4 +2053,6 @@ class JuceGUI : public GUI, public MetaDataUI, public juce::Component
         }
     
 };
+
+#endif
 /**************************  END  JuceGUI.h **************************/
