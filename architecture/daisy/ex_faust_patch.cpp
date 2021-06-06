e1a60ff7e9469e4da3bd9f43b9ca7e4e28e2f1b7
sc3sc3 <sc3sc3@gmail.com>
2021-06-06 21:46:39 +0200

    Update ex_faust_patch.cpp

--- architecture/daisy/ex_faust_patch.cpp
+++ architecture/daisy/ex_faust_patch.cpp
@@ -33,15 +33,15 @@
  ************************************************************************/
 
 #include "daisysp.h"
-#include "daisy_seed.h"
+#include "daisy_patch.h"
 
 #include "faust/gui/meta.h"
 #include "faust/gui/UI.h"
 #include "faust/dsp/dsp.h"
 
-#ifdef MIDICTRL
-#include "faust/midi/daisy-midi.h"
-#endif
+// #ifdef MIDICTRL
+// #include "faust/midi/daisy-midi.h"
+// #endif
 
 using namespace daisysp;
 using namespace daisy;
@@ -54,56 +54,307 @@ using namespace daisy;
  *******************************************************************************
  *******************************************************************************/
 
-<<includeIntrinsic>>
+
 
 /********************END ARCHITECTURE SECTION (part 1/2)****************/
 
 /**************************BEGIN USER SECTION **************************/
 
-<<includeclass>>
+#ifndef FAUSTFLOAT
+#define FAUSTFLOAT float
+#endif 
+
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <math.h>
+
+class mydspSIG0 {
+	
+  private:
+	
+	int iRec0[2];
+	
+  public:
+	
+	int getNumInputsmydspSIG0() {
+		return 0;
+	}
+	int getNumOutputsmydspSIG0() {
+		return 1;
+	}
+	int getInputRatemydspSIG0(int channel) {
+		int rate;
+		switch ((channel)) {
+			default: {
+				rate = -1;
+				break;
+			}
+		}
+		return rate;
+	}
+	int getOutputRatemydspSIG0(int channel) {
+		int rate;
+		switch ((channel)) {
+			case 0: {
+				rate = 0;
+				break;
+			}
+			default: {
+				rate = -1;
+				break;
+			}
+		}
+		return rate;
+	}
+	
+	void instanceInitmydspSIG0(int sample_rate) {
+		for (int l0 = 0; (l0 < 2); l0 = (l0 + 1)) {
+			iRec0[l0] = 0;
+		}
+	}
+	
+	void fillmydspSIG0(int count, float* table) {
+		for (int i = 0; (i < count); i = (i + 1)) {
+			iRec0[0] = (iRec0[1] + 1);
+			table[i] = std::sin((0.000191747604f * float((iRec0[0] + -1))));
+			iRec0[1] = iRec0[0];
+		}
+	}
+
+};
+
+static mydspSIG0* newmydspSIG0() { return (mydspSIG0*)new mydspSIG0(); }
+static void deletemydspSIG0(mydspSIG0* dsp) { delete dsp; }
+
+static float ftbl0mydspSIG0[32768];
+
+#ifndef FAUSTCLASS 
+#define FAUSTCLASS mydsp
+#endif
+
+#ifdef __APPLE__ 
+#define exp10f __exp10f
+#define exp10 __exp10
+#endif
+
+class mydsp : public dsp {
+	
+ private:
+	
+	int fSampleRate;
+	float fConst0;
+	float fConst1;
+	float fRec1[2];
+	float fConst2;
+	float fRec2[2];
+	float fConst3;
+	float fRec3[2];
+	float fConst4;
+	float fRec4[2];
+	
+ public:
+	
+	void metadata(Meta* m) { 
+		m->declare("basics.lib/name", "Faust Basic Element Library");
+		m->declare("basics.lib/version", "0.1");
+		m->declare("compile_options", "-lang cpp -es 1 -scal -ftz 0");
+		m->declare("filename", "faustDaisy.dsp");
+		m->declare("maths.lib/author", "GRAME");
+		m->declare("maths.lib/copyright", "GRAME");
+		m->declare("maths.lib/license", "LGPL with exception");
+		m->declare("maths.lib/name", "Faust Math Library");
+		m->declare("maths.lib/version", "2.3");
+		m->declare("name", "faustDaisy");
+		m->declare("oscillators.lib/name", "Faust Oscillator Library");
+		m->declare("oscillators.lib/version", "0.1");
+		m->declare("platform.lib/name", "Generic Platform Library");
+		m->declare("platform.lib/version", "0.1");
+	}
+
+	virtual int getNumInputs() {
+		return 0;
+	}
+	virtual int getNumOutputs() {
+		return 4;
+	}
+	virtual int getInputRate(int channel) {
+		int rate;
+		switch ((channel)) {
+			default: {
+				rate = -1;
+				break;
+			}
+		}
+		return rate;
+	}
+	virtual int getOutputRate(int channel) {
+		int rate;
+		switch ((channel)) {
+			case 0: {
+				rate = 1;
+				break;
+			}
+			case 1: {
+				rate = 1;
+				break;
+			}
+			case 2: {
+				rate = 1;
+				break;
+			}
+			case 3: {
+				rate = 1;
+				break;
+			}
+			default: {
+				rate = -1;
+				break;
+			}
+		}
+		return rate;
+	}
+	
+	static void classInit(int sample_rate) {
+		mydspSIG0* sig0 = newmydspSIG0();
+		sig0->instanceInitmydspSIG0(sample_rate);
+		sig0->fillmydspSIG0(32768, ftbl0mydspSIG0);
+		deletemydspSIG0(sig0);
+	}
+	
+	virtual void instanceConstants(int sample_rate) {
+		fSampleRate = sample_rate;
+		fConst0 = std::min<float>(192000.0f, std::max<float>(1.0f, float(fSampleRate)));
+		fConst1 = (50.0f / fConst0);
+		fConst2 = (100.0f / fConst0);
+		fConst3 = (200.0f / fConst0);
+		fConst4 = (400.0f / fConst0);
+	}
+	
+	virtual void instanceResetUserInterface() {
+	}
+	
+	virtual void instanceClear() {
+		for (int l1 = 0; (l1 < 2); l1 = (l1 + 1)) {
+			fRec1[l1] = 0.0f;
+		}
+		for (int l2 = 0; (l2 < 2); l2 = (l2 + 1)) {
+			fRec2[l2] = 0.0f;
+		}
+		for (int l3 = 0; (l3 < 2); l3 = (l3 + 1)) {
+			fRec3[l3] = 0.0f;
+		}
+		for (int l4 = 0; (l4 < 2); l4 = (l4 + 1)) {
+			fRec4[l4] = 0.0f;
+		}
+	}
+	
+	virtual void init(int sample_rate) {
+		classInit(sample_rate);
+		instanceInit(sample_rate);
+	}
+	virtual void instanceInit(int sample_rate) {
+		instanceConstants(sample_rate);
+		instanceResetUserInterface();
+		instanceClear();
+	}
+	
+	virtual mydsp* clone() {
+		return new mydsp();
+	}
+	
+	virtual int getSampleRate() {
+		return fSampleRate;
+	}
+	
+	virtual void buildUserInterface(UI* ui_interface) {
+		ui_interface->openVerticalBox("faustDaisy");
+		ui_interface->closeBox();
+	}
+	
+	virtual void compute(int count, FAUSTFLOAT** inputs, FAUSTFLOAT** outputs) {
+		FAUSTFLOAT* output0 = outputs[0];
+		FAUSTFLOAT* output1 = outputs[1];
+		FAUSTFLOAT* output2 = outputs[2];
+		FAUSTFLOAT* output3 = outputs[3];
+		for (int i = 0; (i < count); i = (i + 1)) {
+			fRec1[0] = (fConst1 + (fRec1[1] - std::floor((fConst1 + fRec1[1]))));
+			output0[i] = FAUSTFLOAT(ftbl0mydspSIG0[int((32768.0f * fRec1[0]))]);
+			fRec2[0] = (fConst2 + (fRec2[1] - std::floor((fConst2 + fRec2[1]))));
+			output1[i] = FAUSTFLOAT(ftbl0mydspSIG0[int((32768.0f * fRec2[0]))]);
+			fRec3[0] = (fConst3 + (fRec3[1] - std::floor((fConst3 + fRec3[1]))));
+			output2[i] = FAUSTFLOAT(ftbl0mydspSIG0[int((32768.0f * fRec3[0]))]);
+			fRec4[0] = (fConst4 + (fRec4[1] - std::floor((fConst4 + fRec4[1]))));
+			output3[i] = FAUSTFLOAT(ftbl0mydspSIG0[int((32768.0f * fRec4[0]))]);
+			fRec1[1] = fRec1[0];
+			fRec2[1] = fRec2[0];
+			fRec3[1] = fRec3[0];
+			fRec4[1] = fRec4[0];
+		}
+	}
+
+};
 
 /***************************END USER SECTION ***************************/
 
 /*******************BEGIN ARCHITECTURE SECTION (part 2/2)***************/
 
-DaisyPod hw;
+static DaisyPatch patch;
+
+// FAUSTFLOAT* finputs[2];
+// FAUSTFLOAT* foutputs[4];
+
 mydsp DSP;
 
 #define MY_BUFFER_SIZE 32
 
-static void AudioCallback(float** in, float** out, size_t count)
+static void AudioCallback(float** in, float** out, size_t size)
 {
+    //float output;
+
     // Faust processing
-    DSP.compute(count, in, out);
+    DSP.compute(size, in, out);
+
+
+    /* for(size_t i = 0; i < size; i ++)
+    {
+        out[0][i]  = foutputs[0][i];
+        out[1][i]  = foutputs[1][i];
+        out[2][i]  = foutputs[2][i];
+        out[3][i]  = foutputs[3][i];
+    } */
 }
 
 int main(void)
 {
     // initialize seed hardware and daisysp modules
-    hw.Configure();
-    hw.Init();
-    
+    float sample_rate;
+
+    //patch.Configure();
+    patch.Init();
+    sample_rate = patch.AudioSampleRate();
+
+    // MSM HoG initialize shift registers
+    /*for (int i = 0; i < 32; i++)
+    {
+	    patch.sr_595.Set(i, 0);
+    }
+    patch.sr_595.Write(); */    
+
     // set buffer-size
-    hw.SetAudioBlockSize(MY_BUFFER_SIZE);
+    patch.SetAudioBlockSize(MY_BUFFER_SIZE);
       
     // inti Faust DSP
-    DSP.init(hw.AudioSampleRate());
+    DSP.init(patch.AudioSampleRate());
  
     // start ADC
-    hw.StartAdc();
+    patch.StartAdc();
     
     // define and start callback
-    hw.StartAudio(AudioCallback)
- 
-#ifdef MIDICTRL
-    daisy_midi midi_handler;
-    midi_handler.startMidi();
-#endif
+    patch.StartAudio(AudioCallback);
 
     while(1) {
-    #ifdef MIDICTRL
-        midi_handler.processMidi();
-    #endif
+
     }
 }
 
