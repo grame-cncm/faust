9a9df15a65a0163ba961d1fa8d09f19a0b4dd0dd
Stephane Letz <letz@grame.fr>
2021-06-12 08:22:04 +0200

    Merge bouksimo98 Signal2VHDLVisitor version.

--- compiler/transform/signal2vhdlVisitor.hh
+++ compiler/transform/signal2vhdlVisitor.hh
@@ -27,17 +27,57 @@
 #include "sigtyperules.hh"
 #include "tree.hh"
 #include "treeTraversal.hh"
+//#include "codegen_vhdl.hh"
+#include "xtended.hh"
 
 //-------------------------Signal2VHDLVisitor-------------------------------
 // A a signal visitor used to compile signals to vhdl code
 //----------------------------------------------------------------------
+using namespace std;
 
 class Signal2VHDLVisitor : public TreeTraversal {
     bool fVisitGen;
+    set<Tree> fVisited;           // avoid visiting a tree twice
+    map<string, bool> fEntity;
+    string finput;
+    string fDeclEntity;
+    string fFaustEntity;
+    string fDeclSig;
+    string fDeclCompnt;
+    string fFaustProcess;
+    string fMapCompnt;
 
    public:
     Signal2VHDLVisitor() : TreeTraversal(), fVisitGen(false){};
 
+    void self(Tree t);
+    void sigToVHDL(Tree sig, ofstream& fout);
+    void treatment(string name, const char* op, Tree sig, Tree x, Tree y);
+
+    string addr_to_str(Tree t);
+    string val_to_str(Tree t);
+
+    void entity_header(string & str);
+    void generic_decl(string & str);
+    void port_decl(int input, string & str);
+
+    void entity_bin_op(string name, const char* op, string & str);  // arith + mod
+    void entity_delay(string & str);  // delay
+    void entity_bypass(string name, string & str);  // proj
+    void entity_faust(string & str);  // faust
+
+    void component_standard(string name, int input, string & str);  // arith + mod + proj
+    void component_delay(string & str); // delay
+
+    void faust_process(string & str);
+
+    void inst_delay(Tree sig, Tree x, Tree y, string & str); // delay
+    void inst_proj(Tree sig, Tree x, string & str); // proj -- passer sur le premier
+    void inst_bin_op(string name, Tree sig, Tree x, Tree y, string & str); // arith + mod
+
+    void decl_sig(Tree x, string & str);
+    void input_affectation(Tree sig, string & str);
+
    protected:
-    void visit(Tree t) override;
+    void visit(Tree sig) override;
 };
