66c05117f90148a2fc564d5e0a6a8afc682a6805
Stephane Letz <letz@grame.fr>
2021-06-12 09:00:12 +0200

    Cleanup and formatting.

--- compiler/transform/signal2vhdlVisitor.cpp
+++ compiler/transform/signal2vhdlVisitor.cpp
@@ -1,21 +1,21 @@
 /************************************************************************
  ************************************************************************
-    FAUST compiler
-    Copyright (C) 2003-2018 GRAME, Centre National de Creation Musicale
-    ---------------------------------------------------------------------
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ FAUST compiler
+ Copyright (C) 2003-2018 GRAME, Centre National de Creation Musicale
+ ---------------------------------------------------------------------
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************
  ************************************************************************/
 
@@ -41,22 +41,22 @@ static const char* binopname[] = {"+", "-", "*", "/", "%", "<<", ">>", ">", "<",
 
 void Signal2VHDLVisitor::sigToVHDL(Tree L, ofstream& fout)
 {
-  Tree Output = hd(L);
-  while (!isNil(L)) {
-      self(hd(L)); // comment
-      L = tl(L);
-  }
-  entity_faust(fDeclEntity);
-  faust_process(fFaustProcess);
-  fout << fDeclEntity << endl;
-  fout << fFaustEntity << endl;
-  fout << fDeclSig << endl;
-  fout << fDeclCompnt << endl;
-  fout << fFaustProcess << endl;
-  fout << fInput << endl;
-  fout << fMapCompnt << endl;
-  fout << "out_left_V_int <= to_slv(sig" << Output << ");\n" << endl;
-  fout << "end logic;" << endl;
+    Tree Output = hd(L);
+    while (!isNil(L)) {
+        self(hd(L)); // comment
+        L = tl(L);
+    }
+    entity_faust();
+    faust_process();
+    fout << fDeclEntity << endl;
+    fout << fFaustEntity << endl;
+    fout << fDeclSig << endl;
+    fout << fDeclCompnt << endl;
+    fout << fFaustProcess << endl;
+    fout << fInput << endl;
+    fout << fMapCompnt << endl;
+    fout << "out_left_V_int <= to_slv(sig" << Output << ");\n" << endl;
+    fout << "end logic;" << endl;
 }
 
 // comment
@@ -75,32 +75,32 @@ void Signal2VHDLVisitor::visit(Tree sig)
     double r;
     vector<Tree> subsig;
     Tree   c, sel, x, y, z, u, v, var, le, label, id, ff, largs, type, name, file, sf;
-
+    
     xtended* p = (xtended*)getUserData(sig);
-
+    
     if (p) {
-      if (strcmp(p->name(), "fmod") == 0) {
-        getSubSignals(sig, subsig);
-        treatment("FMOD", "mod", sig, subsig[0], subsig[1]);
-        self(subsig[0]);
-        self(subsig[1]);
-      } else {
-          for (Tree b : sig->branches()) {
-            self(b);
-          }
-      }
-
+        if (strcmp(p->name(), "fmod") == 0) {
+            getSubSignals(sig, subsig);
+            bin_op("FMOD", "mod", sig, subsig[0], subsig[1]);
+            self(subsig[0]);
+            self(subsig[1]);
+        } else {
+            for (Tree b : sig->branches()) {
+                self(b);
+            }
+        }
+        
         return;
     } else if (isSigInt(sig, &i)) {
-        decl_sig(sig, fDeclSig);
+        decl_sig(sig);
         return;
     } else if (isSigReal(sig, &r)) {
-        decl_sig(sig, fDeclSig);
+        decl_sig(sig);
         return;
     } else if (isSigWaveform(sig)) {
         return;
     } else if (isSigInput(sig, &i)) {
-        input_affectation(sig, fInput);
+        input_affectation(sig);
         return;
     } else if (isSigOutput(sig, &i, x)) {
         self(x);
@@ -110,11 +110,11 @@ void Signal2VHDLVisitor::visit(Tree sig)
         return;
     } else if (isSigFixDelay(sig, x, y)) {
         if (fEntity.count("DELAY") == 0) {
-          entity_delay(fDeclEntity);
-          component_delay(fDeclCompnt);
-          fEntity.insert({"DELAY", true});
+            entity_delay(fDeclEntity);
+            component_delay(fDeclCompnt);
+            fEntity.insert({"DELAY", true});
         }
-        decl_sig(sig, fDeclSig);
+        decl_sig(sig);
         inst_delay(sig, x, y, fMapCompnt);
         self(x);
         self(y);
@@ -128,35 +128,35 @@ void Signal2VHDLVisitor::visit(Tree sig)
         return;
     } else if (isSigBinOp(sig, &i, x, y)) {
         switch (i) {
-          case 0:
-            treatment("ADD", binopname[i], sig, x, y);
-            break;
-          case 1:
-            treatment("SUB", binopname[i], sig, x, y);
-            break;
-          case 2:
-            treatment("MUL", binopname[i], sig, x, y);
-            break;
-          case 3:
-            treatment("DIV", binopname[i], sig, x, y);
-            break;
-          case 4:
-            break;
-          case 13:
-            treatment("AND", "and", sig, x, y);
-            break;
-          case 14:
-            treatment("OR", "or", sig, x, y);
-            break;
-          default:
-            // operator is doesn't match any case constant (+, -, *, /)
-            cout << "Error! The operator is not correct";
-            break;
+            case 0:
+                bin_op("ADD", binopname[i], sig, x, y);
+                break;
+            case 1:
+                bin_op("SUB", binopname[i], sig, x, y);
+                break;
+            case 2:
+                bin_op("MUL", binopname[i], sig, x, y);
+                break;
+            case 3:
+                bin_op("DIV", binopname[i], sig, x, y);
+                break;
+            case 4:
+                break;
+            case 13:
+                bin_op("AND", "and", sig, x, y);
+                break;
+            case 14:
+                bin_op("OR", "or", sig, x, y);
+                break;
+            default:
+                // operator is doesn't match any case constant (+, -, *, /)
+                cout << "Error! The operator is not correct";
+                break;
         }
         self(x);
         self(y);
         return;
-    // Foreign functions
+        // Foreign functions
     } else if (isSigFFun(sig, ff, largs)) {
         mapself(largs);
         return;
@@ -165,7 +165,7 @@ void Signal2VHDLVisitor::visit(Tree sig)
     } else if (isSigFVar(sig, type, name, file)) {
         return;
     }
-
+    
     // Tables
     else if (isSigTable(sig, id, x, y)) {
         self(x);
@@ -181,7 +181,7 @@ void Signal2VHDLVisitor::visit(Tree sig)
         self(y);
         return;
     }
-
+    
     // Doc
     else if (isSigDocConstantTbl(sig, x, y)) {
         self(x);
@@ -198,7 +198,7 @@ void Signal2VHDLVisitor::visit(Tree sig)
         self(y);
         return;
     }
-
+    
     // Select2 and Select3
     else if (isSigSelect2(sig, sel, x, y)) {
         self(sel);
@@ -212,7 +212,7 @@ void Signal2VHDLVisitor::visit(Tree sig)
         self(z);
         return;
     }
-
+    
     // Table sigGen
     else if (isSigGen(sig, x)) {
         if (fVisitGen) {
@@ -222,21 +222,21 @@ void Signal2VHDLVisitor::visit(Tree sig)
             return;
         }
     }
-
+    
     // recursive signals
     else if (isProj(sig, &i, x)) {
         faustassert(isRec(x, var, le));
         if (fEntity.count("proj") == 0) {
-          entity_bypass("PROJ", fDeclEntity);
-          component_standard("PROJ", 1, fDeclCompnt);
-          fEntity.insert({"proj", true});
+            entity_bypass("PROJ", fDeclEntity);
+            component_standard("PROJ", 1, fDeclCompnt);
+            fEntity.insert({"proj", true});
         }
-        decl_sig(sig, fDeclSig);
+        decl_sig(sig);
         inst_proj(sig, nth(le, i), fMapCompnt);
         self(nth(le, i));
         return;
     }
-
+    
     // Int and Float Cast
     else if (isSigIntCast(sig, x)) {
         self(x);
@@ -245,7 +245,7 @@ void Signal2VHDLVisitor::visit(Tree sig)
         self(x);
         return;
     }
-
+    
     // UI
     else if (isSigButton(sig, label)) {
         return;
@@ -267,7 +267,7 @@ void Signal2VHDLVisitor::visit(Tree sig)
         self(x), self(y), self(z);
         return;
     }
-
+    
     // Soundfile length, rate, channels, buffer
     else if (isSigSoundfile(sig, label)) {
         return;
@@ -281,7 +281,7 @@ void Signal2VHDLVisitor::visit(Tree sig)
         self(sf), self(x), self(y), self(z);
         return;
     }
-
+    
     // Attach, Enable, Control
     else if (isSigAttach(sig, x, y)) {
         self(x), self(y);
@@ -293,7 +293,7 @@ void Signal2VHDLVisitor::visit(Tree sig)
         self(x), self(y);
         return;
     }
-
+    
     else if (isNil(sig)) {
         // now nil can appear in table write instructions
         return;
@@ -304,27 +304,33 @@ void Signal2VHDLVisitor::visit(Tree sig)
     }
 }
 
-void Signal2VHDLVisitor::treatment(const string& name, const char* op, Tree sig, Tree x, Tree y) {
-  if (fEntity.count(op) == 0) {
-    entity_bin_op(name, op, fDeclEntity);
-    component_standard(name, 2, fDeclCompnt);
-    fEntity.insert({op, true});
-  }
-  decl_sig(sig, fDeclSig);
-  inst_bin_op(name, sig, x, y, fMapCompnt);
+void Signal2VHDLVisitor::bin_op(const string& name, const char* op, Tree sig, Tree x, Tree y)
+{
+    if (fEntity.count(op) == 0) {
+        entity_bin_op(name, op, fDeclEntity);
+        component_standard(name, 2, fDeclCompnt);
+        fEntity.insert({op, true});
+    }
+    decl_sig(sig);
+    inst_bin_op(name, sig, x, y, fMapCompnt);
 }
 
-string Signal2VHDLVisitor::addr_to_str(Tree t) {
+string Signal2VHDLVisitor::addr_to_str(Tree t)
+{
     stringstream s;
     s << t;
-    return s.str(); }
+    return s.str();
+}
 
-string Signal2VHDLVisitor::val_to_str(Tree t) {
+string Signal2VHDLVisitor::val_to_str(Tree t)
+{
     stringstream s;
     s << *t;
-    return s.str(); }
+    return s.str();
+}
 
-void Signal2VHDLVisitor::entity_header(string & str) {
+void Signal2VHDLVisitor::entity_header(string & str)
+{
     str += "library ieee;\n"
     "use ieee.std_logic_1164.all;\n"
     "use ieee.numeric_std.all;\n"
@@ -333,21 +339,24 @@ void Signal2VHDLVisitor::entity_header(string & str) {
     "use work.fixed_pkg.all;\n\n";
 }
 
-void Signal2VHDLVisitor::generic_decl(string & str) {
+void Signal2VHDLVisitor::generic_decl(string & str)
+{
     str += "generic (\n"
     "    msb     : integer;\n"
     "    lsb     : integer);\n";
 }
 
-void Signal2VHDLVisitor::port_decl(int input, string & str) {
+void Signal2VHDLVisitor::port_decl(int input, string & str)
+{
     str += "port (\n";
     for (int i = 0; i < input; i++) {
-      str += "   input_"+ to_string(i) + "  : in  sfixed(msb downto lsb);\n";
+        str += "   input_"+ to_string(i) + "  : in  sfixed(msb downto lsb);\n";
     }
     str += "   output_0 : in  sfixed(msb downto lsb));\n";
 }
 
-void Signal2VHDLVisitor::entity_bin_op(const string& name, const char* op, string & str) {
+void Signal2VHDLVisitor::entity_bin_op(const string& name, const char* op, string & str)
+{
     entity_header(str);
     str += "entity " + name + " is\n";
     generic_decl(str);
@@ -359,7 +368,8 @@ void Signal2VHDLVisitor::entity_bin_op(const string& name, const char* op, strin
     "end behavioral;\n\n";
 }
 
-void Signal2VHDLVisitor::entity_delay(string & str) {
+void Signal2VHDLVisitor::entity_delay(string & str)
+{
     entity_header(str);
     str += "entity DELAY is\n"
     "generic (\n"
@@ -388,7 +398,8 @@ void Signal2VHDLVisitor::entity_delay(string & str) {
     "end behavioral;\n\n";
 }
 
-void Signal2VHDLVisitor::entity_bypass(const string& name, string & str) {
+void Signal2VHDLVisitor::entity_bypass(const string& name, string & str)
+{
     entity_header(str);
     str += "entity " + name + " is\n";
     generic_decl(str);
@@ -400,9 +411,10 @@ void Signal2VHDLVisitor::entity_bypass(const string& name, string & str) {
     "end behavioral;\n\n";
 }
 
-void Signal2VHDLVisitor::entity_faust(string & str) {
-    entity_header(str);
-    str += "entity FAUST_IP is\n"
+void Signal2VHDLVisitor::entity_faust()
+{
+    entity_header(fDeclEntity);
+    fDeclEntity += "entity FAUST_IP is\n"
     "port (\n"
     "  ap_clk : in std_logic;\n"
     "  ap_rst_n : in std_logic;\n"
@@ -429,14 +441,16 @@ void Signal2VHDLVisitor::entity_faust(string & str) {
     "signal    ap_start_reg : std_logic;";
 }
 
-void Signal2VHDLVisitor::component_standard(const string& name, int input, string & str) {
+void Signal2VHDLVisitor::component_standard(const string& name, int input, string & str)
+{
     str += "component " + name + " is\n";
     generic_decl(str);
     port_decl(input,str);
     str += "end component;\n\n";
 }
 
-void Signal2VHDLVisitor::component_delay(string & str) {
+void Signal2VHDLVisitor::component_delay(string & str)
+{
     str += "component DELAY is\n"
     "generic (\n"
     "    delay    : integer;\n"
@@ -449,8 +463,9 @@ void Signal2VHDLVisitor::component_delay(string & str) {
     "end component;\n\n";
 }
 
-void Signal2VHDLVisitor::faust_process(string & str) {
-    str += "begin\n\n"
+void Signal2VHDLVisitor::faust_process()
+{
+    fFaustProcess += "begin\n\n"
     " process(ap_clk, ap_rst_n)\n"
     "   variable clock_cnt : integer := 0;\n"
     " begin\n\n"
@@ -493,7 +508,8 @@ void Signal2VHDLVisitor::faust_process(string & str) {
     " ------------------------------------------------------------------------\n\n";
 }
 
-void Signal2VHDLVisitor::inst_delay(Tree sig, Tree x, Tree y, string & str) {
+void Signal2VHDLVisitor::inst_delay(Tree sig, Tree x, Tree y, string & str)
+{
     str += "DELAY_" + addr_to_str(sig) + " : delay\n"
     "generic map (\n"
     "    delay => "+ val_to_str(y) +",\n"
@@ -505,7 +521,8 @@ void Signal2VHDLVisitor::inst_delay(Tree sig, Tree x, Tree y, string & str) {
     "    output_1 => sig"+ addr_to_str(sig) +");\n\n";
 }
 
-void Signal2VHDLVisitor::inst_proj(Tree sig, Tree x, string & str) {
+void Signal2VHDLVisitor::inst_proj(Tree sig, Tree x, string & str)
+{
     str += "PROJ_" + addr_to_str(sig) + " : PROJ\n"
     "generic map (\n"
     "    msb => 1,\n"
@@ -515,27 +532,30 @@ void Signal2VHDLVisitor::inst_proj(Tree sig, Tree x, string & str) {
     "    output_1 => sig"+ addr_to_str(sig) +");\n\n";
 }
 
-void Signal2VHDLVisitor::inst_bin_op(const string& name, Tree sig, Tree x, Tree y, string & str) {
-  str += name + "_" + addr_to_str(sig) + " : " + name + "\n"
-  "generic map (\n"
-  "    msb => 1,\n"
-  "    lsb => -22)\n"
-  "port map (\n"
-  "    input_0  => sig"+ addr_to_str(x) +",\n"
-  "    input_1  => sig"+ addr_to_str(y) +",\n"
-  "    output_1 => sig"+ addr_to_str(sig) +");\n\n";
+void Signal2VHDLVisitor::inst_bin_op(const string& name, Tree sig, Tree x, Tree y, string & str)
+{
+    str += name + "_" + addr_to_str(sig) + " : " + name + "\n"
+    "generic map (\n"
+    "    msb => 1,\n"
+    "    lsb => -22)\n"
+    "port map (\n"
+    "    input_0  => sig"+ addr_to_str(x) +",\n"
+    "    input_1  => sig"+ addr_to_str(y) +",\n"
+    "    output_1 => sig"+ addr_to_str(sig) +");\n\n";
 }
 
-void Signal2VHDLVisitor::decl_sig(Tree sig, string & str) {
-  int i;
-  double r;
-  if (isSigInt(sig, &i) || isSigReal(sig, &r)) {
-    str += "signal    sig"+addr_to_str(sig)+" : sfixed(1 downto -22) := to_sfixed(" + val_to_str(sig) + ",1,-22);\n";
-  } else {
-    str += "signal    sig"+ addr_to_str(sig) +" : sfixed(1 downto -22) :\n";
-  }
+void Signal2VHDLVisitor::decl_sig(Tree sig)
+{
+    int i;
+    double r;
+    if (isSigInt(sig, &i) || isSigReal(sig, &r)) {
+        fDeclSig += "signal    sig"+addr_to_str(sig)+" : sfixed(1 downto -22) := to_sfixed(" + val_to_str(sig) + ",1,-22);\n";
+    } else {
+        fDeclSig += "signal    sig"+ addr_to_str(sig) +" : sfixed(1 downto -22) :\n";
+    }
 }
 
-void Signal2VHDLVisitor::input_affectation(Tree sig, string & str) {
-  str += "sig" + addr_to_str(sig) + " <= to_sfixed(in_left_V_buf,1,-22);\n";
+void Signal2VHDLVisitor::input_affectation(Tree sig)
+{
+    fInput += "sig" + addr_to_str(sig) + " <= to_sfixed(in_left_V_buf,1,-22);\n";
 }
--- compiler/transform/signal2vhdlVisitor.hh
+++ compiler/transform/signal2vhdlVisitor.hh
@@ -1,21 +1,21 @@
 /************************************************************************
  ************************************************************************
-    FAUST compiler
-    Copyright (C) 2003-2018 GRAME, Centre National de Creation Musicale
-    ---------------------------------------------------------------------
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ FAUST compiler
+ Copyright (C) 2003-2018 GRAME, Centre National de Creation Musicale
+ ---------------------------------------------------------------------
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  ************************************************************************
  ************************************************************************/
 
@@ -35,48 +35,51 @@
 using namespace std;
 
 class Signal2VHDLVisitor : public TreeTraversal {
-    bool fVisitGen;
-    set<Tree> fVisited;           // avoid visiting a tree twice
-    map<string, bool> fEntity;
-    string fInput;
-    string fDeclEntity;
-    string fFaustEntity;
-    string fDeclSig;
-    string fDeclCompnt;
-    string fFaustProcess;
-    string fMapCompnt;
 
-   public:
-    Signal2VHDLVisitor() : TreeTraversal(), fVisitGen(false){};
-
-    void self(Tree t);
-    void sigToVHDL(Tree sig, ofstream& fout);
-    void treatment(const string& name, const char* op, Tree sig, Tree x, Tree y);
-
-    string addr_to_str(Tree t);
-    string val_to_str(Tree t);
-
-    void entity_header(string & str);
-    void generic_decl(string & str);
-    void port_decl(int input, string & str);
-
-    void entity_bin_op(const string& name, const char* op, string & str);  // arith + mod
-    void entity_delay(string & str);  // delay
-    void entity_bypass(const string& name, string & str);  // proj
-    void entity_faust(string & str);  // faust
-
-    void component_standard(const string& name, int input, string & str);  // arith + mod + proj
-    void component_delay(string & str); // delay
-
-    void faust_process(string & str);
-
-    void inst_delay(Tree sig, Tree x, Tree y, string & str); // delay
-    void inst_proj(Tree sig, Tree x, string & str); // proj -- passer sur le premier
-    void inst_bin_op(const string& name, Tree sig, Tree x, Tree y, string & str); // arith + mod
-
-    void decl_sig(Tree x, string & str);
-    void input_affectation(Tree sig, string & str);
-
-   protected:
-    void visit(Tree sig) override;
+    private:
+        bool fVisitGen;
+        set<Tree> fVisited;           // avoid visiting a tree twice
+        map<string, bool> fEntity;
+        string fInput;
+        string fDeclEntity;
+        string fFaustEntity;
+        string fDeclSig;
+        string fDeclCompnt;
+        string fFaustProcess;
+        string fMapCompnt;
+    
+        void bin_op(const string& name, const char* op, Tree sig, Tree x, Tree y);
+    
+        string addr_to_str(Tree t);
+        string val_to_str(Tree t);
+    
+        void entity_header(string & str);
+        void generic_decl(string & str);
+        void port_decl(int input, string & str);
+    
+        void entity_bin_op(const string& name, const char* op, string & str);  // arith + mod
+        void entity_delay(string & str);  // delay
+        void entity_bypass(const string& name, string & str);  // proj
+        void entity_faust();  // faust
+    
+        void component_standard(const string& name, int input, string & str);  // arith + mod + proj
+        void component_delay(string & str); // delay
+    
+        void faust_process();
+    
+        void inst_delay(Tree sig, Tree x, Tree y, string & str); // delay
+        void inst_proj(Tree sig, Tree x, string & str); // proj -- passer sur le premier
+        void inst_bin_op(const string& name, Tree sig, Tree x, Tree y, string & str); // arith + mod
+    
+        void decl_sig(Tree x);
+        void input_affectation(Tree sig);
+    
+    public:
+        Signal2VHDLVisitor() : TreeTraversal(), fVisitGen(false){};
+    
+        void self(Tree t);
+        void sigToVHDL(Tree sig, ofstream& fout);
+    
+    protected:
+        void visit(Tree sig) override;
 };
