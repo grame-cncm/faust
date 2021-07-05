f9aac268c0fbd4c15eef0442ecb8910edd5002d2
Stephane Letz <letz@grame.fr>
2021-07-05 16:07:01 +0200

    SHAKey code reworked to use public domain Steve Reid <steve@edmweb.com> code.

--- compiler/generator/sha_key.hh
+++ compiler/generator/sha_key.hh
@@ -23,200 +23,288 @@
 #define SHA_KEY_H
 
 #include "export.hh"
+#include "stdint.h"
 
-// Code by: B-Con (http://b-con.us)
-// Released under the GNU GPL
-// MD5 Hash Digest implementation (little endian byte order)
+/*
+ SHA-1 in C (see: https://github.com/clibs/sha1/blob/master/sha1.h)
+ By Steve Reid <steve@edmweb.com>
+ 100% Public Domain
+ */
 
-// Signed variables are for wimps
-#define uchar unsigned char
-#define uint unsigned int
-
-// DBL_INT_ADD treats two unsigned ints a and b as one 64-bit integer and adds c to it
-#define ROTLEFT(a, b) ((a << b) | (a >> (32 - b)))
-#define DBL_INT_ADD(a, b, c)     \
-    if (a > 0xffffffff - c) ++b; \
-    a += c;
+#define SHA1HANDSOFF
 
 typedef struct {
-    uchar data[64];
-    uint  datalen;
-    uint  bitlen[2];
-    uint  state[5];
-    uint  k[4];
+    uint32_t state[5];
+    uint32_t count[2];
+    unsigned char buffer[64];
 } SHA1_CTX;
 
-inline void sha1_transform(SHA1_CTX* ctx, uchar data[])
+#define rol(value, bits) (((value) << (bits)) | ((value) >> (32 - (bits))))
+
+/* blk0() and blk() perform the initial expand. */
+/* I got the idea of expanding during the round function from SSLeay */
+#if BYTE_ORDER == LITTLE_ENDIAN
+#define blk0(i) (block->l[i] = (rol(block->l[i],24)&0xFF00FF00) \
+    |(rol(block->l[i],8)&0x00FF00FF))
+#elif BYTE_ORDER == BIG_ENDIAN
+#define blk0(i) block->l[i]
+#else
+#error "Endianness not defined!"
+#endif
+#define blk(i) (block->l[i&15] = rol(block->l[(i+13)&15]^block->l[(i+8)&15] \
+    ^block->l[(i+2)&15]^block->l[i&15],1))
+
+/* (R0+R1), R2, R3, R4 are the different operations used in SHA1 */
+#define R0(v,w,x,y,z,i) z+=((w&(x^y))^y)+blk0(i)+0x5A827999+rol(v,5);w=rol(w,30);
+#define R1(v,w,x,y,z,i) z+=((w&(x^y))^y)+blk(i)+0x5A827999+rol(v,5);w=rol(w,30);
+#define R2(v,w,x,y,z,i) z+=(w^x^y)+blk(i)+0x6ED9EBA1+rol(v,5);w=rol(w,30);
+#define R3(v,w,x,y,z,i) z+=(((w|x)&y)|(w&x))+blk(i)+0x8F1BBCDC+rol(v,5);w=rol(w,30);
+#define R4(v,w,x,y,z,i) z+=(w^x^y)+blk(i)+0xCA62C1D6+rol(v,5);w=rol(w,30);
+
+/* Hash a single 512-bit block. This is the core of the algorithm. */
+inline void SHA1Transform(uint32_t state[5], const unsigned char buffer[64])
 {
-    uint a, b, c, d, e, i, j, t, m[80];
-
-    for (i = 0, j = 0; i < 16; ++i, j += 4) {
-        m[i] = (data[j] << 24) + (data[j + 1] << 16) + (data[j + 2] << 8) + (data[j + 3]);
-    }
-    for (; i < 80; ++i) {
-        m[i] = (m[i - 3] ^ m[i - 8] ^ m[i - 14] ^ m[i - 16]);
-        m[i] = (m[i] << 1) | (m[i] >> 31);
-    }
-
-    a = ctx->state[0];
-    b = ctx->state[1];
-    c = ctx->state[2];
-    d = ctx->state[3];
-    e = ctx->state[4];
-
-    for (i = 0; i < 20; ++i) {
-        t = ROTLEFT(a, 5) + ((b & c) ^ (~b & d)) + e + ctx->k[0] + m[i];
-        e = d;
-        d = c;
-        c = ROTLEFT(b, 30);
-        b = a;
-        a = t;
-    }
-    for (; i < 40; ++i) {
-        t = ROTLEFT(a, 5) + (b ^ c ^ d) + e + ctx->k[1] + m[i];
-        e = d;
-        d = c;
-        c = ROTLEFT(b, 30);
-        b = a;
-        a = t;
-    }
-    for (; i < 60; ++i) {
-        t = ROTLEFT(a, 5) + ((b & c) ^ (b & d) ^ (c & d)) + e + ctx->k[2] + m[i];
-        e = d;
-        d = c;
-        c = ROTLEFT(b, 30);
-        b = a;
-        a = t;
-    }
-    for (; i < 80; ++i) {
-        t = ROTLEFT(a, 5) + (b ^ c ^ d) + e + ctx->k[3] + m[i];
-        e = d;
-        d = c;
-        c = ROTLEFT(b, 30);
-        b = a;
-        a = t;
-    }
-
-    ctx->state[0] += a;
-    ctx->state[1] += b;
-    ctx->state[2] += c;
-    ctx->state[3] += d;
-    ctx->state[4] += e;
+    uint32_t a, b, c, d, e;
+    
+    typedef union {
+        unsigned char c[64];
+        uint32_t l[16];
+    } CHAR64LONG16;
+    
+#ifdef SHA1HANDSOFF
+    CHAR64LONG16 block[1];      /* use array to appear as a pointer */
+    memcpy(block, buffer, 64);
+#else
+    /* The following had better never be used because it causes the
+     * pointer-to-const buffer to be cast into a pointer to non-const.
+     * And the result is written through. I threw a "const" in, hoping
+     * this will cause a diagnostic.
+     */
+    CHAR64LONG16* block = (const CHAR64LONG16*)buffer;
+#endif
+    /* Copy context->state[] to working vars */
+    a = state[0];
+    b = state[1];
+    c = state[2];
+    d = state[3];
+    e = state[4];
+    /* 4 rounds of 20 operations each. Loop unrolled. */
+    R0(a, b, c, d, e, 0);
+    R0(e, a, b, c, d, 1);
+    R0(d, e, a, b, c, 2);
+    R0(c, d, e, a, b, 3);
+    R0(b, c, d, e, a, 4);
+    R0(a, b, c, d, e, 5);
+    R0(e, a, b, c, d, 6);
+    R0(d, e, a, b, c, 7);
+    R0(c, d, e, a, b, 8);
+    R0(b, c, d, e, a, 9);
+    R0(a, b, c, d, e, 10);
+    R0(e, a, b, c, d, 11);
+    R0(d, e, a, b, c, 12);
+    R0(c, d, e, a, b, 13);
+    R0(b, c, d, e, a, 14);
+    R0(a, b, c, d, e, 15);
+    R1(e, a, b, c, d, 16);
+    R1(d, e, a, b, c, 17);
+    R1(c, d, e, a, b, 18);
+    R1(b, c, d, e, a, 19);
+    R2(a, b, c, d, e, 20);
+    R2(e, a, b, c, d, 21);
+    R2(d, e, a, b, c, 22);
+    R2(c, d, e, a, b, 23);
+    R2(b, c, d, e, a, 24);
+    R2(a, b, c, d, e, 25);
+    R2(e, a, b, c, d, 26);
+    R2(d, e, a, b, c, 27);
+    R2(c, d, e, a, b, 28);
+    R2(b, c, d, e, a, 29);
+    R2(a, b, c, d, e, 30);
+    R2(e, a, b, c, d, 31);
+    R2(d, e, a, b, c, 32);
+    R2(c, d, e, a, b, 33);
+    R2(b, c, d, e, a, 34);
+    R2(a, b, c, d, e, 35);
+    R2(e, a, b, c, d, 36);
+    R2(d, e, a, b, c, 37);
+    R2(c, d, e, a, b, 38);
+    R2(b, c, d, e, a, 39);
+    R3(a, b, c, d, e, 40);
+    R3(e, a, b, c, d, 41);
+    R3(d, e, a, b, c, 42);
+    R3(c, d, e, a, b, 43);
+    R3(b, c, d, e, a, 44);
+    R3(a, b, c, d, e, 45);
+    R3(e, a, b, c, d, 46);
+    R3(d, e, a, b, c, 47);
+    R3(c, d, e, a, b, 48);
+    R3(b, c, d, e, a, 49);
+    R3(a, b, c, d, e, 50);
+    R3(e, a, b, c, d, 51);
+    R3(d, e, a, b, c, 52);
+    R3(c, d, e, a, b, 53);
+    R3(b, c, d, e, a, 54);
+    R3(a, b, c, d, e, 55);
+    R3(e, a, b, c, d, 56);
+    R3(d, e, a, b, c, 57);
+    R3(c, d, e, a, b, 58);
+    R3(b, c, d, e, a, 59);
+    R4(a, b, c, d, e, 60);
+    R4(e, a, b, c, d, 61);
+    R4(d, e, a, b, c, 62);
+    R4(c, d, e, a, b, 63);
+    R4(b, c, d, e, a, 64);
+    R4(a, b, c, d, e, 65);
+    R4(e, a, b, c, d, 66);
+    R4(d, e, a, b, c, 67);
+    R4(c, d, e, a, b, 68);
+    R4(b, c, d, e, a, 69);
+    R4(a, b, c, d, e, 70);
+    R4(e, a, b, c, d, 71);
+    R4(d, e, a, b, c, 72);
+    R4(c, d, e, a, b, 73);
+    R4(b, c, d, e, a, 74);
+    R4(a, b, c, d, e, 75);
+    R4(e, a, b, c, d, 76);
+    R4(d, e, a, b, c, 77);
+    R4(c, d, e, a, b, 78);
+    R4(b, c, d, e, a, 79);
+    /* Add the working vars back into context.state[] */
+    state[0] += a;
+    state[1] += b;
+    state[2] += c;
+    state[3] += d;
+    state[4] += e;
+    /* Wipe variables */
+    a = b = c = d = e = 0;
+#ifdef SHA1HANDSOFF
+    memset(block, '\0', sizeof(block));
+#endif
 }
 
-inline void sha1_init(SHA1_CTX* ctx)
+/* SHA1Init - Initialize new context */
+inline void SHA1Init(SHA1_CTX* context)
 {
-    ctx->datalen   = 0;
-    ctx->bitlen[0] = 0;
-    ctx->bitlen[1] = 0;
-    ctx->state[0]  = 0x67452301;
-    ctx->state[1]  = 0xEFCDAB89;
-    ctx->state[2]  = 0x98BADCFE;
-    ctx->state[3]  = 0x10325476;
-    ctx->state[4]  = 0xc3d2e1f0;
-    ctx->k[0]      = 0x5a827999;
-    ctx->k[1]      = 0x6ed9eba1;
-    ctx->k[2]      = 0x8f1bbcdc;
-    ctx->k[3]      = 0xca62c1d6;
+    /* SHA1 initialization constants */
+    context->state[0] = 0x67452301;
+    context->state[1] = 0xEFCDAB89;
+    context->state[2] = 0x98BADCFE;
+    context->state[3] = 0x10325476;
+    context->state[4] = 0xC3D2E1F0;
+    context->count[0] = context->count[1] = 0;
 }
 
-inline void sha1_update(SHA1_CTX* ctx, uchar data[], uint len)
+/* Run your data through this. */
+inline void SHA1Update(SHA1_CTX* context, const unsigned char* data, uint32_t len)
 {
-    uint i;
-
-    for (i = 0; i < len; ++i) {
-        ctx->data[ctx->datalen] = data[i];
-        ctx->datalen++;
-        if (ctx->datalen == 64) {
-            sha1_transform(ctx, ctx->data);
-            DBL_INT_ADD(ctx->bitlen[0], ctx->bitlen[1], 512);
-            ctx->datalen = 0;
+    uint32_t i;
+    uint32_t j;
+    
+    j = context->count[0];
+    if ((context->count[0] += len << 3) < j) {
+        context->count[1]++;
+    }
+    context->count[1] += (len >> 29);
+    j = (j >> 3) & 63;
+    if ((j + len) > 63) {
+        memcpy(&context->buffer[j], data, (i = 64 - j));
+        SHA1Transform(context->state, context->buffer);
+        for (; i + 63 < len; i += 64) {
+            SHA1Transform(context->state, &data[i]);
         }
-    }
-}
-
-static void sha1_final(SHA1_CTX* ctx, uchar hash[])
-{
-    uint i;
-
-    i = ctx->datalen;
-
-    // Pad whatever data is left in the buffer.
-    if (ctx->datalen < 56) {
-        ctx->data[i++] = 0x80;
-        while (i < 56) {
-            ctx->data[i++] = 0x00;
-        }
+        j = 0;
     } else {
-        ctx->data[i++] = 0x80;
-        while (i < 64) {
-            ctx->data[i++] = 0x00;
-        }
-        sha1_transform(ctx, ctx->data);
-        memset(ctx->data, 0, 56);
+        i = 0;
     }
-
-    // Append to the padding the total message's length in bits and transform.
-    DBL_INT_ADD(ctx->bitlen[0], ctx->bitlen[1], 8 * ctx->datalen);
-    ctx->data[63] = ctx->bitlen[0];
-    ctx->data[62] = ctx->bitlen[0] >> 8;
-    ctx->data[61] = ctx->bitlen[0] >> 16;
-    ctx->data[60] = ctx->bitlen[0] >> 24;
-    ctx->data[59] = ctx->bitlen[1];
-    ctx->data[58] = ctx->bitlen[1] >> 8;
-    ctx->data[57] = ctx->bitlen[1] >> 16;
-    ctx->data[56] = ctx->bitlen[1] >> 24;
-    sha1_transform(ctx, ctx->data);
-
-    // Since this implementation uses little endian byte ordering and MD uses big endian,
-    // reverse all the bytes when copying the final state to the output hash.
-    for (i = 0; i < 4; ++i) {
-        hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
-        hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
-        hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
-        hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
-        hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
-    }
+    memcpy(&context->buffer[j], &data[i], len - i);
 }
+
+/* Add padding and return the message digest. */
+void SHA1Final(unsigned char digest[20], SHA1_CTX* context)
+{
+    unsigned i;
+    unsigned char finalcount[8];
+    unsigned char c;
+    
+#if 0    /* untested "improvement" by DHR */
+    /* Convert context->count to a sequence of bytes
+     * in finalcount.  Second element first, but
+     * big-endian order within element.
+     * But we do it all backwards.
+     */
+    unsigned char* fcp = &finalcount[8];
+    
+    for (i = 0; i < 2; i++) {
+        uint32_t t = context->count[i];
+        int j;
+        for (j = 0; j < 4; t >>= 8, j++) {
+            *--fcp = (unsigned char)t
+        }
+    }
+#else
+    for (i = 0; i < 8; i++) {
+        finalcount[i] = (unsigned char) ((context->count[(i >= 4 ? 0 : 1)] >> ((3 - (i & 3)) * 8)) & 255);      /* Endian independent */
+    }
+#endif
+    c = 0200;
+    SHA1Update(context, &c, 1);
+    while ((context->count[0] & 504) != 448) {
+        c = 0000;
+        SHA1Update(context, &c, 1);
+    }
+    SHA1Update(context, finalcount, 8); /* Should cause a SHA1Transform() */
+    for (i = 0; i < 20; i++) {
+        digest[i] = (unsigned char)
+        ((context->state[i >> 2] >> ((3 - (i & 3)) * 8)) & 255);
+    }
+    /* Wipe variables */
+    memset(context, '\0', sizeof(*context));
+    memset(&finalcount, '\0', sizeof(finalcount));
+}
+
+inline void SHA1(char* hash_out, const char* str, unsigned int len)
+{
+    SHA1_CTX ctx;
+    SHA1Init(&ctx);
+    for (unsigned int ii = 0; ii < len; ii += 1) {
+        SHA1Update(&ctx, (const unsigned char*)str + ii, 1);
+    }
+    SHA1Final((unsigned char*)hash_out, &ctx);
+}
+
+// External C++ libfaust API
 
 EXPORT std::string generateSHA1(const std::string& data)
 {
-    SHA1_CTX      ctx;
+    SHA1_CTX ctx;
     unsigned char obuf[20] = {0};
-
-    // Hash one
-    sha1_init(&ctx);
-    sha1_update(&ctx, (unsigned char*)data.c_str(), int(data.size()));
-    sha1_final(&ctx, obuf);
-
-    // convert SHA1 key into hexadecimal string
-    std::string sha1key;
+     
+    SHA1Init(&ctx);
+    SHA1Update(&ctx, (unsigned char const*)data.c_str(), data.size());
+    SHA1Final(obuf, &ctx);
+    
+    std::string sha_key;
     for (int i = 0; i < 20; i++) {
         const char* H  = "0123456789ABCDEF";
         char        c1 = H[(obuf[i] >> 4)];
         char        c2 = H[(obuf[i] & 15)];
-        sha1key += c1;
-        sha1key += c2;
+        sha_key += c1;
+        sha_key += c2;
     }
-
-    return sha1key;
+    
+    return sha_key;
 }
 
 // External C libfaust API
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-    
-EXPORT void generateCSHA1(const char* data, char* sha_key)
+extern "C" EXPORT void generateCSHA1(const char* data, char* sha_key)
 {
-    SHA1_CTX      ctx;
+    SHA1_CTX ctx;
     unsigned char obuf[20] = {0};
-
-    // Hash one
-    sha1_init(&ctx);
-    sha1_update(&ctx, (unsigned char*)data, strlen(data));
-    sha1_final(&ctx, obuf);
+    
+    SHA1Init(&ctx);
+    SHA1Update(&ctx, (unsigned char const*)data, strlen(data));
+    SHA1Final(obuf, &ctx);
 
     // convert SHA1 key into hexadecimal string
     for (int i = 0; i < 20; i++) {
@@ -227,9 +315,5 @@ EXPORT void generateCSHA1(const char* data, char* sha_key)
         sha_key += c2;
     }
 }
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
