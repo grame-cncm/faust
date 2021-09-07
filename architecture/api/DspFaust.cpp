6275eabbde7bc736c69bf44278bd343d27e90f94
Stephane Letz <letz@grame.fr>
2021-09-07 16:51:59 +0200

    Remove use of <iostream> in faust2api architecture files.

--- architecture/api/DspFaust.cpp
+++ architecture/api/DspFaust.cpp
@@ -19,6 +19,7 @@
 
 #include <cmath>
 #include <cstring>
+#include <stdio.h>
 
 #include "faust/misc.h"
 #include "faust/gui/DecoratorUI.h"
@@ -160,7 +161,7 @@ DspFaust::DspFaust(bool auto_connect)
     // JUCE audio device has its own sample rate and buffer size
     driver = new juceaudio();
 #else
-    std::cerr << "You are not setting 'sample_rate' and 'buffer_size', but the audio driver needs it !\n";
+    printf("You are not setting 'sample_rate' and 'buffer_size', but the audio driver needs it !\n");
     throw std::bad_alloc();
 #endif
     init(NULL, driver);
@@ -179,18 +180,18 @@ DspFaust::DspFaust(const string& dsp_content, int sample_rate, int buffer_size, 
     // Is dsp_content a filename ?
     fFactory = createDSPFactoryFromFile(dsp_content, 0, NULL, "", error_msg, -1);
     if (!fFactory) {
-        std::cerr << error_msg;
+        fprintf(stderr, "ERROR : %s", error_msg.c_str());
         // Is dsp_content a string ?
         fFactory = createDSPFactoryFromString("FaustDSP", dsp_content, 0, NULL, "", error_msg);
         if (!fFactory) {
-            std::cerr << error_msg;
+            fprintf(stderr, "ERROR : %s", error_msg.c_str());
             throw bad_alloc();
         }
     }
 
     dsp* dsp = fFactory->createDSPInstance();
     if (!dsp) {
-        std::cerr << "Cannot allocate DSP instance\n";
+        fprintf(stderr, "Cannot allocate DSP instance\n");
         throw bad_alloc();
     }
     init(dsp, createDriver(sample_rate, buffer_size, auto_connect));
@@ -205,13 +206,13 @@ audio* DspFaust::createDriver(int sample_rate, int buffer_size, bool auto_connec
     audio* driver = new iosaudio(sample_rate, buffer_size);
 #elif ANDROID_DRIVER
     // OBOE has its own and buffer size
-    std::cerr << "You are setting 'buffer_size' with a driver that does not need it !\n";
+    fprintf(stderr, "You are setting 'buffer_size' with a driver that does not need it !\n");
     audio* driver = new oboeaudio(-1);
 #elif ALSA_DRIVER
     audio* driver = new alsaaudio(sample_rate, buffer_size);
 #elif JACK_DRIVER
     // JACK has its own sample rate and buffer size
-    std::cerr << "You are setting 'sample_rate' and 'buffer_size' with a driver that does not need it !\n";
+    fprintf(stderr, "You are setting 'sample_rate' and 'buffer_size' with a driver that does not need it !\n");
 #if MIDICTRL
     audio* driver = new jackaudio_midi(auto_connect);
 #else
@@ -225,7 +226,7 @@ audio* DspFaust::createDriver(int sample_rate, int buffer_size, bool auto_connec
     audio* driver = new ofaudio(sample_rate, buffer_size);
 #elif JUCE_DRIVER
     // JUCE audio device has its own sample rate and buffer size
-    std::cerr << "You are setting 'sample_rate' and 'buffer_size' with a driver that does not need it !\n";
+    fprintf(stderr, "You are setting 'sample_rate' and 'buffer_size' with a driver that does not need it !\n");
     audio* driver = new juceaudio();
 #elif DUMMY_DRIVER
     audio* driver = new dummyaudio(sample_rate, buffer_size);
@@ -325,7 +326,7 @@ bool DspFaust::start()
 #endif
 #if MIDICTRL
     if (!fMidiInterface->run()) {
-        std::cerr << "MIDI run error...\n";
+        fprintf(stderr, "MIDI run error...\n");
     }
 #endif
 	return fPolyEngine->start();
@@ -556,7 +557,7 @@ int main(int argc, char* argv[])
 {
 #ifdef DYNAMIC_DSP
     if (argc == 1) {
-        std::cout << "./dynamic-api <foo.dsp> \n";
+        printf("./dynamic-api <foo.dsp> \n");
         exit(-1);
     }
     DspFaust* dsp = new DspFaust(argv[1], 44100, 512);
@@ -564,7 +565,7 @@ int main(int argc, char* argv[])
     DspFaust* dsp = new DspFaust(44100, 512);
 #endif
     dsp->start();
-    std::cout << "Type 'q' to quit\n";
+    printf("Type 'q' to quit\n");
     char c;
     while ((c = getchar()) && (c != 'q')) { usleep(100000); }
     dsp->stop();
