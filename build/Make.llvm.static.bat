d2e89f9d66e02ee26314323e3cba98841ff43166
Dominique Fober <fober@grame.fr>
2021-11-22 16:55:32 +0100

    build libfaust with embedded llvm libraries

---
+++ build/Make.llvm.static.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+
+SET OUTLIB=lib\libfaustwithllvm.lib
+FOR /F "delims=" %%i IN ('llvm-config --libdir') DO SET LLVMDIR=%%i
+FOR /F "delims=" %%i IN ('llvm-config --libnames') DO SET libnames=%%i
+
+set in=%libnames: =" "!LLVMDIR!\%
+set inputlibs=lib/libfaust.lib "!LLVMDIR!\!in!"
+
+@echo "Generating the library %OUTLIB%"
+lib.exe /OUT:%OUTLIB% /NOLOGO !inputlibs!
