06ce3adfae6341e2e55e916b46c3614c03ff156a
Dominique Fober <fober@grame.fr>
2021-11-26 08:48:44 +0100

    report link-static option on windows

--- build/Make.llvm.static.bat
+++ build/Make.llvm.static.bat
@@ -3,7 +3,7 @@ SETLOCAL EnableDelayedExpansion
 
 SET OUTLIB=lib\libfaustwithllvm.lib
 FOR /F "delims=" %%i IN ('llvm-config --libdir') DO SET LLVMDIR=%%i
-FOR /F "delims=" %%i IN ('llvm-config --libnames') DO SET libnames=%%i
+FOR /F "delims=" %%i IN ('llvm-config --libnames  --link-static') DO SET libnames=%%i
 
 set in=%libnames: =" "!LLVMDIR!\%
 set inputlibs=lib/libfaust.lib "!LLVMDIR!\!in!"
