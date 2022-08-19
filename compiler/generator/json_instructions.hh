978d36a2ad3f60ba9c3e73bcb9f123ccf878d893
Stephane Letz <letz@grame.fr>
2022-08-19 20:27:05 +0200

    Correct Make.julia and Make.soul, cleanup JSONInstVisitor.

--- compiler/generator/json_instructions.hh
+++ compiler/generator/json_instructions.hh
@@ -46,12 +46,12 @@ struct JSONInstVisitor : public DispatchVisitor, public JSONUIReal<REAL> {
  
     using DispatchVisitor::visit;
     
-    const string& insertPath(set<string>& table, const string& path, bool check = true)
+    const string& insertPath(const string& path, bool check = true)
     {
-        if (check && table.find(path) != table.end()) {
+        if (check && fControlPathSet.find(path) != fControlPathSet.end()) {
             throw faustexception("ERROR : path '" + path + "' is already used\n");
         } else {
-            table.insert(path);
+            fControlPathSet.insert(path);
         }
         return path;
     }
@@ -106,7 +106,7 @@ struct JSONInstVisitor : public DispatchVisitor, public JSONUIReal<REAL> {
                 break;
         }
         faustassert(fPathTable.find(inst->fZone) == fPathTable.end());
-        fPathTable[inst->fZone] = insertPath(fControlPathSet, this->buildPath(inst->fLabel));
+        fPathTable[inst->fZone] = insertPath(this->buildPath(inst->fLabel));
     }
 
     virtual void visit(AddSliderInst* inst)
@@ -126,7 +126,7 @@ struct JSONInstVisitor : public DispatchVisitor, public JSONUIReal<REAL> {
                 break;
         }
         faustassert(fPathTable.find(inst->fZone) == fPathTable.end());
-        fPathTable[inst->fZone] = insertPath(fControlPathSet, this->buildPath(inst->fLabel));
+        fPathTable[inst->fZone] = insertPath(this->buildPath(inst->fLabel));
     }
 
     virtual void visit(AddBargraphInst* inst)
@@ -143,14 +143,14 @@ struct JSONInstVisitor : public DispatchVisitor, public JSONUIReal<REAL> {
                 break;
         }
         faustassert(fPathTable.find(inst->fZone) == fPathTable.end());
-        fPathTable[inst->fZone] = insertPath(fControlPathSet, this->buildPath(inst->fLabel), false);
+        fPathTable[inst->fZone] = insertPath(this->buildPath(inst->fLabel), false);
     }
 
     virtual void visit(AddSoundfileInst* inst)
     {
         this->addSoundfile(inst->fLabel.c_str(), inst->fURL.c_str(), nullptr);
         faustassert(fPathTable.find(inst->fSFZone) == fPathTable.end());
-        fPathTable[inst->fSFZone] = insertPath(fControlPathSet, this->buildPath(inst->fLabel));
+        fPathTable[inst->fSFZone] = insertPath(this->buildPath(inst->fLabel));
     }
 
     void setInputs(int input) { this->fInputs = input; }
