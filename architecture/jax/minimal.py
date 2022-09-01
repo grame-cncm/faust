00d1f2eb22e14a31f297fdc4972eaded17272f9c
David Braun <2096055+DBraun@users.noreply.github.com>
2022-09-01 07:49:16 -0700

    improve jax architecture minimal.py

--- architecture/jax/minimal.py
+++ architecture/jax/minimal.py
@@ -18,51 +18,66 @@
 <<includeIntrinsic>>
 <<includeclass>>
 
+from jax import random
 
-SAMPLERATE = 44100
+def test(args):
 
+    dtype = jnp.float32  # todo: pass dtype to the model
 
-def test():
+    model = mydsp(sample_rate=args.sample_rate)
+    print("NUM INPUT CHANNELS: ", model.getNumInputs())
+    print("NUM OUTPUT CHANNELS: ", model.getNumOutputs())
 
-    # Init DSP
-    my_dsp = mydsp(sample_rate=SAMPLERATE)
-    print("getNumInputs: ", my_dsp.getNumInputs())
-    print("getNumOutputs: ", my_dsp.getNumOutputs())
+    # import json
+    # json_str = model.getJSON()
+    # json_obj = json.loads(json_str)
+    # print('json_obj: ', json_obj)
 
-    import json
+    key = random.PRNGKey(0)
 
-    json_str = my_dsp.getJSON()
-    json_obj = json.loads(json_str)
-    print('json_obj: ', json_obj)
-    
-    # # Create a MapUI controller
-    # map_ui = MapUI(my_dsp)
-    # my_dsp.buildUserInterface(map_ui)
+    if args.input is not None:
+        input_audio, _ = librosa.load(args.input, mono=False, sr=args.sample_rate, duration=args.duration)
+        if input_audio.ndim == 1:
+            input_audio = input_audio.unsqueeze(0)
 
-    # # Print all zones
-    # print("Path/UIZone dictionary: ", getZoneMap(map_ui))
-     
-    # # Possibly change control values
-    # - using simple labels (end of path):
-    # map_ui.setParamValue("freq", 500.0)
-    # map_ui.setParamValue("/volume", -10.0)
-    # - or using complete path:
-    # map_ui.setParamValue("/Oscillator/freq", 500.0)
-    # map_ui.setParamValue("/Oscillator/volume", -10.0)
+        N_SAMPLES = input_audio.shape[1]
+        N_CHANNELS = input_audio.shape[0]
+        assert N_CHANNELS == model.getNumInputs()
 
-    duration = 1.
+        input_audio = dtype(input_audio)
+    else:
+        duration_sec = args.duration or 1.  # default to 1 second when making noise.
 
-    inputs = torch.zeros(my_dsp.getNumInputs(), int(SAMPLERATE*duration))
-    output = my_dsp(inputs)
+        N_SAMPLES = int(duration_sec*args.sample_rate)
+        N_CHANNELS = model.getNumInputs()
 
-    assert output.ndim == 2
-    assert output.shape[0] == my_dsp.getNumOutputs()
-    assert output.shape[1] == inputs.shape[1]
+        # input_audio = jnp.ones((N_CHANNELS, N_SAMPLES), dtype)
+        input_audio = random.uniform(key, shape=(N_CHANNELS, N_SAMPLES), dtype=dtype)
+
+    variables = model.init({'params': key}, input_audio, N_SAMPLES)  
+    y, mod_vars = model.apply(variables, input_audio, N_SAMPLES, mutable='intermediates')
+
+    assert y.ndim == 2
+    assert y.shape[0] == model.getNumOutputs()
+    assert y.shape[1] == input_audio.shape[1]
+    assert y.shape[1] == N_SAMPLES
+
+    if args.output is not None:
+        from scipy.io import wavfile
+        import numpy as np
+        output_audio = np.array(y).T
+        wavfile.write(args.output, args.sample_rate, output_audio)
         
-    # todo: display the outputs
-
-def main():
-    pass
 
 if __name__ == '__main__':
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description='Run a JAX/Flax model converted from Faust code')
+    parser.add_argument('-sr', '--sample-rate', type=int, default=44100, help='Sample rate (such as 44100)')
+    parser.add_argument('-d', '--duration', type=float, default=None, help='Duration in seconds')
+    parser.add_argument('-i', '--input', type=str, default=None, help='Filepath for input audio WAV')
+    parser.add_argument('-o', '--output', type=str, default=None, help='Filepath for output audio WAV')
+
+    args = parser.parse_args()
+
+    test(args)
+    print('All done!')
