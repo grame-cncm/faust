ddb53e162552ed8148c6746a6faf3d8d18e28ff2
Stephane Letz <letz@grame.fr>
2022-10-04 08:21:55 +0200

    Remove unused includes.

--- compiler/generator/julia/julia_code_container.hh
+++ compiler/generator/julia/julia_code_container.hh
@@ -25,9 +25,7 @@
 #include "julia_instructions.hh"
 #include "code_container.hh"
 #include "dsp_factory.hh"
-#include "omp_code_container.hh"
 #include "vec_code_container.hh"
-#include "wss_code_container.hh"
 
 #ifdef WIN32
 #pragma warning(disable : 4250)
