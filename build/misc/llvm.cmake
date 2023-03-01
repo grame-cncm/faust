8be8b6c3fce93abad847c2e3f5fc0ec4de3cfa6b
David Braun <2096055+DBraun@users.noreply.github.com>
2023-03-01 14:05:59 -0800

    attempt

--- build/misc/llvm.cmake
+++ build/misc/llvm.cmake
@@ -116,7 +116,7 @@ macro (llvm_cmake)
 		message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
 		message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
 		# Find the libraries that correspond to the LLVM components that we wish to use
-		if(WIN32)
+		if(MSVC)
 		# Normally we'd just want to do the following:
 		# execute_process(COMMAND ${LLVM_DIR}/../../../Release/bin/llvm-config.exe --libs all
 		#                 OUTPUT_VARIABLE LLVM_LIBS)
@@ -132,7 +132,7 @@ macro (llvm_cmake)
 		
 		string(STRIP "${LLVM_LIBS}" LLVM_LIBS)
 
-		if(!WIN32)
+		if(!MSVC)
 		# Expecting to find -lz -lpthread -ledit -lcurses -lm
 		execute_process(COMMAND ${LLVM_DIR}/../../../bin/llvm-config --system-libs
                 OUTPUT_VARIABLE LLVM_SYSLIBS)
