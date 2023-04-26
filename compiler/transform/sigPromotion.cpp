99ae1ce96e0a4b9c1f8058c7e4b6cfb61fc5a375
Stephane Letz <letz@grame.fr>
2023-04-26 13:34:47 +0200

    Improve -norm2 impression.

--- compiler/transform/sigPromotion.cpp
+++ compiler/transform/sigPromotion.cpp
@@ -59,9 +59,9 @@ string SignalTypePrinter::print()
 
 void SignalTypePrinter::visit(Tree sig)
 {
-    stringstream type;
-    type << "Type = " << getCertifiedSigType(sig) << endl;
-    fPrinted.push_back(type.str());
+    stringstream res;
+    res << "Sig = " << ppsig(sig, MAX_ERROR_SIZE) << ", "<< getCertifiedSigType(sig) << endl;
+    fPrinted.push_back(res.str());
     
     // Default case and recursion
     SignalVisitor::visit(sig);
