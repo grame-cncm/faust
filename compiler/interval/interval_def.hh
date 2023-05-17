f47fb6940cd9107fbe3c3cbd5c2599d79d4e957f
Agathe Herrou <herrou@grame.fr>
2023-05-17 11:19:57 +0200

    Updates the intervals library

--- compiler/interval/interval_def.hh
+++ compiler/interval/interval_def.hh
@@ -24,11 +24,11 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <limits>
+#include <limits.h>
 #include <algorithm>
 #include <string>
 
-#include "global.hh"
+// #include "global"
 
 // ***************************************************************************
 //
@@ -114,7 +114,7 @@ class interval {
         // can be < 1.0, in which case the msb will be negative and indicate the number of implicit leading zeroes
         double range = std::max(std::abs(fLo), std::abs(fHi));
 
-        if (isinf(range)) return gGlobal->gVHDLFloatMSB; 
+        if (std::isinf(range)) return 20; // max MSB of the VHDL design; TODO: change when integrating in the compiler
 
         // The sign bit will be added later on
         return int(std::floor(std::log2(range)));
