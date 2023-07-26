97d7c8cf922b17c779df0423a0ad094d98977db8
Stephane Letz <letz@grame.fr>
2023-07-26 22:43:32 +0200

    Correct extract_items_info in rnbo.py.

--- architecture/max-msp/rnbo.py
+++ architecture/max-msp/rnbo.py
@@ -3,6 +3,13 @@ from py2max.py2max import *
 import argparse
 import json
 
+'''
+Takes the DSP compiled as codebox file, the JSON file, an the output file.Codebox will be added in the codebox~ object.
+Parsing the JSON file gives:
+    - the list of parameters (button, checkbox, sliders, nentry) to be added as "set param" objects
+    - the number of audio inputs/outputs to be added in the subpatch
+'''
+
 def gen_faust_rnbo(dsp_path, json_path, output_path):
 
     # Extracting items info
@@ -12,24 +19,33 @@ def gen_faust_rnbo(dsp_path, json_path, output_path):
             for item in ui_items:
                 if "shortname" in item:
                     shortname = item["shortname"]
+                    # slider and nentry
                     if "min" in item and "max" in item and "step" in item:
                         init_value = item["init"]
                         min_value = item["min"]
                         max_value = item["max"]
                         step_value = item["step"]
                         info_list.append({"shortname": shortname, "init": init_value, "min": min_value, "max": max_value, "step": step_value})
+                    # button
+                    if "type" in item and item["type"] == "button":
+                        info_list.append({"shortname": shortname, "init": 0, "min": 0, "max": 1, "step": 1})
+                    # checkbox
+                    if "type" in item and item["type"] == "checkbox":
+                        info_list.append({"shortname": shortname, "init": 0, "min": 0, "max": 1, "step": 1})
+                # possibly recurse
                 if "items" in item:
                     info_list.extend(extract_from_ui(item["items"]))
             return info_list
 
         # Extracting items info from the "ui" section
-        short_names = []
+        info_list = []
         if "ui" in json_data:
             ui_section = json_data["ui"]
-            short_names.extend(extract_from_ui(ui_section))
+            info_list.extend(extract_from_ui(ui_section))
 
-        return short_names
+        return info_list
         
+    # Create the patcher
     patcher = Patcher(output_path)
     
     # Faust generated patch
@@ -38,26 +54,26 @@ def gen_faust_rnbo(dsp_path, json_path, output_path):
     # Add the audio driver output
     audio_out = patcher.add_textbox('ezdac~', patching_rect = [50.0, 100.0, 50.0, 50.0]);
 
-    # Opening JSON file
-    json_file = open(json_path)
-
-    # Read the file and parse it
-    json_data = json.loads(json_file.read())
-
     # Create RNBO object
     sbox = patcher.add_rnbo(patching_rect = [50.0, 60.0, 50.0, 50.0])
 
     # Create the subpatcher
     sp = sbox.subpatcher
 
-    # Opening JSON file
+    # Opening codebox file
     codebox_file = open(dsp_path)
     
     # Create codebox~ section
     codebox = sp.add_codebox_tilde(
-        code = codebox_file.read(),					  # required
+        code = codebox_file.read(),                     # required
         patching_rect = [200.0, 200.0, 400.0, 400.0]   # optional
     )
+
+    # Opening JSON file
+    json_file = open(json_path)
+
+    # Read the file and parse it
+    json_data = json.loads(json_file.read())
 
     # Create inputs
     if "inputs" in json_data:
@@ -80,14 +96,19 @@ def gen_faust_rnbo(dsp_path, json_path, output_path):
 
     # Add parameter control
     items_info_list = extract_items_info(json_data)
+    #print(items_info_list)
     for item in items_info_list:
         param = sp.add_textbox('set ' + item["shortname"])
         rect = param.patching_rect;
-        value = sp.add_textbox('number~', mode=1, comment = item["shortname"], comment_pos = 'below',
-            patching_rect = [rect[0], rect[1]+25, rect[2], rect[3]])
+        value = sp.add_textbox('number~', 
+            mode = 1, 
+            comment = item["shortname"], 
+            comment_pos = 'below',
+            patching_rect = [rect[0], rect[1]-25, rect[2], rect[3]])
         sp.add_line(value, param)
         sp.add_line(param, codebox)
 
+    # And finally save the patch
     patcher.save()
 
 # Parse command line
