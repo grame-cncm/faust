36eb9d78cc1e157cef578ce662979425f5b62739
Stephane Letz <letz@grame.fr>
2023-07-27 07:13:40 +0200

    Add button/checkbox handling, add init/min/max values in rnbo.py.

--- architecture/max-msp/rnbo.py
+++ architecture/max-msp/rnbo.py
@@ -1,3 +1,26 @@
+'''
+ FAUST Architecture File
+ Copyright (C) 2023 GRAME, Centre National de Creation Musicale
+ ---------------------------------------------------------------------
+ This Architecture section is free software; you can redistribute it
+ and/or modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either version 3
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; If not, see <http://www.gnu.org/licenses/>.
+ 
+ EXCEPTION : As a special exception, you may create a larger work
+ that contains this FAUST architecture section and distribute
+ that work under terms of your choice, so long as this FAUST
+ architecture section is not modified.
+'''
+
 from py2max.py2max import *
 
 import argparse
@@ -25,13 +48,13 @@ def gen_faust_rnbo(dsp_path, json_path, output_path):
                         min_value = item["min"]
                         max_value = item["max"]
                         step_value = item["step"]
-                        info_list.append({"shortname": shortname, "init": init_value, "min": min_value, "max": max_value, "step": step_value})
+                        info_list.append({"shortname": shortname, "type": item["type"], "init": init_value, "min": min_value, "max": max_value, "step": step_value})
                     # button
                     if "type" in item and item["type"] == "button":
-                        info_list.append({"shortname": shortname, "init": 0, "min": 0, "max": 1, "step": 1})
+                        info_list.append({"shortname": shortname, "type": item["type"], "init": 0, "min": 0, "max": 1, "step": 1})
                     # checkbox
                     if "type" in item and item["type"] == "checkbox":
-                        info_list.append({"shortname": shortname, "init": 0, "min": 0, "max": 1, "step": 1})
+                        info_list.append({"shortname": shortname, "type": item["type"], "init": 0, "min": 0, "max": 1, "step": 1})
                 # possibly recurse
                 if "items" in item:
                     info_list.extend(extract_from_ui(item["items"]))
@@ -100,13 +123,19 @@ def gen_faust_rnbo(dsp_path, json_path, output_path):
     for item in items_info_list:
         param = sp.add_textbox('set ' + item["shortname"])
         rect = param.patching_rect;
-        value = sp.add_textbox('number~', 
-            mode = 1, 
-            comment = item["shortname"], 
-            comment_pos = 'below',
-            patching_rect = [rect[0], rect[1]-25, rect[2], rect[3]])
-        sp.add_line(value, param)
-        sp.add_line(param, codebox)
+        if item["type"] == "button" or item["type"] == "checkbox":
+            value = sp.add_textbox('toggle', patching_rect = [rect[0], rect[1]-25, 24.0, 24.0])
+            sp.add_line(value, param)
+            sp.add_line(param, codebox)
+        else:
+            value = sp.add_textbox('number~',
+                mode = 1,
+                sig = item["init"],
+                minimum = item["min"],
+                maximum = item["max"],
+                patching_rect = [rect[0], rect[1]-25, rect[2], rect[3]])
+            sp.add_line(value, param)
+            sp.add_line(param, codebox)
 
     # And finally save the patch
     patcher.save()
