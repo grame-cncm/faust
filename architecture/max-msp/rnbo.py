5af57592543825e32207ae2b6e20d619b8e388bb
Stephane Letz <letz@grame.fr>
2023-07-31 10:44:42 +0200

    Cleanup rnbo.py file, alternate params handling WIP.

--- architecture/max-msp/rnbo.py
+++ architecture/max-msp/rnbo.py
@@ -1,4 +1,4 @@
-'''
+"""
  FAUST Architecture File
  Copyright (C) 2023 GRAME, Centre National de Creation Musicale
  ---------------------------------------------------------------------
@@ -19,134 +19,180 @@
  that contains this FAUST architecture section and distribute
  that work under terms of your choice, so long as this FAUST
  architecture section is not modified.
-'''
+"""
 
-from py2max.py2max import *
-
-import argparse
-import json
-
-'''
-Take the DSP compiled as codebox file, the JSON file, and the maxpat output file. Codebox code will be added in the codebox~ object.
+"""
+Take the DSP name, the DSP compiled as codebox file, the JSON file, and the maxpat output file name. Codebox code will be added in the codebox~ object.
 Parsing the JSON file gives:
     - the list of parameters (button, checkbox, sliders, nentry) to be added as "set param" objects
     - the number of audio inputs/outputs to be added in the subpatch
-'''
+The maxpat file is generated with the following structure: 
+    - rnbo~ object
+        - subpatch
+            - codebox~ object
+            - input boxes
+            - output boxes
+            - parameter boxes
+            - lines between boxes
+    - ezdac~ object
+"""
 
-def gen_faust_rnbo(dsp_name, codebox_path, json_path, maxpat_path):
+from py2max.py2max import *
+import argparse
+import json
 
-    # Extracting items info
-    def extract_items_info(json_data):
-        def extract_from_ui(ui_items):
-            info_list = []
-            for item in ui_items:
-                if "shortname" in item:
-                    shortname = item["shortname"]
-                    # slider and nentry
-                    if "min" in item and "max" in item and "step" in item:
-                        init_value = item["init"]
-                        min_value = item["min"]
-                        max_value = item["max"]
-                        step_value = item["step"]
-                        info_list.append({"shortname": shortname, "type": item["type"], "init": init_value, "min": min_value, "max": max_value, "step": step_value})
-                    # button
-                    if "type" in item and item["type"] == "button":
-                        info_list.append({"shortname": shortname, "type": item["type"], "init": 0, "min": 0, "max": 1, "step": 1})
-                    # checkbox
-                    if "type" in item and item["type"] == "checkbox":
-                        info_list.append({"shortname": shortname, "type": item["type"], "init": 0, "min": 0, "max": 1, "step": 1})
-                # possibly recurse
-                if "items" in item:
-                    info_list.extend(extract_from_ui(item["items"]))
-            return info_list
 
-        # Extracting items info from the "ui" section
+# Extracting items info
+def extract_items_info(json_data):
+    def extract_from_ui(ui_items):
         info_list = []
-        if "ui" in json_data:
-            ui_section = json_data["ui"]
-            info_list.extend(extract_from_ui(ui_section))
+        for item in ui_items:
+            shortname = item.get("shortname", "")
+            item_type = item.get("type", "")
 
+            # button and checkbox
+            if item_type in ["button", "checkbox"]:
+                info_list.append(
+                    {
+                        "shortname": shortname,
+                        "type": item_type,
+                        "init": 0,
+                        "min": 0,
+                        "max": 1,
+                        "step": 1,
+                    }
+                )
+            # slider and nentry
+            elif "min" in item and "max" in item and "step" in item:
+                init_value = item.get("init", 0)
+                min_value = item["min"]
+                max_value = item["max"]
+                step_value = item["step"]
+                info_list.append(
+                    {
+                        "shortname": shortname,
+                        "type": item_type,
+                        "init": init_value,
+                        "min": min_value,
+                        "max": max_value,
+                        "step": step_value,
+                    }
+                )
+
+            # possibly recurse
+            if "items" in item:
+                info_list.extend(extract_from_ui(item["items"]))
         return info_list
-        
+
+    # Extracting items info from the "ui" section
+    if "ui" in json_data:
+        ui_section = json_data["ui"]
+        return extract_from_ui(ui_section)
+    return []
+
+
+def create_rnbo_patch(
+    maxpat_path, dsp_name, codebox_code, items_info_list, num_inputs, num_outputs
+):
     # Create the patcher
     patcher = Patcher(maxpat_path)
-    
-    # Faust generated patch
-    patcher.add_comment('Faust generated RNBO patch, Copyright (c) 2023 Grame', patching_rect = [50.0, 20.0, 250.0, 50.0])
-    
-    # Add the audio driver output
-    audio_out = patcher.add_textbox('ezdac~', patching_rect = [50.0, 100.0, 50.0, 50.0]);
 
-    # Create RNBO object
-    sbox = patcher.add_rnbo(saved_object_attributes=dict(optimization="O3", title=dsp_name), patching_rect = [50.0, 60.0, 50.0, 50.0])
+    # Faust generated patch
+    patcher.add_comment(
+        "Faust generated RNBO patch, Copyright (c) 2023 Grame",
+        patching_rect=[50.0, 20.0, 250.0, 50.0],
+    )
+
+    # Add the audio driver output
+    audio_out = patcher.add_textbox("ezdac~", patching_rect=[50.0, 100.0, 50.0, 50.0])
+    sbox = patcher.add_rnbo(
+        saved_object_attributes=dict(optimization="O3", title=dsp_name),
+        patching_rect=[50.0, 60.0, 50.0, 50.0],
+    )
 
     # Create the subpatcher
     sp = sbox.subpatcher
 
-    # Opening codebox file
-    codebox_file = open(codebox_path)
-    
     # Create codebox~ section
     codebox = sp.add_codebox_tilde(
-        code = codebox_file.read(),                   # required
-        patching_rect = [200.0, 200.0, 400.0, 400.0]  # optional
+        code=codebox_code, patching_rect=[200.0, 200.0, 400.0, 400.0]
     )
 
-    # Opening JSON file
-    json_file = open(json_path)
-
-    # Read the file and parse it
-    json_data = json.loads(json_file.read())
-
-    # Create inputs
-    if "inputs" in json_data:
-        num_inputs = json_data["inputs"]
-  
     # Generating the lines of code for inputs
-    for i in range(0, num_inputs):
-        input = sp.add_textbox('in~ ' + str(i+1))
-        sp.add_line(input, codebox, inlet=i)
+    for i in range(num_inputs):
+        input_box = sp.add_textbox(f"in~ {i + 1}")
+        sp.add_line(input_box, codebox, inlet=i)
 
-    # Create outputs
-    if "outputs" in json_data:
-        num_outputs = json_data["outputs"]
-  
     # Generating the lines of code for outputs
-    for i in range(0, num_outputs):
-        output = sp.add_textbox('out~ ' + str(i+1))
-        sp.add_line(codebox, output, outlet=i)
-        #patcher.add_line(sbox, audio_out, inlet=i)
+    for i in range(num_outputs):
+        output_box = sp.add_textbox(f"out~ {i + 1}")
+        sp.add_line(codebox, output_box, outlet=i)
 
     # Add parameter control
-    items_info_list = extract_items_info(json_data)
-    #print(items_info_list)
     for item in items_info_list:
-        param = sp.add_textbox('set ' + item["shortname"])
-        rect = param.patching_rect;
-        if item["type"] in ["button", "checkbox"]:
-            value = sp.add_textbox('toggle', patching_rect = [rect[0], rect[1]-25, 24.0, 24.0])
-            sp.add_line(value, param)
-            sp.add_line(param, codebox)
+        shortname = item["shortname"]
+        item_type = item["type"]
+
+        param = sp.add_textbox(f"set {shortname}")
+        # param = sp.add_textbox(f'param~ {shortname} @minimum {min_value} @maximum {max_value}')
+
+        if item_type in ["button", "checkbox"]:
+            value = sp.add_textbox(
+                "toggle",
+                patching_rect=[
+                    param.patching_rect[0],
+                    param.patching_rect[1] - 25,
+                    24.0,
+                    24.0,
+                ],
+            )
         else:
-            value = sp.add_textbox('number~',
-                mode = 1,
-                sig = item["init"],
-                minimum = item["min"],
-                maximum = item["max"],
-                patching_rect = [rect[0], rect[1]-25, rect[2], rect[3]])
-            sp.add_line(value, param)
-            sp.add_line(param, codebox)
+            min_value = item["min"]
+            max_value = item["max"]
+            init_value = item["init"]
+            value = sp.add_textbox(
+                "number~",
+                mode=1,
+                sig=init_value,
+                minimum=min_value,
+                maximum=max_value,
+                patching_rect=[
+                    param.patching_rect[0],
+                    param.patching_rect[1] - 25,
+                    param.patching_rect[2],
+                    param.patching_rect[3],
+                ],
+            )
+        sp.add_line(value, param)
+        sp.add_line(param, codebox)
 
     # And finally save the patch
     patcher.save()
 
-# Parse command line
-parser = argparse.ArgumentParser()
-parser.add_argument('arg1', type=str, help='DSP name')
-parser.add_argument('arg2', type=str, help='Codebox file')
-parser.add_argument('arg3', type=str, help='JSON file')
-parser.add_argument('arg4', type=str, help='RNBO maxpat file')
-args = parser.parse_args()
 
-# Create the Faust RNBO patch
-gen_faust_rnbo(args.arg1, args.arg2, args.arg3, args.arg4)
+# Generate RNBO patch
+def gen_faust_rnbo(dsp_name, codebox_path, json_path, maxpat_path):
+    with open(codebox_path) as codebox_file:
+        codebox_code = codebox_file.read()
+
+    with open(json_path) as json_file:
+        json_data = json.load(json_file)
+        items_info_list = extract_items_info(json_data)
+        num_inputs = json_data.get("inputs", 0)
+        num_outputs = json_data.get("outputs", 0)
+
+    create_rnbo_patch(
+        maxpat_path, dsp_name, codebox_code, items_info_list, num_inputs, num_outputs
+    )
+
+
+# Parse command line
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("arg1", type=str, help="DSP name")
+    parser.add_argument("arg2", type=str, help="Codebox file")
+    parser.add_argument("arg3", type=str, help="JSON file")
+    parser.add_argument("arg4", type=str, help="RNBO maxpat file")
+    args = parser.parse_args()
+
+    gen_faust_rnbo(args.arg1, args.arg2, args.arg3, args.arg4)
