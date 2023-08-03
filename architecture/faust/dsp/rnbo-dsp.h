863921814f593f6be27ca46409bbf3b566349a60
Stephane Letz <letz@grame.fr>
2023-08-03 12:37:13 +0200

    Codebox backend working with a patched version of RNBO C++ sources.

--- architecture/faust/dsp/rnbo-dsp.h
+++ architecture/faust/dsp/rnbo-dsp.h
@@ -1,3 +1,30 @@
+/************************** BEGIN rnbo-dsp.h **************************
+ FAUST Architecture File
+ Copyright (C) 2003-2023 GRAME, Centre National de Creation Musicale
+ ---------------------------------------------------------------------
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 2.1 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ 
+ EXCEPTION : As a special exception, you may create a larger work
+ that contains this FAUST architecture section and distribute
+ that work under terms of your choice, so long as this FAUST
+ architecture section is not modified.
+ ***************************************************************************/
+
+#ifndef __rnbo_dsp__
+#define __rnbo_dsp__
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -48,7 +75,6 @@ class rnbo_dsp : public dsp {
             // Inlined
             // classInit(sample_rate);
             instanceInit(sample_rate);
-        
         }
         
         int getNumInputs() { return fDSP.getNumInputChannels(); }
@@ -143,3 +169,7 @@ class rnbo_dsp : public dsp {
         }
     
 };
+
+#endif
+
+/************************** END rnbo-dsp.h **************************/
