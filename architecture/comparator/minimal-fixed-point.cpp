105b3b753f4ad2f0870e56e5f8185a03d9e5f232
Stephane Letz <letz@grame.fr>
2023-10-26 14:32:16 +0200

    Formatting.

--- architecture/comparator/minimal-fixed-point.cpp
+++ architecture/comparator/minimal-fixed-point.cpp
@@ -38,8 +38,6 @@
 #include "faust/gui/UI.h"
 #include "faust/gui/meta.h"
 #include "faust/dsp/dsp.h"
-#include "faust/audio/dummy-audio.h"
-#include "faust/dsp/one-sample-dsp.h"
 
 #if defined(SOUNDFILE)
 #include "faust/gui/SoundUI.h"
@@ -47,118 +45,12 @@
 
 #include "ap_fixed.h"
 
-
-
 typedef ap_fixed<32,8,AP_RND_CONV,AP_SAT> fixpoint_t;
+
 // m: position of the most significant bit of the value, without taking the sign bit into account
 // l: LSB with negative coding
 #define sfx_t(m,l) ap_fixed<((m+1)-l+1),(m+1)+1,AP_RND_CONV,AP_SAT>
 #define ufx_t(m,l) ap_ufixed<((m+1)-l),(m+1),AP_RND_CONV,AP_SAT>
-
-/*
-// fx version
-inline fixpoint_t fabsfx(fixpoint_t x)
-{
-    return fixpoint_t(std::fabs(float(x)));
-}
-inline fixpoint_t acosfx(fixpoint_t x)
-{
-    return fixpoint_t(std::acos(float(x)));
-}
-inline fixpoint_t asinfx(fixpoint_t x)
-{
-    return fixpoint_t(std::asin(float(x)));
-}
-inline fixpoint_t atanfx(fixpoint_t x)
-{
-    return fixpoint_t(std::atan(float(x)));
-}
-inline fixpoint_t atan2fx(fixpoint_t x, fixpoint_t y)
-{
-    return fixpoint_t(std::atan2(float(x), float(y)));
-}
-inline fixpoint_t ceilfx(fixpoint_t x)
-{
-    return fixpoint_t(std::ceil(float(x)));
-}
-inline fixpoint_t cosfx(fixpoint_t x)
-{
-    return fixpoint_t(std::cos(float(x)));
-}
-inline fixpoint_t expfx(fixpoint_t x)
-{
-    return fixpoint_t(std::exp(float(x)));
-}
-inline fixpoint_t exp2fx(fixpoint_t x)
-{
-    return fixpoint_t(std::exp2(float(x)));
-}
-inline fixpoint_t exp10fx(fixpoint_t x)
-{
-#ifdef __APPLE__
-    return fixpoint_t(__exp10f(float(x)));
-#else
-    return fixpoint_t(std::exp10(float(x)));
-#endif
-}
-inline fixpoint_t floorfx(fixpoint_t x)
-{
-    return fixpoint_t(std::floor(float(x)));
-}
-inline fixpoint_t fmodfx(fixpoint_t x, fixpoint_t y)
-{
-    return fixpoint_t(std::fmod(float(x), float(y)));
-}
-inline fixpoint_t logfx(fixpoint_t x)
-{
-    return fixpoint_t(std::log(float(x)));
-}
-inline fixpoint_t log2fx(fixpoint_t x)
-{
-    return fixpoint_t(std::log2(float(x)));
-}
-inline fixpoint_t log10fx(fixpoint_t x)
-{
-    return fixpoint_t(std::log10(float(x)));
-}
-inline fixpoint_t powfx(fixpoint_t x, fixpoint_t y)
-{
-    return fixpoint_t(std::pow(float(x), float(y)));
-}
-inline fixpoint_t remainderfx(fixpoint_t x, fixpoint_t y)
-{
-    return fixpoint_t(std::remainder(float(x), float(y)));
-}
-inline fixpoint_t rintfx(fixpoint_t x)
-{
-    return fixpoint_t(std::rint(float(x)));
-}
-inline fixpoint_t roundfx(fixpoint_t x)
-{
-    return fixpoint_t(std::round(float(x)));
-}
-inline fixpoint_t sinfx(fixpoint_t x)
-{
-    return fixpoint_t(std::sin(float(x)));
-}
-inline fixpoint_t sqrtfx(fixpoint_t x)
-{
-    return fixpoint_t(std::sqrt(float(x)));
-}
-inline fixpoint_t tanfx(fixpoint_t x)
-{
-    return fixpoint_t(std::tan(float(x)));
-}
-// min/max
-inline fixpoint_t fminfx(fixpoint_t x, fixpoint_t y)
-{
-    return fixpoint_t(std::min(float(x), float(y)));
-}
-inline fixpoint_t fmaxfx(fixpoint_t x, fixpoint_t y)
-{
-    return fixpoint_t(std::max(float(x), float(y)));
-}
-*/
 
 // fx version
 inline float fabsfx(float x)
@@ -253,11 +145,6 @@ inline float tanfx(float x)
 {
     return std::tan(x);
 }
-// min/max
-/*
-#define minfx(x, y) { (((x) < (y)) ? (x) : (y)) }
-#define maxfx(x, y) { (((x) < (y)) ? (y) : (x)) }
-*/
 
 inline float minfx(float x, float y) { return (x < y) ? x : y; }
 inline float maxfx(float x, float y) { return (x < y) ? y : x; }
@@ -281,7 +168,5 @@ inline float maxfx(float x, float y) { return (x < y) ? y : x; }
 /***************************END USER SECTION ***************************/
 
 /*******************BEGIN ARCHITECTURE SECTION (part 2/2)***************/
-
-
 
 /******************* END minimal-fixed-point.cpp ****************/
