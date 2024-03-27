d1a1bc5927b3e937804e7024e9e72a4ed2f1f3bd
Yann Orlarey <orlarey@gmail.com>
2024-03-27 16:37:02 +0100

    Fix Interval NE operation

--- compiler/interval/intervalNe.cpp
+++ compiler/interval/intervalNe.cpp
@@ -1,11 +1,11 @@
 /* Copyright 2023 Yann ORLAREY
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,15 +28,9 @@ namespace itv {
 
 interval interval_algebra::Ne(const interval& x, const interval& y) const
 {
-    if (x.isEmpty() || y.isEmpty()) {
-        return {};
-    }
-    if ((x.hi() < y.lo()) || x.lo() > y.hi()) {
-        return interval{1.0};
-    }
-    if ((x.hi() == y.lo()) || x.lo() == y.hi()) {
-        return interval{0.0};
-    }
+    if (x.isEmpty() || y.isEmpty()) { return {}; }
+    if ((x.hi() < y.lo()) || x.lo() > y.hi()) { return interval{1.0}; }
+    if ((x.hi() == y.lo()) && x.lo() == y.hi()) { return interval{0.0}; }
     return {0, 1};
 }
 
