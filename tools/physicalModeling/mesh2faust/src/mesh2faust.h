825db061986f74f62bde61f3fbba3d2f890d5129
Karl Hiner <karl.hiner@gmail.com>
2024-03-31 12:40:58 -0400

    mesh2faust: Add computed mode freqs and gains to API responses, and other cleanup.
    One important use-case this opens is using the lowest (fundamental) frequency as a default `freq` value
    when passing `freqControl = true`, so that by default the model's fundamental frequency is the same
    as the one with `freqControl = false` (estimated based on the mesh by FEM).

--- tools/physicalModeling/mesh2faust/src/mesh2faust.h
+++ tools/physicalModeling/mesh2faust/src/mesh2faust.h
@@ -17,31 +17,39 @@ struct MaterialProperties {
 
 struct CommonArguments {
     std::string modelName = "modalModel"; // name for the generated model
-    bool freqControl = false;    // freq control activated
-    float modesMinFreq = 20;     // lowest mode freq
-    float modesMaxFreq = 10000;  // highest mode freq
-    int targetNModes = 20;       // number of synthesized modes
-    int femNModes = 100;         // number of modes to be computed for the finite element analysis
-    std::vector<int> exPos = {}; // specific excitation positions
-    int nExPos = -1;             // number of excitation positions (default is max)
-    bool showFreqs = false;      // display computed frequencies
-    bool debugMode = false;      // for verbose printing
+    bool freqControl = false;    // Freq control activated
+    float modesMinFreq = 20;     // Lowest mode freq
+    float modesMaxFreq = 10000;  // Highest mode freq
+    int targetNModes = 20;       // Number of synthesized modes
+    int femNModes = 100;         // Number of modes to be computed with Finite Element Analysis (FEA)
+    std::vector<int> exPos = {}; // Specific excitation positions
+    int nExPos = -1;             // Number of excitation positions (default is max)
+    bool showFreqs = false;      // Display computed frequencies
+    bool debugMode = false;      // Verbose printing
+};
+
+// The response type of all mesh2faust functions.
+// todo introduce an intermediate function to return just mode freqs and gains in its own struct
+// (and anything else needed) then a function to turn this into dsp code.
+struct Response {
+    std::string modelDsp; // Faust DSP code defining the model function, with the provided `modelName`
+    std::vector<float> modeFreqs; // Mode frequencies
+    std::vector<std::vector<float>> modeGains; // Mode gains by [exitation position][mode]
 };
 
 // The main library function.
 // The `mesh2faust` command line tool wraps this function.
-// Returns the generated Faust code as a string.
-std::string mesh2faust(
+Response mesh2faust(
     const char *objectFileName = "", // .obj file name
-    MaterialProperties materialProperties = {}, // material properties to set on the generated volumetric mesh
+    MaterialProperties materialProperties = {}, // Material properties of the mesh, for FEA
     CommonArguments args = {}
 );
 
 // This version takes a pre-loaded tetraheral mesh.
 // Material properties are assumed to already be baked into the mesh.
-std::string mesh2faust(TetMesh *volumetricMesh, CommonArguments args = {});
+Response mesh2faust(TetMesh *volumetricMesh, CommonArguments args = {});
 
-std::string mesh2faust(
+Response mesh2faust(
     const Eigen::SparseMatrix<double> &M, // Mass matrix
     const Eigen::SparseMatrix<double> &K, // Stiffness matrix
     int numVertices,
