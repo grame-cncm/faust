4ddba7e38d7103b07e5118478ef1b0a3db048fa8
Martin Bartlett <jbartle9@dxc.com>
2024-05-26 18:43:38 +0200

    All for docker install from installer

--- architecture/chaos-stratus/build-effect.sh
+++ architecture/chaos-stratus/build-effect.sh
@@ -10,6 +10,13 @@ _term() {
 MY_PATH=$(cd $(dirname $0); pwd
 . ${MY_PATH}/_pedal-tools.sh
 
+PEDAL_BUILD="buildOnStratus"
+if [[ "$1" == "-nodocker" ]]; then
+  shift
+else
+  which docker > /dev/null && PEDAL_BUILD="buildWithDocker"
+fi
 EFFECT_CPP=${1:-${EFFECT_CPP:?first argument must be the CPP file path}}
 EFFECT_SO=${2:-${EFFECT_SO:?second argument must be the SO file path}}
-buildOnStratus "${EFFECT_CPP}" "${EFFECT_SO}"
+
+${PEDAL_BUILD} "${EFFECT_CPP}" "${EFFECT_SO}"
