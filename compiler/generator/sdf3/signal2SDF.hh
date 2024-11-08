05f17622758ec80564957d6013fdf20e3a71bce9
Stéphane Letz <letz@grame.fr>
2024-11-08 10:57:45 +0100

    Remove std namespace in signal2SDF.hh, improve 'Developing a new backend' documentation.

--- compiler/generator/sdf3/signal2SDF.hh
+++ compiler/generator/sdf3/signal2SDF.hh
@@ -10,8 +10,6 @@
 // Compile signal expresssions into SDF representations in XML
 //------------------------------------------------------------------
 
-using namespace std;
-
 class Signal2SDF : public TreeTraversal {
    protected:
     bool           fTraceFlag{false};  // trace transformations when true
@@ -20,13 +18,13 @@ class Signal2SDF : public TreeTraversal {
     std::string    fMessage;    // trace message
     std::set<Tree> fVisited;    // avoid visiting a tree twice
 
-    map<std::string, Actor>   actorList;
-    map<std::string, Channel> chList;
-    int                       chCount  = 0;
-    int                       outCount = 0;
-    vector<std::string>       delayActors;
-    vector<std::string>       recActors;
-    vector<std::string>       inputArgTrackedActors;
+    std::map<std::string, Actor>   actorList;
+    std::map<std::string, Channel> chList;
+    int                            chCount  = 0;
+    int                            outCount = 0;
+    std::vector<std::string>       delayActors;
+    std::vector<std::string>       recActors;
+    std::vector<std::string>       inputArgTrackedActors;
 
     void visit(Tree t) override;
 
@@ -36,7 +34,7 @@ class Signal2SDF : public TreeTraversal {
     void        sigToSDF(Tree t, std::ostream& fout);
     std::string chAttr(Type t);
     void        mergeChannels(const std::string& ch1, const std::string& ch2);
-    void        bypassRec(const std::string& recActorName, vector<std::string>& inputSignalNames);
+    void bypassRec(const std::string& recActorName, std::vector<std::string>& inputSignalNames);
     std::string channelNameFromPort(Port port);
     std::string channelNameFromActors(const std::string& srcActor, const std::string& dstActor);
     void        updateArguments(const std::string& oldArg, const std::string& newArg);
