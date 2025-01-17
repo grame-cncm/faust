7befb765b1ff8642608d90c3fbb011bd592341f5
Yann Orlarey <orlarey@gmail.com>
2025-01-17 12:47:19 +0100

    Extend merge and split to 0 connexions (for exaple !:>0 and !<:0)

--- compiler/boxes/boxtype.cpp
+++ compiler/boxes/boxtype.cpp
@@ -318,13 +318,13 @@ static bool inferBoxType(Tree box, int* inum, int* onum)
             return false;
         }
 
-        if ((v == 0) || (x == 0) || (x % v != 0)) {
+        if ((v == x) || ((v != 0) && (x % v == 0))) {
+            *inum = u;
+            *onum = y;
+        } else {
             throw faustexception((computeTypeErrorMessage(a, b, v, x, "<:", "split composition",
                                                           " must be a divisor of ")));
         }
-
-        *inum = u;
-        *onum = y;
 
     } else if (isBoxMerge(box, a, b)) {
         int u, v, x, y;
@@ -335,13 +335,13 @@ static bool inferBoxType(Tree box, int* inum, int* onum)
             return false;
         }
 
-        if ((v == 0) || (x == 0) || (v % x != 0)) {
+        if ((v == x) || ((x != 0) && (v % x == 0))) {
+            *inum = u;
+            *onum = y;
+        } else {
             throw faustexception(computeTypeErrorMessage(a, b, v, x, ":>", "merge composition",
                                                          " must be a multiple of "));
         }
-
-        *inum = u;
-        *onum = y;
 
     } else if (isBoxRec(box, a, b)) {
         int u, v, x, y;
