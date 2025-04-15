ea5ea04067c5dda0e71d7d9d8e663870a54c16ec
Yann Orlarey <orlarey@gmail.com>
2025-04-15 10:06:27 +0200

    [FIX] Widget Modulation Bug. Labels in format 'v:group1/h:group2/name' were not handled correctly

--- compiler/transform/boxModulationImplanter.cpp
+++ compiler/transform/boxModulationImplanter.cpp
@@ -39,6 +39,55 @@ using namespace std;
 //----------------------------------------------------------------------
 
 /**
+ * @brief Checks if all elements of a given path are present in a label in the same order.
+ *
+ * This function verifies whether all elements of the `path` are contained in the `wLabel`
+ * in the same order, but not necessarily consecutively. The `wLabel` can contain additional
+ * elements before, between, or after the elements of the `path`.
+ *
+ * For example:
+ * - `path = (b, d, e)` and `wLabel = (a, b, c, d, e, f)` will return `true`.
+ * - `path = (b, d, e)` and `wLabel = (a, b, c, e, d, f)` will return `false`
+ *   because the order of elements in `path` is not preserved in `wLabel`.
+ *
+ * @param path The sequence of elements to match (a linked list-like structure).
+ * @param wLabel The sequence in which to search for the elements of `path` (a linked list-like
+ * structure).
+ * @return `true` if all elements of `path` are found in `wLabel` in the same order, otherwise
+ * `false`.
+ */
+
+static bool isPathMatchingLabel(Tree path, Tree wLabel)
+{
+    // we need to find all elements of path in label
+    // in the same order
+    while (path != gGlobal->nil) {
+        if (wLabel == gGlobal->nil) {
+            // wLabel is exhausted before path
+            // => path and label do not match
+            return false;
+        }
+
+        // search for s in wLabel
+        Tree s = hd(path);
+        while ((wLabel != gGlobal->nil) && (s != hd(wLabel))) {
+            wLabel = tl(wLabel);
+        }
+
+        if (wLabel == gGlobal->nil) {
+            // s wat not found => path and label do not match
+            return false;
+        }
+        // s was found, we continue
+        wLabel = tl(wLabel);
+        path   = tl(path);
+    }
+    // all elements of path were found in label
+    // in the same order => path and label match
+    return true;
+}
+
+/**
  * @brief Implant a modulation circuit around a widget matching a given path.
  *
  * @param widget The widget to modulate if it matches the path
@@ -48,10 +97,14 @@ using namespace std;
 Tree BoxModulationImplanter::implantWidgetIfMatch(Tree widget, Tree wLabel)
 {
     Tree wPath = superNormalizePath(cons(wLabel, gGlobal->nil));
-    if (fPath != wPath) {
+    // std::cerr << "BoxModulationImplanter::implantWidgetIfMatch"
+    //           << " fPath = " << *fPath << "; wPath = " << *wPath << ";  wLabel = " << *wLabel
+    //           << std::endl;
+    if (!isPathMatchingLabel(fPath, wPath)) {
         return widget;  // not the widget we are looking therefore no transformation
     }
 
+    // std::cerr << "We have a MATCH! " << std::endl;
     // We have a matching widget to modulate. Depending on the number of inputs
     // of the modulation circuit we have three cases to consider:
     switch (fInputsNumber) {
@@ -81,8 +134,8 @@ Tree BoxModulationImplanter::implantWidgetIfMatch(Tree widget, Tree wLabel)
 Tree BoxModulationImplanter::implantInsideGroup(int gcode, Tree glabel, Tree x)
 {
     Tree gPath = superNormalizePath(cons(cons(tree(gcode), glabel), gGlobal->nil));
-    // std::cerr << "BoxModulation::modulateIfSameGroup"
-    //           << " gPath = " << *gPath << " fPath = " << *fPath << std::endl;
+    // std::cerr << "BoxModulationImplanter::implantInsideGroup"
+    //           << " fPath = " << *fPath << "; gPath = " << *gPath << std::endl;
     // Transform group label into a path
     // check if fPath is in this group
     Tree rpath;
