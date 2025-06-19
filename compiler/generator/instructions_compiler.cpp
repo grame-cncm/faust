ec2609922ca3038ca01b9d32e3e274b064dfacf8
Stéphane Letz <letz@grame.fr>
2025-06-19 21:36:29 +0200

    Test OD with LLVM and wasm backends.

--- compiler/generator/instructions_compiler.cpp
+++ compiler/generator/instructions_compiler.cpp
@@ -3637,7 +3637,7 @@ ValueInst* InstructionsCompiler::generateOD(Tree sig, const tvec& w)
         }
         fContainer->getCurLoop()->closeODblock();
     }
-    
+
     // There is no compiled expression
     return IB::genNullValueInst();
 }
@@ -3706,7 +3706,7 @@ ValueInst* InstructionsCompiler::generateUS(Tree sig, const tvec& w)
         CS(x);
     }
     fContainer->getCurLoop()->closeUSblock();
-    
+
     // There is no compiled expression
     return IB::genNullValueInst();
 }
@@ -3775,7 +3775,7 @@ ValueInst* InstructionsCompiler::generateDS(Tree sig, const tvec& w)
         CS(x);
     }
     fContainer->getCurLoop()->closeDSblock();
-    
+
     // There is no compiled expression
     return IB::genNullValueInst();
 }
