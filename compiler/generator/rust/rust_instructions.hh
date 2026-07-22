515dc515c668f7ea285e6d3454a3f651f1be646e
crop <crop_tech@proton.me>
2026-07-22 22:57:02 +0200

    implement bitcastinst for rust to make -ftz 2 work in rust

--- compiler/generator/rust/rust_instructions.hh
+++ compiler/generator/rust/rust_instructions.hh
@@ -490,7 +490,12 @@ class RustInstVisitor : public TextInstVisitor {
         }
     }
 
-    virtual void visit(BitcastInst* inst) { faustassert(false); }
+    virtual void visit(BitcastInst* inst) {
+        std::string type = fTypeManager->generateType(inst->fType);
+        *fOut << "(";
+        inst->fInst->accept(this);
+        *fOut << ".to_bits().cast_signed())";
+    }
 
     virtual void visit(FunCallInst* inst)
     {
